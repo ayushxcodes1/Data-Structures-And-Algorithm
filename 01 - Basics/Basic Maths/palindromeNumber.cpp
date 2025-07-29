ed269a35bdb4267509d9abae9ea28fe37cecdead
ayushporwal01 <porwalayush953@gmail.com>
2025-07-29 10:07:04 +0530

    Initial Commit

---
+++ 01 - Basics/Basic Maths/palindromeNumber.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool palindromeNumber(int n) {
+ int revNum = 0;
+ int dup = n;
+
+  while(n > 0) {
+    int lastDigit = n % 10;
+    revNum = (revNum * 10) + lastDigit;
+    n = n / 10;
+  }
+   if(revNum == dup) cout << "True";
+   else cout << "False";
+}
+
+int main() {
+    palindromeNumber(121);
+    return 0;
+}
