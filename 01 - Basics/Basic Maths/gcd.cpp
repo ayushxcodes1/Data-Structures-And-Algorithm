fa421d164afb5b3abc86fc21e2e86ba6fea7db9a
ayushporwal01 <porwalayush953@gmail.com>
2025-07-29 19:27:26 +0530

    Initial Commit

---
+++ 01 - Basics/Basic Maths/gcd.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Time Complexity - O(min(N1, N2)) - 1 to min
+int findGcd(int n1, int n2) {
+    if (n1 == 0 && n2 == 0) return -1; //GCD undefined
+    if (n1 == 0 || n2 == 0) return n1 == 0 ? n2 : n1; // Returns the non-zero number
+
+    int gcd = 1;
+    for (int i = 1; i <= min(n1, n2); i++) {
+        if ((n1 % i == 0) && (n2 % i == 0)) {
+            gcd = i;
+        }
+    }
+    return gcd;
+}
+
+// Time Complexity - O(min(N1, N2)) - min to 1
+int findGcd2(int n1, int n2) {
+    if (n1 == 0 && n2 == 0) return -1;
+    if (n1 == 0 || n2 == 0) return n1 == 0 ? n2 : n1;
+
+
+    for (int i = min(n1, n2); i >= 1; i--) {
+        if ((n1 % i == 0) && (n2 % i == 0)) {
+            return i;
+        }
+    }
+    return 1;
+}
+
+// Time Complexity - O(logφ(min(n1, n2))) - Euclidean Algorithm
+int findGcd3(int n1, int n2) {
+
+    if (n1 == 0 && n2 == 0) return -1;
+    if (n1 == 0 || n2 == 0) return n1 == 0 ? n2 : n1;
+
+
+    while (n1 > 0 && n2 > 0) {
+        if (n1 > n2)
+            n1 = n1 % n2;
+        else
+            n2 = n2 % n1;
+    }
+    if (n1 == 0)
+        return n2;
+    return n1;
+}
+
+
+int main() {
+    int res = findGcd(52, 10); 
+    int res2 = findGcd2(52, 10);
+    int res3 = findGcd3(52, 10);
+    
+    int res4 = __gcd(52, 10); //uses Euclidean Algorithm
+
+    cout << res << endl; 
+    cout << res2 << endl; 
+    cout << res3 << endl;
+    cout << res4 << endl;
+    return 0;
+}
