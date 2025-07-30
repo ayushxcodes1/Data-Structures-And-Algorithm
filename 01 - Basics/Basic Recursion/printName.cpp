2e62779fddb21524aa14e8c97c08bd465b722e4e
ayushporwal01 <porwalayush953@gmail.com>
2025-07-30 17:54:48 +0530

    Initial Commit

--- 01 - Basics/Basic Recursion/printName.cpp
+++ 01 - Basics/Basic Recursion/printName.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-void print(int cnt, string name) {
+void printName(int cnt, string name) {
     if(cnt > 5) return;
     cout << name << endl;
-    print(cnt + 1, name);
+    printName(cnt + 1, name);
 }
 
 int main() {
   string name = "Ayush";
-  print(1, name);
+  printName(1, name);
   return 0;
 }
