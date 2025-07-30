918ac476232a653affa0b258f91e61536ad6b83c
ayushporwal01 <porwalayush953@gmail.com>
2025-07-30 11:03:13 +0530

    Initial Commit

--- 01 - Basics/Basic Maths/countDigits.cpp
+++ 01 - Basics/Basic Maths/countDigits.cpp
@@ -4,6 +4,9 @@ using namespace std;
 
 //Time Complexity - O(log₁₀ * N)
 int countDigit(int n) {
+    if (n == 0) return 1;
+
+    n = abs(n);
     int cnt = 0;
     while (n > 0) {
         cnt++;
@@ -12,8 +15,19 @@ int countDigit(int n) {
     return cnt;
 }
 
+int countDigit2(int n) {
+    if (n == 0) return 1;
+
+    int cnt = (int)(log10(abs(n)) + 1);
+    return cnt;
+}
+
 int main() {
-    int res = countDigit(4);
-    cout << res; 
+    int res = countDigit(0); 
+    int res2 = countDigit2(0); 
+
+    cout << res << endl; 
+    cout << res2 << endl; 
+
     return 0;
 }
