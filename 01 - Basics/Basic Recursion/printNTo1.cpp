a45b7f6e5a2e51f74b108d8c9f042bf14341e498
ayushporwal01 <porwalayush953@gmail.com>
2025-07-30 18:33:12 +0530

    Initial Commit

--- 01 - Basics/Basic Recursion/printNTo1.cpp
+++ 01 - Basics/Basic Recursion/printNTo1.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-void print(int i, int n) {
-    if(i > n) return;
+void print(int n) {
+    if(n < 1) return;
     cout << n << " ";
-    print(i, n - 1);
+    print(n - 1);
 }
 
 int main() {
   int n = 10;
-  print(1, n);
+  print(n);
   return 0;
 }
