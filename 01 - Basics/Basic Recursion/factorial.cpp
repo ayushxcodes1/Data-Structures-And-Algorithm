a891dd6e263651560ad48c3573017747632f946d
ayushporwal01 <porwalayush953@gmail.com>
2025-07-31 10:29:16 +0530

    Initial Commit

--- 01 - Basics/Basic Recursion/factorial.cpp
+++ 01 - Basics/Basic Recursion/factorial.cpp
@@ -2,13 +2,16 @@
  
 using namespace std;
 
-int  Factorial(int n) {
+//Parametrized
+
+//Functional 
+int  Factorial2(int n) {
    if(n == 1) return 1;
-   return n * Factorial(n - 1);
+   return n * Factorial2(n - 1);
 }
 
 int main() {
-  int res = Factorial(5);
+  int res = Factorial2(5);
   cout << "factorial is " << res; 
   return 0;
 }
