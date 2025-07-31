693bb67a88b5c8f15d700692ad8946f3dab86f83
ayushporwal01 <porwalayush953@gmail.com>
2025-07-31 16:35:46 +0530

    Initial Commit

--- 01 - Basics/Basic Recursion/palindromeString.cpp
+++ 01 - Basics/Basic Recursion/palindromeString.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 //Approach 1
-bool isPalindrome(string s, int l, int r) {
+bool isPalindrome(string &s, int l, int r) {
    if(l >= r) return true;
    if(s[l] != s[r]) return false;
    return isPalindrome(s, l + 1, r - 1);
 }
 
 //Approach 2
-bool isPalindrome2(string s, int i, int n) {
+bool isPalindrome2(int i, string &s, int n) {
    if(i >= n/2) return true;
    if(s[i] != s[n - i - 1]) return false;
-   return isPalindrome2(s, i + 1, n);
+   return isPalindrome2(i + 1, s, n);
 }
 
 int main() {
@@ -23,7 +23,7 @@ int main() {
   bool res = isPalindrome(s, 0, n-1);
   cout << boolalpha << res << endl;
 
-  bool res2 = isPalindrome2(s, 0, n);
+  bool res2 = isPalindrome2(0, s, n);
   cout << boolalpha << res2; 
   return 0;
 }
