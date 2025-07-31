2e2305dd9aa0a09f9e4095ce0e676e684d5afb11
ayushporwal01 <porwalayush953@gmail.com>
2025-07-31 17:55:07 +0530

    Initial Commit

--- 01 - Basics/Basic Recursion/palindromeString.cpp
+++ 01 - Basics/Basic Recursion/palindromeString.cpp
@@ -14,7 +14,7 @@ bool isPalindrome2(int i, string &s, int n) {
    if(i >= n/2) return true;
    if(s[i] != s[n - i - 1]) return false;
    return isPalindrome2(i + 1, s, n);
-}
+}   
 
 int main() {
   string s = "racecar";
