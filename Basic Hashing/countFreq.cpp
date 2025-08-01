8dbd3dc9a0529ecccff6ac985d629a71594edac3
ayushporwal01 <porwalayush953@gmail.com>
2025-08-01 10:39:31 +0530

    Initial Commit

---
+++ Basic Hashing/countFreq.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int arr[5] = {1, 2, 1, 3, 2};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int hash[13] = {0};
+
+  for(int i = 0; i < n; i++) {
+    hash[arr[i]]++;
+  }
+
+  int x;
+  while(cin >> x) {
+    cout << hash[x] << " ";
+  }
+
+  return 0;
+}
