4a841e0d92689aeb4468546b365057b1e183ef84
ayushporwal01 <porwalayush953@gmail.com>
2025-08-02 16:21:46 +0530

    Initial Commit

--- 01 - Basics/Patterns/patterns.cpp
+++ 01 - Basics/Patterns/patterns.cpp
@@ -263,6 +263,6 @@ int main() {
     // print15(4);
     // print16(4);
     // print17(4);
-    print(4);
+    print18(5);
     return 0;
 }
--- 01 - Basics/Patterns/tempCodeRunnerFile.cpp
+++ 01 - Basics/Patterns/tempCodeRunnerFile.cpp
@@ -1,1 +1,7 @@
-print16(4);
+
+    // print15(4);
+    // print16(4);
+    // print17(4);
+    print18(5);
+    return 0;
+}
