a6c96bb22045644b46040872fbf8cb79b56bc0b7
ayushporwal01 <porwalayush953@gmail.com>
2025-08-02 15:39:03 +0530

    Initial Commit

--- 01 - Basics/Patterns/patterns.cpp
+++ 01 - Basics/Patterns/patterns.cpp
@@ -222,7 +222,7 @@ void print17(int n) {
         //characters
         char ch = 'A';
         int breakpoint = (2 * i + 1) / 2;
-        for(int j = 0; j < 2 * i + 1; j++) {
+        for(int j = 1; j <= 2 * i + 1; j++) {
             cout << ch;
             if(j <= breakpoint) ch++;
             else ch--;
