f3ed06772bb3d9c87fbb9b62bf8b1c9bf2f29fd6
ayushporwal01 <porwalayush953@gmail.com>
2025-08-02 19:54:31 +0530

    Initial Commit

--- 01 - Basics/Patterns/patterns.cpp
+++ 01 - Basics/Patterns/patterns.cpp
@@ -326,8 +326,12 @@ void print21(int n) {
 
 void print22(int n) {
     for(int i = 0; i < 2 * n - 1; i++) {
-        for(int j = 0; j < 2 * n - 1; j++) {
-        
+        for(int j = 0; j < 2*n - 1; j++) {
+            int top = i;
+            int left = j;
+            int right = (2*n - 2) - i;
+            int bottom = (2*n - 2) - j;
+            cout << (n - min(min(top, bottom), min(left, right)));
         }
         cout << endl;  
     }
