e3fd63ac389b449df982c9d39941e05507f8acbd
ayushporwal01 <porwalayush953@gmail.com>
2025-08-02 11:39:37 +0530

    Initial Commit

--- 01 - Basics/Patterns/patterns.cpp
+++ 01 - Basics/Patterns/patterns.cpp
@@ -206,7 +206,7 @@ void print16(int n) {
     for(int i = 0; i < n; i++) {
         char ch = 'A' + i;
         for(int j = 0; j <= i; j++) {
-            cout << ch;
+            cout << ch << " ";
         }
         cout << endl;
     }
