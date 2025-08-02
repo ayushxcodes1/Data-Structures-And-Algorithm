7b0c2dee31cd8da99df529e2d837cc384a82abbf
ayushporwal01 <porwalayush953@gmail.com>
2025-08-02 17:41:46 +0530

    Initial Commit

--- 01 - Basics/Patterns/patterns.cpp
+++ 01 - Basics/Patterns/patterns.cpp
@@ -74,7 +74,7 @@ void print7(int n) {
     }
 }
 
-void print8(int n) { //4
+void print8(int n) { 
     for (int i = n; i > 0; i--) { //n = 4 -> 1
         // space
         for (int j = 0; j < n - i; j++) { 
@@ -263,7 +263,7 @@ void print19(int n) {
         }
         cout << endl;
     }
-    
+
     for(int i = 0; i < n; i++) {
         //stars
         for(int j = 1; j <= n - i; j++) {
