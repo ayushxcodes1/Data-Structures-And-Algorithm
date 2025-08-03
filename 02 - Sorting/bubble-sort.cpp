97b144c66355820ab9fbf116c4a3461aaf26b1c6
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 11:25:25 +0530

    Initial Commit

--- 02 - Sorting/bubble-sort.cpp
+++ 02 - Sorting/bubble-sort.cpp
@@ -2,16 +2,14 @@
 
 //Time Complexity - O(N^2)
 void bubbleSort(int arr[], int n) {
-    for(int i = 0; i <= n - 2; i++) {
-        int mini = i;
-        for(int j = i; j <= n - 1; j++) {
-            if(arr[j] < arr[mini]) {
-               mini = j;
+    for(int i = n - 1; i >= 0; i++) {
+        for(int j = 0; j <= i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+               int temp = arr[j + 1];
+               arr[j + 1] =  arr[j];
+               arr[j] = temp;
             }
         }
-        int temp = arr[mini];
-        arr[mini] = arr[i];
-        arr[i] = temp;
     }
 }
 
