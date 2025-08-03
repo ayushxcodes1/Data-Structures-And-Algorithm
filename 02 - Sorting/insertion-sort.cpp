ef903094c1ebf48a6fa688fef15ebc24ae7c12eb
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 22:44:19 +0530

    Initial Commit

--- 02 - Sorting/insertion-sort.cpp
+++ 02 - Sorting/insertion-sort.cpp
@@ -7,10 +7,10 @@ void insertionSort(int arr[], int n) {
     for(int i = 0; i < n; i++) {
         int j = i;
         while(j > 0 && arr[j - 1] > arr[j]) {
-            int temp = arr[j - 1];
-            arr[j - 1] = arr[j];
-            arr[j] = temp;
-            j--;
+          int temp = arr[j - 1];
+          arr[j - 1] = arr[j];
+          arr[j] = temp;
+          j--;
         }
     }
 }
