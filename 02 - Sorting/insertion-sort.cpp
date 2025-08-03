8de18795f6d29db6642fa0ce1eea749faacf1494
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 17:17:46 +0530

    Initial Commit

--- 02 - Sorting/insertion-sort.cpp
+++ 02 - Sorting/insertion-sort.cpp
@@ -15,6 +15,7 @@ void insertionSort(int arr[], int n) {
     }
 }
 
+
 int main() {
    int arr[7] = {6, 8, 9, 12, 14, 15, 5};
    int n = sizeof(arr) / sizeof(arr[0]);
---
+++ 02 - Sorting/merge-sort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void mS(vector<int> &arr, int low, int high) {
+
+}
+
+void mergeSort(vector<int> &arr, int n) {
+    mS(arr, 0, n - 1);
+}
+
+int main() {
+  vector<int> arr = {3, 1, 2, 4, 1, 5, 2, 6, 4};
+  int n = arr.size();
+
+  mergeSort(arr, n);
+
+  for(auto it : arr) {
+    cout << it << " ";
+  }
+  return 0;
+}
