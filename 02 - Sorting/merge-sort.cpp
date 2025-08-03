9b188115b12caedfdde31689b8912a22dfe885ff
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 17:19:50 +0530

    Initial Commit

--- 02 - Sorting/merge-sort.cpp
+++ 02 - Sorting/merge-sort.cpp
@@ -5,6 +5,9 @@ using namespace std;
 void mS(vector<int> &arr, int low, int high) {
     if(low == high) return;
     int mid = (low + high) / 2;
+    mS(arr, low, mid);
+    mS(arr, mid + 1, high);
+    
 }
 
 void mergeSort(vector<int> &arr, int n) {
