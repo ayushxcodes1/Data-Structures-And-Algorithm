429ee3f84b296fed40ee29a3414f7dce33134b32
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 20:10:11 +0530

    Initial Commit

--- 02 - Sorting/quick-sort.cpp
+++ 02 - Sorting/quick-sort.cpp
@@ -3,7 +3,21 @@
 using namespace std;
 
 int partition(vector<int> &arr, int low, int high) {
-    
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    while(i < j) {
+        while(arr[i] <= pivot && i <= high - 1) {
+            i++;
+        }
+
+        while(arr[j] > pivot && i <= low + 1) {
+            j--;
+        }
+        if(i < j) swap(arr[i], arr[j]);
+    }
+    swap(arr[low], arr[j]);
+    return j;
 }
 
 void qs(vector<int> &arr, int low, int high) {
