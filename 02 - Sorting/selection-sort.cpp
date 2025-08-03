3294659d4c98ac3ebc1a0fd33fd7788014ef4a96
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 09:55:07 +0530

    Initial Commit

---
+++ 02 - Sorting/selection-sort.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+void selectionSort(int arr[], int n) {
+   
+}
+
+using namespace std;
+
+int main() {
+   int arr[6] = {13, 46, 24, 52, 20, 9};
+   int n = sizeof(arr) / sizeof(arr[0]);
+
+   for(auto it : arr) {
+    cout << it << " ";
+   }
+  return 0;
+}
