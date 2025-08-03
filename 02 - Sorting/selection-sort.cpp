355eb4f0b70440a3da1f4c95480f409670b4ae63
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 09:55:16 +0530

    Initial Commit

--- 02 - Sorting/selection-sort.cpp
+++ 02 - Sorting/selection-sort.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 
 void selectionSort(int arr[], int n) {
-   
+    for(int i = 0; i <= n - 2; i++) {
+        int mini = i;
+        for(int j = i; j <= n - 1; j++) {
+            if(arr[j] < arr[mini]) {
+               mini = j;
+            }
+        }
+        int temp = arr[mini];
+        arr[mini] = arr[i];
+        arr[i] = temp;
+    }
 }
 
 using namespace std;
