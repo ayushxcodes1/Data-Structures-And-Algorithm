15b9614798fd27ee54a534ea73624adce1fa6c87
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 22:45:18 +0530

    Initial Commit

--- 02 - Sorting/insertion-sort.cpp
+++ 02 - Sorting/insertion-sort.cpp
@@ -4,15 +4,15 @@ using namespace std;
 
 //Time Complexity - Worst & Avg: O(N^2), Best: O(N)
 void insertionSort(int arr[], int n) {
-    for(int i = 0; i < n; i++) {
-        int j = i;
-        while(j > 0 && arr[j - 1] > arr[j]) {
-          int temp = arr[j - 1];
-          arr[j - 1] = arr[j];
-          arr[j] = temp;
-          j--;
-        }
+  for(int i = 0; i < n; i++) {
+    int j = i;
+    while(j > 0 && arr[j - 1] > arr[j]) {
+      int temp = arr[j - 1];
+      arr[j - 1] = arr[j];
+      arr[j] = temp;
+      j--;
     }
+  }
 }
 
 
