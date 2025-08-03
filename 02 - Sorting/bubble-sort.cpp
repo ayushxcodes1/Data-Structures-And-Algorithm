ecdab13f51b2f8b2df69426d16be93e45f44ee7b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-03 11:21:55 +0530

    Initial Commit

--- 02 - Sorting/bubble-sort.cpp
+++ 02 - Sorting/bubble-sort.cpp
@@ -21,8 +21,10 @@ int main() {
    int arr[6] = {13, 46, 24, 52, 20, 9};
    int n = sizeof(arr) / sizeof(arr[0]);
 
-   for(auto it : arr) {
+  bubbleSort(arr, n);
+  
+  for(auto it : arr) {
     cout << it << " ";
-   }
+  }
   return 0;
 }
