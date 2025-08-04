05ec479d5d15493d633ad7663ed9c0c340ea45d4
ayushporwal01 <porwalayush953@gmail.com>
2025-08-04 09:53:13 +0530

    Initial Commit

--- 02 - Sorting/quick-sort.cpp
+++ 02 - Sorting/quick-sort.cpp
@@ -7,11 +7,11 @@ int partition(vector<int> &arr, int low, int high) {
     int i = low;
     int j = high;
     while(i < j) {
-        while(arr[i] <= pivot && i <= high - 1) {
+        while(arr[i] <= pivot && i <= high - 1) {   
             i++;
         }
 
-        while(arr[j] > pivot && i >= low + 1) {
+        while(arr[j] > pivot && j >= low + 1) {
             j--;
         }
         if(i < j) swap(arr[i], arr[j]);
