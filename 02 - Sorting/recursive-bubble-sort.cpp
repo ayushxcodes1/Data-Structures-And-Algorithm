7c2dcc27858d7ec46f4fa0d313262b0ab6fbe0de
ayushporwal01 <porwalayush953@gmail.com>
2025-08-04 10:54:04 +0530

    Initial Commit

--- 02 - Sorting/recursive-bubble-sort.cpp
+++ 02 - Sorting/recursive-bubble-sort.cpp
@@ -5,11 +5,14 @@ using namespace std;
 void bubbleSort(vector<int> &arr, int n) {
     if(n == 1) return;
 
+    int didSwap = 0;
     for(int j = 0; j < n - 1; j++) {
         if(arr[j] > arr[j + 1]) {
             swap(arr[j], arr[j + 1]);
+            didSwap = 1;
         }
     }
+    if(didSwap == 0) return;
 
     bubbleSort(arr, n - 1);
 }
