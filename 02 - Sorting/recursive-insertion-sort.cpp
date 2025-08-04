d06c1ecf4d43bf55f854b9465d3baf3469c2d4d5
ayushporwal01 <porwalayush953@gmail.com>
2025-08-04 11:21:02 +0530

    Initial Commit

--- 02 - Sorting/recursive-insertion-sort.cpp
+++ 02 - Sorting/recursive-insertion-sort.cpp
@@ -4,26 +4,15 @@ using namespace std;
 
 //Time Complexity - Worst & Avg: O(N^2), Best: O(N)
 //Space Complexity - O(N)
-void bubbleSort(vector<int> &arr, int n) {
-    if(n == 1) return;
-
-    int didSwap = 0;
-    for(int j = 0; j < n - 1; j++) {
-        if(arr[j] > arr[j + 1]) {
-            swap(arr[j], arr[j + 1]);
-            didSwap = 1;
-        }
-    }
-    if(didSwap == 0) return;
-
-    bubbleSort(arr, n - 1);
+void insertionSort(vector<int> &arr, int n) {
+    
 }
 
 int main() {
   vector<int> arr = {13, 46, 24, 52, 20, 9};
   int n = arr.size();
 
-  bubbleSort(arr, n);
+  insertionSort(arr, n);
 
   for(auto it : arr) {
     cout << it << " ";
