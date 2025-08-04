e189fc387a0f2a3c5cebee3a0862b0b55c057ed7
ayushporwal01 <porwalayush953@gmail.com>
2025-08-04 10:26:08 +0530

    Initial Commit

--- 02 - Sorting/quick-sort.cpp
+++ 02 - Sorting/quick-sort.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//TC: Best & Avg O(N * logN), Worst O(N^2), SC: Best & Avg O(logN), Worst O(N)
 int partition(vector<int> &arr, int low, int high) {
     int pivot = arr[low];
     int i = low;
