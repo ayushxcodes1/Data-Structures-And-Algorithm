2fbfb34703eddf1ecf9a90e73cf4493dae627405
ayushporwal01 <porwalayush953@gmail.com>
2025-08-05 09:53:48 +0530

    Initial Commit

--- 03 - Arrays/Easy/largestElement.cpp
+++ 03 - Arrays/Easy/largestElement.cpp
@@ -22,7 +22,7 @@ int largestElement2(int arr[], int n) {
 int main() {
   int arr[6] = {2, 8, 6, 13, 5, 9};
   int n = sizeof(arr) / sizeof(arr[0]);
-  int res = largestElement(arr, n);
+  int res = largestElement2(arr, n);
   cout << "Largest Element: " << res;
   return 0;
 }
