e86c027ebaa07be3983c7c74f304543f13ce1199
ayushporwal01 <porwalayush953@gmail.com>
2025-08-05 21:43:11 +0530

    Initial Commit

---
+++ 03 - Arrays/Easy/sortedArray.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Time Complexity: O(N), Space Complexity: O(1)
+int isSorted(int arr[], int n) {
+    for(int i = 1; i < n; i++) {
+        if(arr[i - 1] <= arr[i]) {
+
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+int main() {
+  int arr[5] = {1, 2, 1, 3, 4};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  
+  return 0;
+}
