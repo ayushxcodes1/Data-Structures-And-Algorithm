58cf84aac8e3c2d35314c0ce0ee64e9b6398d17e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-05 11:17:12 +0530

    Initial Commit

--- 03 - Arrays/Easy/secondLargest.cpp
+++ 03 - Arrays/Easy/secondLargest.cpp
@@ -20,7 +20,19 @@ int secondLargest(vector<int> &arr, int n) {
 
 //Better - TC: (2N), SC: O(1)
 int secondLargest2(vector<int> &arr, int n) {
-   
+    int largest = arr[0];
+    int secLargest = INT_MIN;
+
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > largest) {
+           secLargest = largest;
+           largest = arr[i];
+        } 
+        else if(arr[i] < largest && arr[i] > secLargest) {
+           secLargest = arr[i];
+        }
+    }
+    return secLargest;
 }
 
 
