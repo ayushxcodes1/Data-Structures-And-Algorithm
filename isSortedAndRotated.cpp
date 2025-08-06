3cd592e1e13e64aa99a5d65077c47f1b3fbc4d09
ayushporwal01 <porwalayush953@gmail.com>
2025-08-06 08:30:44 +0530

    Initial Commit

--- isSortedAndRotated.cpp
+++ isSortedAndRotated.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 //Brute - TC: O(N^2), SC: O(N)
 bool isSorted(vector<int> &arr, int n) {
-  for(int i = 0; i < n; i++) {
-    if(arr[i] > arr[i + 1]) {
+  for(int i = 1; i < n; i++) {
+    if(arr[i - 1] <= arr[i]) {
       return false;
     }
   }
