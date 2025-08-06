c1fbbb05c408997b8279189293856d83c1c504cd
ayushporwal01 <porwalayush953@gmail.com>
2025-08-06 08:10:47 +0530

    Initial Commit

--- 03 - Arrays/Easy/isSortedArray.cpp
+++ 03 - Arrays/Easy/isSortedArray.cpp
@@ -16,7 +16,7 @@ int isSorted(int arr[], int n) {
 }
 
 int main() {
-  int arr[5] = {1, 2, 1, 3, 4};
+  int arr[5] = {3, 4, 5, 1, 2};
   int n = sizeof(arr) / sizeof(arr[0]);
   bool res = isSorted(arr, n);
   cout << boolalpha << res;
