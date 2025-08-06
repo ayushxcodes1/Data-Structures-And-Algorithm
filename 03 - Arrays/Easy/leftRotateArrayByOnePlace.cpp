f4376fece9e155e083f7f84edc9ca645e5fad0eb
ayushporwal01 <porwalayush953@gmail.com>
2025-08-06 13:00:56 +0530

    Initial Commit

--- 03 - Arrays/Easy/leftRotateArrayByOnePlace.cpp
+++ 03 - Arrays/Easy/leftRotateArrayByOnePlace.cpp
@@ -12,6 +12,16 @@ vector<int> rotateArray(vector<int> &arr, int n) {
   return arr;
 }
 
+//Right Rotate
+vector<int> rotateArray(vector<int> &arr, int n) {
+  int temp = arr[n - 1];
+  for(int i = n - 1; i > 0; i--) {
+    arr[i] = arr[i - 1];
+  }
+  arr[0] = temp;
+  return arr;
+}
+
 int main() {
   vector<int> arr = {1, 2, 3, 4, 5};
   int n = arr.size();
