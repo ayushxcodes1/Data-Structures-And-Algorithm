f022e8811c41ae9f657c8c08ea897fdd1d51df1b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-06 11:20:38 +0530

    Initial Commit

---
+++ RotateArray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> rotateArray(vector<int> &arr, int n, int k) {
+    int temp = arr[0];
+    for(int i = 1; i < n; i++) {
+        arr[i - 1] = arr[i];
+    }
+    arr[n - 1] = temp;
+    return arr;
+}
+
+int main() {
+  vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
+  int n = arr.size();
+  int res = rotateArray(arr, n, k);
+  cout << res;
+  return 0;
+}
