019aab2dcb984b892cfe6dea9db826158e89e92a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-06 10:43:55 +0530

    Initial Commit

--- removeDuplicatesFromSortedArr.cpp
+++ removeDuplicatesFromSortedArr.cpp
@@ -21,17 +21,14 @@ int removeDuplicates(vector<int> &arr, int n) {
 
 //Better - TC: O(N^2), SC: O(1)
 int removeDuplicates2(vector<int> &arr, int n) {
-    int cnt = 0;
-    for(int i = 0; i < n - 1; i++) {
-        cnt = i;
-        for(int j = i + 1; j < n; j++) {
-            if(arr[j] != arr[i]) {
-               arr[i + 1] = arr[j];
-               break;
-            }
+    int i = 0;
+    for(int j = i + 1; j < n; j++) {
+        if(arr[j] != arr[i]) {
+            arr[i + 1] = arr[j];
+            i++;
         }
     }
-    return cnt;
+    return i + 1;
 }
 
 int main() {
