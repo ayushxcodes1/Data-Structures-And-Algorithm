3dc29c985a5c51bcacacc4e183e2c27da1e75928
ayushporwal01 <porwalayush953@gmail.com>
2025-08-06 08:17:57 +0530

    Initial Commit

--- isSortedAndRotated.cpp
+++ isSortedAndRotated.cpp
@@ -11,10 +11,27 @@ bool isSorted(vector<int> &arr, int n) {
   }
 }
 
+bool check(vector<int> &arr, int n) {
+  vector<int> temp = arr;
+
+  for(int i = 0; i < n; i++) {
+    if(isSorted(temp)) return false;
+
+    //Rotate left by 1
+    int first = temp[0];
+    for(int j = 0; j < n - 1; j++) {
+      temp[j] = temp[j + 1];
+    }
+    temp[n - 1] = first
+  }
+
+  return false;
+}
+
 int main() {
   vector<int> arr = {3, 4, 5, 1, 2};
   int n = sizeof(arr) / sizeof(arr[0]);
-  bool res = isSortedAndRotated(arr, n);
+  bool res = isSorted(arr, n);
   cout << boolalpha << res;
   return 0;
 }
