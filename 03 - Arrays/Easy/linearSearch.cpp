87ed4746b12e6187373891f9fd0ace0058ede75b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-07 17:30:12 +0530

    Initial Commit

--- 03 - Arrays/Easy/linearSearch.cpp
+++ 03 - Arrays/Easy/linearSearch.cpp
@@ -2,7 +2,21 @@
 
 using namespace std;
 
+//TC: O(N), SC: O(1)
+int linearSearch(int arr[], int n, int x) {
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == x) {
+            return i; //found
+        }
+    }
+    return -1; //not found
+}
+
 int main() {
-  
+    int arr[5] = {3, 4, 5, 1, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    linearSearch(arr, n, 4);
+
   return 0;
 }
