7c920b7f04d631720a8aa57ab78b74037f813a90
ayushporwal01 <porwalayush953@gmail.com>
2025-08-07 16:41:53 +0530

    Initial Commit

--- 03 - Arrays/Easy/moveZeroes.cpp
+++ 03 - Arrays/Easy/moveZeroes.cpp
@@ -21,19 +21,10 @@ void moveZeroes(vector<int> &arr, int n) {
     }
 }
 
-//Optimal - TC: O(2N), SC: O(1)
+//Optimal - TC: O(N), SC: O(1)
 void moveZeroes2(vector<int> &arr, int n) {
-    int j = -1;
+    int j = 0;
     for(int i = 0; i < n; i++) {
-        if(arr[i] == 0) {
-           j = i;
-           break;
-        }
-    }
-
-    if(j == -1) return; // No zero found
-
-    for(int i = j + 1; i < n; i++) { 
         if(arr[i] != 0) {
            swap(arr[i], arr[j]);
            j++;
