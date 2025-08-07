8a02cedccdfea070e10e296c03c62eea26ef02bb
ayushporwal01 <porwalayush953@gmail.com>
2025-08-07 08:09:10 +0530

    Initial Commit

--- 03 - Arrays/Easy/rotateArrayByDPlaces.cpp
+++ 03 - Arrays/Easy/rotateArrayByDPlaces.cpp
@@ -3,14 +3,17 @@
 using namespace std;
 
 //TC: O(N), SC: O(1)
-vector<int> leftRotate(vector<int> &arr, int n) {
-  int temp = arr[0]; 
+vector<int> leftRotate(vector<int> &arr, int n, int d) {
+  vector<int> temp; 
+  d = d % n;
   
-  for(int i = 1; i < n; i++) {
-    arr[i - 1] = arr[i];
+  for(int i = 0; i < d; i++) {
+    temp[i] = arr[i];
   }
-  arr[n - 1] = temp;
-  return arr;
+ 
+  for(int i = n - d; i < n; i++) {
+    arr[]
+  }  
 }
 
 //TC: O(N), SC: O(1)
@@ -24,10 +27,10 @@ vector<int> rightRotate(vector<int> &arr, int n) {
 }
 
 int main() {
-  vector<int> arr = {1, 2, 3, 4, 5};
+  vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
   int n = arr.size();
 
-  vector<int> res = leftRotate(arr, n);
+  vector<int> res = leftRotate(arr, n, 3);
 
   for(auto it : res) {
     cout << it << " ";
