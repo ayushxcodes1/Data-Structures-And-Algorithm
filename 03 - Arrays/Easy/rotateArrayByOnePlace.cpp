d3d3cfb5dc30143ce4bab82054f44a455ee1550e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-07 10:35:59 +0530

    Initial Commit

--- 03 - Arrays/Easy/rotateArrayByOnePlace.cpp
+++ 03 - Arrays/Easy/rotateArrayByOnePlace.cpp
@@ -14,7 +14,7 @@ void leftRotate(vector<int> &arr, int n) {
 //Right Rotate - TC: O(N), SC: O(1)
 void rightRotate(vector<int> &arr, int n) {
   int temp = arr[n - 1];
-  for(int i = n - 1; i >= 0; i--) {
+  for(int i = n - 1; i > 0; i--) {
     arr[i] = arr[i - 1];
   }
   arr[0] = temp;
