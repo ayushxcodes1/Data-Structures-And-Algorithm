dac64deb07069105e2dceb8135e8c3e57b667f45
ayushporwal01 <porwalayush953@gmail.com>
2025-08-07 17:11:04 +0530

    Initial Commit

--- 03 - Arrays/Easy/moveZeroes.cpp
+++ 03 - Arrays/Easy/moveZeroes.cpp
@@ -26,7 +26,7 @@ void moveZeroes2(vector<int> &arr, int n) {
     int j = 0;
     for(int i = 0; i < n; i++) {
         if(arr[i] != 0) {
-            if(i != j) {
+            if(i != j) { 
                 swap(arr[i], arr[j]);
             }
            j++;
