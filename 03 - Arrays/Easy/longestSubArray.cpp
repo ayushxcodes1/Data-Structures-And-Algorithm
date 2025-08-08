7d24b987a5c307174476f0cb35111c2391144e03
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 18:28:03 +0530

    Initial Commit

---
+++ 03 - Arrays/Easy/longestSubArray.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Brute Force
+int longestSubArraySum(vector<int> &arr) {
+    int n = arr.size();
+    int len = 0;
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = i; j < n; j++) {
+            for(int k = i; k <= j; k++) {
+                sum += arr[k];
+                if(sum == k) {
+                   len = max(len, j - i + 1);
+                }
+            }
+        }
+    }
+}
+
+int main() {
+  vector<int> arr = {10, 5, 2, 7, 1, 9};
+  int len = longestSubArraySum(arr);
+  cout << "Length is " << len;
+ 
+  return 0;
+}
