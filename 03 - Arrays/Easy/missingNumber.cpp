5a4d8dfa881b8291e2c0bc1922436291797b880e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 08:30:56 +0530

    Initial Commit

--- 03 - Arrays/Easy/missingNumber.cpp
+++ 03 - Arrays/Easy/missingNumber.cpp
@@ -46,14 +46,10 @@ int findMissing3(vector<int> arr, int n) {
 
 //Optimal2 - TC: O(2N), SC: O(1)
 int findMissing3(vector<int> arr, int n) {
-  int xor1 = 0;
-  for(int i = 1; i <= n; i++) {
-    xor1 = xor1 ^ i; 
-  }
-
-  int xor2 = 0;
+  int xor1 = 0, xor2 = 0;
   for(int i = 0; i < n - 1; i++) {
-    xor2 = xor2 ^ arr[i];
+    xor1 = xor1 ^ arr[i];
+    xor2 = xor2 ^ (i + 1);
   }
 
   return xor1 ^ xor2;
