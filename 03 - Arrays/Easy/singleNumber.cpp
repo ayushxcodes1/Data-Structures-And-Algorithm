9b4bf3f630981bd80f78f1d0b5dfb692c48daaec
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 14:39:50 +0530

    Initial Commit

--- 03 - Arrays/Easy/singleNumber.cpp
+++ 03 - Arrays/Easy/singleNumber.cpp
@@ -41,6 +41,28 @@ int singleNumber2(vector<int> &arr) {
     return -1;
 }
 
+//Optimal - TC: O(N), SC: O(k)
+void singleNumber3(vector<int> &arr) {
+    int n = arr.size();
+    int ans = -1;
+    
+    //O(k) - k is no. of distinct elements
+    unordered_map<int, int> mpp;
+
+    //O(N)
+    for(int i = 0; i < n; i++) {
+        mpp[arr[i]]++;
+    }
+
+    //O(k)
+    for(auto it : mpp) {
+        if(it.second == 1) {
+           cout << "Single Number: " << it.first;
+        }
+    }
+}
+
+
 int main() {
   vector<int> arr = {4, 4, 2, 2, 1, 3, 3};
 
