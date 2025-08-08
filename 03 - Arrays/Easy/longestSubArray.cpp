99a443b006beff9bf7555258bd3197d30688e0a4
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 18:52:37 +0530

    Initial Commit

--- 03 - Arrays/Easy/longestSubArray.cpp
+++ 03 - Arrays/Easy/longestSubArray.cpp
@@ -21,17 +21,7 @@ int longestSubArray2(vector<int> &arr, int target) {
 //Better
 int longestSubArray2(vector<int> &arr, int target) {
     int n = arr.size();
-    int len = 0;
-    for(int i = 0; i < n; i++) {
-        int sum = 0;
-        for(int j = i; j < n; j++) {
-            sum += arr[j];
-            if(sum == target) {
-               len = max(len, j - i + 1);
-            }
-        }
-    }
-    return len;
+    
 }
 
 int main() {
