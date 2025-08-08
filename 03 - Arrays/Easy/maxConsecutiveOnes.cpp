3b4fba4c5e5643b2cb59c5909e4fe71c1e6449c1
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 12:59:36 +0530

    Initial Commit

--- 03 - Arrays/Easy/maxConsecutiveOnes.cpp
+++ 03 - Arrays/Easy/maxConsecutiveOnes.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 //Optimal - TC: O(N), SC: O(1)
-int findMaxConsecutiveOnes(vector<int> arr, int n) {
+int findMaxConsecutiveOnes(vector<int> arr) {
     int n = arr.size();
     int count = 0, maxCount = 0;
     for(int i = 0; i < n; i++) {
@@ -23,7 +23,7 @@ int findMaxConsecutiveOnes(vector<int> arr, int n) {
 int main() {
   vector<int> arr = {1, 1, 0, 1, 1, 1};
   int n = arr.size();
-  int res = findMaxConsecutiveOnes(arr, n);
+  int res = findMaxConsecutiveOnes(arr);
   cout << "Maximum Consecutive Ones: " << res;
   return 0;
 }
