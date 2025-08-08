cfd1021c5e160a6b5afe074ffbf80635b75392c8
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 08:11:10 +0530

    Initial Commit

--- 03 - Arrays/Easy/missingNumber.cpp
+++ 03 - Arrays/Easy/missingNumber.cpp
@@ -5,19 +5,22 @@ using namespace std;
 //Brute Force - TC: O(N^2), SC: O(1)
 int findMissing(vector<int> arr, int n) {
     for(int i = 1; i <= n; i++) {
+        bool found = false;
         for(int j = 0; j < n - 1; j++) {
-            if(arr[j] != i) {
-               return i;
+            if(arr[j] == i) {
+               found = true;
+               break;
             }
-            i++;
         }
+        if(!found) return i;
     }
     return -1;
 }
 
 //Optimal - TC: O(N), SC: O(1)
 int findMissing2(vector<int> arr, int n) {
-    int i = 0, j = 1;
+    int i = 0, j = 1; 
+    int flag = 1;
     while(i <= n && j <= n) {
         if((arr[i] + 1) != arr[j]) {
            return arr[i] + 1;
