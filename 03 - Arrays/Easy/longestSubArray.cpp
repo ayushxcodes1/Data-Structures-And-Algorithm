492739def417d08f368fb85ce46367183b5fd331
ayushporwal01 <porwalayush953@gmail.com>
2025-08-08 18:31:56 +0530

    Initial Commit

--- 03 - Arrays/Easy/longestSubArray.cpp
+++ 03 - Arrays/Easy/longestSubArray.cpp
@@ -7,8 +7,8 @@ int longestSubArray(vector<int> &arr, int k) {
     int n = arr.size();
     int len = 0;
     for(int i = 0; i < n; i++) {
-        int sum = 0;
         for(int j = i; j < n; j++) {
+            int sum = 0;
             for(int k = i; k <= j; k++) {
                 sum += arr[k];
                 if(sum == k) {
@@ -22,8 +22,8 @@ int longestSubArray(vector<int> &arr, int k) {
 int main() {
   vector<int> arr = {10, 5, 2, 7, 1, 9};
   int k = 7;
-  int len = longestSubArray(arr, 7);
-  cout << "Length is " << len;
+  int len = longestSubArray(arr, 15);
+  cout << "Length of Longest SubArray with Sum K is " << len;
  
   return 0;
 }
