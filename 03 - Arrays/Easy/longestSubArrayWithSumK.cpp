c8c8e9cdcc6d966b1b06b9e08f0183bbaa182db6
ayushporwal01 <porwalayush953@gmail.com>
2025-08-09 07:20:52 +0530

    Initial Commit

--- 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
+++ 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
@@ -23,19 +23,7 @@ int longestSubArrayWithSumK(vector<int> &arr, int target) {
 //Bettter for +ves & 0's - TC: O(N^2), SC: O(1) / Optimal for both +ves & -ves
 int longestSubArrayWithSumK(vector<int> &arr, int target) {
     int n = arr.size();
-    int len = 0;
-    for(int i = 0; i < n; i++) {
-        for(int j = i; j < n; j++) {
-            int sum = 0;
-            for(int k = i; k <= j; k++) {
-                sum += arr[k];
-            }
-            if(sum == target) {
-               len = max(len, j - i + 1);
-            }
-        }
-    }
-    return len; 
+   
 } 
 
 
