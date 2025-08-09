0a959f893817beed3990c0710e2e2f4bf704f0f9
ayushporwal01 <porwalayush953@gmail.com>
2025-08-09 11:05:18 +0530

    Initial Commit

--- 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
+++ 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
@@ -47,12 +47,19 @@ int longestSubArrayWithSumK(vector<int> &arr, int k) {
     int left = 0, right = 0;
     long long sum = arr[0];
     int maxLen = 0;
-    
-    while(left < n && right < n) {
-        long long sum = 0;
 
+    while(right < n) {
+        while(left <= right && sum > k) {
+            sum -= arr[left];
+            left++;
+        }
+        if(sum == k) {
+            maxLen = max(maxLen, right - left + 1);
+        }
+        right++;
+        if(right < n) sum += arr[right];
     }
-    return len; 
+    return maxLen; 
 } 
 
 int main() {
