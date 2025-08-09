53b4bb0f599a093dc7e32b2de7c7a5d34f90ef44
ayushporwal01 <porwalayush953@gmail.com>
2025-08-09 07:06:51 +0530

    Initial Commit

--- 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
+++ 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
@@ -3,70 +3,13 @@
 using namespace std;
 
 //Brute - TC: O(N^2), SC: O(1)
-int longestSubArrayWithSumK(vector<int> &arr, int target) {
-    int n = arr.size();
-    int len = 0;
-    for(int i = 0; i < n; i++) {
-        int sum = 0;
-        for(int j = i; j < n; j++) {
-            sum += arr[j];
-            if(sum == target) {
-               len = max(len, j - i + 1);
-            }
-        }
-    }
-    return len;
-}
 
-//Better for +ve's & 0's - TC: O(N * logN), SC: O(N) / Optimal for both +ve's & -ve's
-int longestSubArrayWithSumK2(vector<int> &arr, int k) {
-    int n = arr.size();
-    map<int, int> preSumMap;
-    int sum = 0;
-    int maxLen = 0;
-    for(int i = 0; i < n; i++) {
-        sum += arr[i];
-        if(sum == k) {
-           maxLen = max(maxLen, i + 1);
-        }
-
-        int rem = sum - k;
-        if(preSumMap.find(rem) != preSumMap.end()) {
-           int len = i - preSumMap[rem];
-           maxLen = max(maxLen, i + 1);
-        }
-        if(preSumMap.find(sum) == preSumMap.end()) {
-           preSumMap[sum] = i;
-        }
-    }
-    return maxLen;
-}
-
-//Optimal - TC: O(2N), SC: O(1)
-int longestSubArrayWithSumK2(vector<int> &arr, int k) {
-    int n = arr.size();
-    int left = 0, right = 0;
-    int sum = arr[0];
-    int maxLen = 0;
-    while(right < n) {
-        while(left <= right && sum > k) {
-            sum -= arr[left];
-            left++;
-        }
-        if(sum == k) {
-           maxLen = max(maxLen, right - left + 1);
-        }
-        right++;
-        if(right < n) sum += arr[right];
-    }
-    return maxLen;
-}
 
 
 int main() {
   vector<int> arr = {10, 5, 2, 7, 1, 9};
   int k = 7;
-  int len = longestSubArrayWithSumK2(arr, k);
+  int len = longestSubArrayWithSumK(arr, k);
   cout << "Length of Longest SubArray with Sum K is " << len;
  
   return 0;
