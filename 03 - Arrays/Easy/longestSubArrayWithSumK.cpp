6fa85bb26c6a057f33a85f64f918e85cac63b8d4
ayushporwal01 <porwalayush953@gmail.com>
2025-08-09 10:06:46 +0530

    Initial Commit

--- 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
+++ 03 - Arrays/Easy/longestSubArrayWithSumK.cpp
@@ -19,7 +19,7 @@ int longestSubArrayWithSumK(vector<int> &arr, int k) {
 } 
 
 //Better for +ves & 0's - TC: O(NlogN), SC: O(N) / Optimal for +ves, -ves & 0's
-int longestSubArrayWithSumK(vector<int> &arr, int k) {
+int longestSubArrayWithSumK2(vector<int> &arr, int k) {
     int n = arr.size();
     map<long long, int> preSumMap;
     long long sum = 0;
