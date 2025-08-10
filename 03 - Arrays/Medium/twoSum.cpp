63a4359d161ff56465fcb01cde8b5be1b5240af3
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 10:41:35 +0530

    Initial Commit

--- 03 - Arrays/Medium/twoSum.cpp
+++ 03 - Arrays/Medium/twoSum.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 //Brute Force - TC: O(N^2), SC: O(1)
-void TwoSum(vector<int> &arr, int target) {
+vector<int> TwoSum(vector<int> &arr, int target) {
     int n = arr.size();
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
             if(arr[i] + arr[j] == target) {
-               cout << "[" << i << "," << j << "]";
+               return {i, j};
             } 
         }
     }
