9768727854a11eb1f09d9cc79d9bc7d3777f90ee
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 17:57:47 +0530

    Initial Commit

--- 03 - Arrays/Medium/majorityElement.cpp
+++ 03 - Arrays/Medium/majorityElement.cpp
@@ -16,9 +16,29 @@ int majorityElement(vector<int> arr) {
     }
 }
 
+//Better - TC: O(NlogN), SC: O(N)
+int majorityElement2(vector<int> arr) {
+    int n = arr.size();
+    map<int, int> mpp;
+    for(int i = 0; i < n; i++){
+        mpp[arr[i]]++;
+    }
+
+    for(auto it : mpp) {
+        if(it.second > n / 2) {
+           return it.first;
+        }
+    }
+}
+
 int main() {
   vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
   int res = majorityElement(arr);
-  cout << res;
+  cout << "Majority Element: " << res;
+
+  cout << endl;
+
+  int res2 = majorityElement2(arr);
+  cout << "Majority Element: " << res2;
   return 0;
 }
