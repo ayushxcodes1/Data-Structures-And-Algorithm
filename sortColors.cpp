c48fd815f8f552e021ca902c1215c6508c03388a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 16:06:29 +0530

    Initial Commit

--- sortColors.cpp
+++ sortColors.cpp
@@ -15,8 +15,8 @@ vector<int> sortColors2(vector<int> &arr) {
     int cnt0 = 0, cnt1 = 0, cnt2 = 0;
     for(int i = 0; i < n; i++) {
         if(arr[i] == 0) cnt0++;
-        if(arr[i] == 1) cnt1++;
-        if(arr[i] == 2) cnt2++;
+        else if(arr[i] == 1) cnt1++;
+        else cnt2++;
     }
 
     for(int i = 0; i < n; i++) {
