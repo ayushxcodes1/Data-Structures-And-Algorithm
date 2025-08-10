d65fa76bcdc639d6b1ba4fb3cd5aaa22d93997a7
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 16:05:23 +0530

    Initial Commit

--- sortColors.cpp
+++ sortColors.cpp
@@ -12,13 +12,26 @@ vector<int> sortColors(vector<int> &arr) {
 //Better - TC: O(N), SC: O(1)
 vector<int> sortColors2(vector<int> &arr) {
     int n = arr.size();
-    int left = 0, right = n - 1;
-    while(left < right) {
-        if(arr[left] > arr[right]) {
-           swap(arr[left], arr[right]);
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == 0) cnt0++;
+        if(arr[i] == 1) cnt1++;
+        if(arr[i] == 2) cnt2++;
+    }
+
+    for(int i = 0; i < n; i++) {
+        if(cnt0 != 0) {
+           arr[i] = 0;
+           cnt0--;
         }
-        left++;
-        right--;
+        else if(cnt1 != 0) {
+            arr[i] = 1;
+            cnt1--;
+        }
+        else {
+            arr[i] = 2;
+            cnt2--;
+        }
     }
 }
 
