8dfbc23710b563cdfaa49be43a91f00c65d91bb1
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 22:10:14 +0530

    Initial Commit

--- 03 - Arrays/Medium/maxSubArraySum.cpp
+++ 03 - Arrays/Medium/maxSubArraySum.cpp
@@ -5,14 +5,10 @@ using namespace std;
 //Brute - TC: O(N^2), SC: O(1)
 int maxSubArraySum(vector<int> &arr) {
   int n = arr.size();
-  int len = 0;
   for(int i = 0; i < n; i++) {
       int sum = 0;
       for(int j = i; j < n; j++) {
-          sum += arr[j];
-          if(sum == k) {
-            len = max(len, j - i + 1);
-          }
+         
       }
   }
   return len; 
