02f358b86cb5620b4471346ed349dbdb5aadfaca
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 16:18:21 +0530

    Initial Commit

--- sortColors.cpp
+++ sortColors.cpp
@@ -11,26 +11,18 @@ vector<int> sortColors(vector<int> &arr) {
 vector<int> sortColors2(vector<int> &arr) {
     int n = arr.size();
     int cnt0 = 0, cnt1 = 0, cnt2 = 0;
-    for(int i = 0; i < n; i++) {
-        if(arr[i] == 0) cnt0++;
-        else if(arr[i] == 1) cnt1++;
+
+    for(auto num : arr) {
+        if(num == 0) cnt0++;
+        else if(num == 1) cnt1++;
         else cnt2++;
     }
-
-    for(int i = 0; i < n; i++) {
-        if(cnt0 != 0) {
-           arr[i] = 0;
-           cnt0--;
-        }
-        else if(cnt1 != 0) {
-            arr[i] = 1;
-            cnt1--;
-        }
-        else {
-            arr[i] = 2;
-            cnt2--;
-        }
-    }
+ 
+    int idx = 0;
+    while(cnt0--) arr[idx++] = 0;
+    while(cnt1--) arr[idx++] = 1;
+    while(cnt2--) arr[idx++] = 2;
+    
 }
 
 int main() {
