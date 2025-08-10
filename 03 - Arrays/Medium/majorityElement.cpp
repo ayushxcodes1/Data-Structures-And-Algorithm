9d7da45ff837409c624c65195b36df6725e49210
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 17:49:52 +0530

    Initial Commit

--- 03 - Arrays/Medium/majorityElement.cpp
+++ 03 - Arrays/Medium/majorityElement.cpp
@@ -5,7 +5,15 @@ using namespace std;
 //Brute Force 
 int majorityElement(vector<int> arr) {
     int n = arr.size();
-
+    for(int i = 0; i < n; i++){
+        int cnt = 0;
+        for(int j = 0; j < n; j++){
+            if(arr[j] == arr[i]) {
+               cnt++;
+            }
+        } 
+        if(cnt > (n / 2)) return arr[i];
+    }
 }
 
 int main() {
