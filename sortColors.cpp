be3a721128ef173c2ee52d51e41e4852e8514dda
ayushporwal01 <porwalayush953@gmail.com>
2025-08-10 16:57:35 +0530

    Initial Commit

--- sortColors.cpp
+++ sortColors.cpp
@@ -32,7 +32,22 @@ vector<int> sortColors2(vector<int> &arr) {
 //Optimal - TC: O(N), SC: O(1)
 vector<int> sortColors3(vector<int> &arr) {
     int n = arr.size();
-    
+    int low = 0, mid = n / 2, high = n - 1;
+
+    while(mid <= high) {
+        if(arr[mid] == 0) {
+          swap(arr[low], arr[mid]);
+          low++; mid++;
+        }
+        else if(arr[mid] == 1) {
+          mid++;
+        }
+        else {
+           swap(arr[mid], arr[high]);
+           mid++; high--;
+        }
+    }
+    return arr;
 }
 
 
