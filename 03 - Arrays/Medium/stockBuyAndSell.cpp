03f39d971f87f750a36c123b45adbff0a512a02a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 11:21:27 +0530

    Initial Commit

--- 03 - Arrays/Medium/stockBuyAndSell.cpp
+++ 03 - Arrays/Medium/stockBuyAndSell.cpp
@@ -19,6 +19,7 @@ int stockBuyAndSell(vector<int> &arr) {
 
 int main() {
   vector<int> prices = {7, 1, 5, 3, 6, 4};
-  
+  int res = stockBuyAndSell(arr);
+  cout << "Maximum Profit: " << res;
   return 0;
 }
