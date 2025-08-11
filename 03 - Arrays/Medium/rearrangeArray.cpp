011a03055f981521f64ba9654bda2dab735d396e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 19:02:45 +0530

    Initial Commit

--- 03 - Arrays/Medium/rearrangeArray.cpp
+++ 03 - Arrays/Medium/rearrangeArray.cpp
@@ -23,6 +23,27 @@ vector<int> rearrangeArray(vector<int> arr) {
     return arr;
 }
 
+//Optimal - TC: O(N), SC: O(N)
+vector<int> rearrangeArray(vector<int> arr) {
+    int n = arr.size();
+    vector<int> pos;
+    vector<int> neg;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > 0) {
+           pos.push_back(arr[i]);
+        }
+        else {
+           neg.push_back(arr[i]);
+        }
+    }
+
+    for(int i = 0; i < n / 2; i++) {
+        arr[2 * i] = pos[i];
+        arr[2 * i + 1] = neg[i];
+    }
+    return arr;
+}
+
 int main() {
   vector<int> arr = {-3, -1, 2, -5, 4, 8};
   vector<int> res = rearrangeArray(arr);
