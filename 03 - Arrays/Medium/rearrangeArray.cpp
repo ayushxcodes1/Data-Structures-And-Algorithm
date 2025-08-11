4b2405ceaef5f5dc420c9e28abfa06b8beb9bb84
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 19:08:21 +0530

    Initial Commit

--- 03 - Arrays/Medium/rearrangeArray.cpp
+++ 03 - Arrays/Medium/rearrangeArray.cpp
@@ -16,7 +16,7 @@ vector<int> rearrangeArray(vector<int> arr) {
         }
     }
 
-    for(int i = 0; i < n / 2; i++) {
+    for(int i = 0; i < (n / 2); i++) {
         arr[2 * i] = pos[i];
         arr[2 * i + 1] = neg[i];
     }
