5ac2c4df125ad1ab340da69962dfb6cbb2d46fc4
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 18:00:42 +0530

    Initial Commit

---
+++ 03 - Arrays/Medium/rearrangeArray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> rearrangeArray(vector<int> arr) {
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+
+        }
+    }
+}
+
+int main() {
+  vector<int> arr = {-3, -1, 2, -5, 4, 8};
+  
+  return 0;
+}
