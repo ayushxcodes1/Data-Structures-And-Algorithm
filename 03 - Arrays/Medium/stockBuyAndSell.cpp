68696524b02627f4099d738a1c4965786fbfa8ec
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 11:20:25 +0530

    Initial Commit

--- 03 - Arrays/Medium/stockBuyAndSell.cpp
+++ 03 - Arrays/Medium/stockBuyAndSell.cpp
@@ -2,6 +2,21 @@
 
 using namespace std;
 
+int stockBuyAndSell(vector<int> &arr) {
+    int n = arr.size();
+    int maxProfit = 0;
+    int buyDay, sellDay;
+    for(int i = 0; i < n; i++) {
+        buyDay = i;
+        int profit = 0;
+        for(int j = i + 1; j < n - 1 ; j++) {
+            sellDay = j;
+            profit = arr[i] - arr[j];
+        }
+        maxProfit = max(profit, maxProfit);
+    }
+}
+
 int main() {
   vector<int> prices = {7, 1, 5, 3, 6, 4};
 
