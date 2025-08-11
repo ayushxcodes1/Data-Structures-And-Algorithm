66ed2b84738b301d47cd58a8f78aad48cf25b686
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 11:30:57 +0530

    Initial Commit

--- 03 - Arrays/Medium/stockBuyAndSell.cpp
+++ 03 - Arrays/Medium/stockBuyAndSell.cpp
@@ -14,6 +14,20 @@ int stockBuyAndSell(vector<int> &prices) {
     return maxProfit;
 }
 
+//Optimal - TC: O(N), SC: O(1)
+int stockBuyAndSell2(vector<int> &prices) {
+    int n = prices.size();
+    int maxProfit = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n - 1 ; j++) {
+            maxProfit = max(maxProfit, prices[j] - prices[i]);
+        }
+    }
+    return maxProfit;
+}
+
+
+
 int main() {
   vector<int> prices = {7, 1, 5, 3, 6, 4};
   int res = stockBuyAndSell(prices);
