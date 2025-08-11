d79a5904b8b285d1216b4daf8b841658f6434482
ayushporwal01 <porwalayush953@gmail.com>
2025-08-11 18:19:23 +0530

    Initial Commit

--- 03 - Arrays/Medium/rearrangeArray.cpp
+++ 03 - Arrays/Medium/rearrangeArray.cpp
@@ -6,7 +6,14 @@ vector<int> rearrangeArray(vector<int> arr) {
     int n = arr.size();
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
-            
+            //Negative number on Even Index
+            if(arr[i] < 0 && (i % 2 == 0)) {
+                if(arr[j] > 0) {
+                   swap(arr[i], arr[j]); 
+                   break;
+                }
+            }
+            else if(arr[i] > 0 && (i % 2 != 0))
         }
     }
 }
