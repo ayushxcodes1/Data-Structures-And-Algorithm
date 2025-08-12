f66ae42f6b839d35bd127a07db6955c95cc73607
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 11:18:07 +0530

    Initial Commit

--- 03 - Arrays/Medium/CntSubArrayWithSumK.cpp
+++ 03 - Arrays/Medium/CntSubArrayWithSumK.cpp
@@ -20,16 +20,16 @@ int cntSubArray(vector<int> &arr, int x) {
 
 //Better - TC: O(N^2), SC: O(1)
 int cntSubArray2(vector<int> &arr, int k) {
-    int n = arr.size();
-    int cnt = 0;
-    for(int i = 0; i < n; i++) {
-        int sum = 0;
-        for(int j = i; j < n; j++) {
-          sum += arr[j];
-          if(sum == k) cnt++;
-        }
+  int n = arr.size();
+  int cnt = 0;
+  for(int i = 0; i < n; i++) {
+    int sum = 0;
+    for(int j = i; j < n; j++) {
+      sum += arr[j];
+      if(sum == k) cnt++;
     }
-    return cnt; 
+  }
+  return cnt; 
 } 
 
 //Optimal - TC: O(N * logN), SC: O(1)
