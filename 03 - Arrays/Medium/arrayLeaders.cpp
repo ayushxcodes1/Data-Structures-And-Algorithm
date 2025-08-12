c21954ba61a075b756784a9ecd9bc4c58f09046f
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 17:59:50 +0530

    Initial Commit

--- 03 - Arrays/Medium/arrayLeaders.cpp
+++ 03 - Arrays/Medium/arrayLeaders.cpp
@@ -27,7 +27,9 @@ vector<int> arrayLeaders(vector<int> &arr) {
     vector<int> ans;
     int maxi = INT_MIN;
     for(int i = n - 1; i >= 0; i--) {
-       
+        if(arr[i] > maxi) {
+           ans.push_back(arr[i]);
+        }
     }
 }
 
