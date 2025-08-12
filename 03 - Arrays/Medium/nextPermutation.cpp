31c077a618e95920a4c9245369e044a8c5aa9045
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 13:41:09 +0530

    Initial Commit

--- 03 - Arrays/Medium/nextPermutation.cpp
+++ 03 - Arrays/Medium/nextPermutation.cpp
@@ -2,13 +2,15 @@
 
 using namespace std;
 
-//Brute Force - TC: O(N^2 * N!), SC: O(N * N!) - very slow
+//Brute Force - very slow for large numbers
 
 //Better - TC: O(N), SC: O(1) - Using STL
 vector<int> nextPermutation(vector<int> arr) {
     next_permutation(arr.begin(), arr.end());
     return arr;
 }
+
+//Optimal 
 
 
 int main() {
