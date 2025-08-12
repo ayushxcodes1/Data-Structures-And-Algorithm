745eebba6defc311e9cce63f426ee7e4f2fc9e16
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 16:15:08 +0530

    Initial Commit

--- 03 - Arrays/Medium/nextPermutation.cpp
+++ 03 - Arrays/Medium/nextPermutation.cpp
@@ -15,10 +15,10 @@ vector<int> nextPermutation(vector<int> arr) {
   int n = arr.size();
   int ind = -1;
   for(int i = n - 2; i >= 0; i--) {
-      if(arr[i] < arr[i + 1]) {
-        ind = i;
-        break;
-      }
+    if(arr[i] < arr[i + 1]) {
+      ind = i;
+      break;
+    }
   }
   if(ind == -1) {
     reverse(arr.begin(), arr.end());
