8e8dd8d80f9ab6178bb6162fdace0bcf8a4f0292
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 18:09:47 +0530

    Initial Commit

--- 03 - Arrays/Medium/leaders.cpp
+++ 03 - Arrays/Medium/leaders.cpp
@@ -23,18 +23,18 @@ vector<int> leaders(vector<int> &arr) {
 
 //Optimal - TC: O(N), SC: O(N)
 vector<int> leaders2(vector<int> &arr) {
-    int n = arr.size();
-    vector<int> ans;
-    int maxi = INT_MIN;
-    for(int i = n - 1; i >= 0; i--) {
-        if(arr[i] > maxi) {
-           ans.push_back(arr[i]);
-        }
-        maxi = max(maxi, arr[i]);
-    }
-    //if needs to return in original order - reverse: O(N)
-    //if needs to return in sorted order - sort: O(NlogN)
-    return ans;
+  int n = arr.size();
+  vector<int> ans;
+  int maxi = INT_MIN;
+  for(int i = n - 1; i >= 0; i--) {
+      if(arr[i] > maxi) {
+          ans.push_back(arr[i]);
+      }
+      maxi = max(maxi, arr[i]);
+  }
+  //if needs to return in original order - reverse: O(N)
+  //if needs to return in sorted order - sort: O(NlogN)
+  return ans;
 }
 
 int main() {
