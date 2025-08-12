db1870bd5de61d2fdc933e419b41e279244e111a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 13:16:26 +0530

    Initial Commit

--- 03 - Arrays/Medium/nextPermutation.cpp
+++ 03 - Arrays/Medium/nextPermutation.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
+vector<int> nextPermutation(vector<int> arr) {
+
+}
+
 int main() {
-  
+  vector<int> arr = {3, 2, 1};
+  vector<int> ans = nextPermutation(arr);
   return 0;
 }
