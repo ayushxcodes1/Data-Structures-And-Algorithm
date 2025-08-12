8d547cfa5e371c91e86171aea99b5fdb85e69aa0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 18:29:15 +0530

    Initial Commit

--- 03 - Arrays/Medium/leaders.cpp
+++ 03 - Arrays/Medium/leaders.cpp
@@ -27,7 +27,7 @@ vector<int> leaders2(vector<int> &arr) {
   vector<int> ans;
   int maxi = INT_MIN;
   for(int i = n - 1; i >= 0; i--) {
-    if(arr[i] > maxi) {
+    if(arr[i] >= maxi) {
       ans.push_back(arr[i]);
     }
     maxi = max(maxi, arr[i]);
