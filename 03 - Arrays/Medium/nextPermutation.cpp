571f33b6f3316b7b9995fdbe1ede91a98e180e38
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 16:09:04 +0530

    Initial Commit

--- 03 - Arrays/Medium/nextPermutation.cpp
+++ 03 - Arrays/Medium/nextPermutation.cpp
@@ -12,28 +12,28 @@ vector<int> nextPermutation(vector<int> arr) {
 
 //Optimal - TC: O(3N), SC: O(1)
 vector<int> nextPermutation(vector<int> arr) {
-    int n = arr.size();
-    int ind = -1;
-    for(int i = n - 2; i >= 0; i--) {
-        if(arr[i] < arr[i + 1]) {
-           ind = i;
-           break;
-        }
-    }
-    if(ind == -1) {
-       reverse(arr.begin(), arr.end());
-       return arr;
-    }
+  int n = arr.size();
+  int ind = -1;
+  for(int i = n - 2; i >= 0; i--) {
+      if(arr[i] < arr[i + 1]) {
+          ind = i;
+          break;
+      }
+  }
+  if(ind == -1) {
+      reverse(arr.begin(), arr.end());
+      return arr;
+  }
 
-    for(int i = n - 1; i > ind; i--) {
-        if(arr[i] < arr[ind]) {
-           swap(arr[i], arr[ind]);
-           break;
-        }
-    }
+  for(int i = n - 1; i > ind; i--) {
+      if(arr[i] < arr[ind]) {
+          swap(arr[i], arr[ind]);
+          break;
+      }
+  }
 
-    reverse(arr.begin() + ind + 1, arr.end());
-    return arr;
+  reverse(arr.begin() + ind + 1, arr.end());
+  return arr;
 }
 
 int main() {
