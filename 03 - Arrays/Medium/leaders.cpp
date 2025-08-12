25adcf2bb602711054ccaccb1e1cfaaff1c2e759
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 18:09:32 +0530

    Initial Commit

--- 03 - Arrays/Medium/leaders.cpp
+++ 03 - Arrays/Medium/leaders.cpp
@@ -4,21 +4,21 @@ using namespace std;
 
 //Brute Force - TC: O(N^2), SC: O(N)
 vector<int> leaders(vector<int> &arr) {
-    int n = arr.size();
-    vector<int> ans;
-    for(int i = 0; i < n; i++) {
-      bool leader = true;
-      for(int j = i + 1; j < n; j++) {
-        if(arr[j] > arr[i]) {
-          leader = false;
-          break;
-        }
-      }
-      if(leader) {
-        ans.push_back(arr[i]);
+  int n = arr.size();
+  vector<int> ans;
+  for(int i = 0; i < n; i++) {
+    bool leader = true;
+    for(int j = i + 1; j < n; j++) {
+      if(arr[j] > arr[i]) {
+        leader = false;
+        break;
       }
     }
-    return ans;
+    if(leader) {
+      ans.push_back(arr[i]);
+    }
+  }
+  return ans;
 }
 
 //Optimal - TC: O(N), SC: O(N)
