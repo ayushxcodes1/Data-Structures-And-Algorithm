213b9eec26c12253abe67fb5f627229aee8b2ab5
ayushporwal01 <porwalayush953@gmail.com>
2025-08-12 22:12:12 +0530

    Initial Commit

--- 03 - Arrays/Medium/longestConsecutiveSequence.cpp
+++ 03 - Arrays/Medium/longestConsecutiveSequence.cpp
@@ -2,13 +2,28 @@
 
 using namespace std;
 
+int ls(vector<int> arr, int num) {
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == num) {
+           return true;
+        }
+    }
+    return false;
+}
+
 int longestConsecutiveSequence(vector<int> &arr) {
     int n = arr.size();
     int longest = 1;
     for(int i = 0; i < n; i++) {
         int x = arr[i];
         int cnt = 1;
+        while(ls(arr, x + 1) == true) {
+            x = x + 1;
+            cnt = cnt + 1;
+        }
     }
+    return cnt;
 }
 
 int main() {
