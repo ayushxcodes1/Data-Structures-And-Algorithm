7f208a1718759da9a11d4dc1209510e4e9f2a774
ayushporwal01 <porwalayush953@gmail.com>
2025-08-13 09:27:13 +0530

    Initial Commit

--- 03 - Arrays/Medium/longestConsecutiveSequence.cpp
+++ 03 - Arrays/Medium/longestConsecutiveSequence.cpp
@@ -64,8 +64,8 @@ int longestConsecutiveSequence3(vector<int> &arr) {
 
     for(auto it : st) {
         if(st.find(it - 1) == st.end()) {
-            int cnt = 1;
             int x = it;
+            int cnt = 1;
             while(st.find(x + 1) != st.end()) {
                 x = x + 1;
                 cnt = cnt + 1;
