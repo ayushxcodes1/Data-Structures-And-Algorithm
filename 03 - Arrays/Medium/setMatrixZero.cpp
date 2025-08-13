03d759b0de7bd4019383c83ff95b504336fc5697
ayushporwal01 <porwalayush953@gmail.com>
2025-08-13 10:09:48 +0530

    Initial Commit

--- 03 - Arrays/Medium/setMatrixZero.cpp
+++ 03 - Arrays/Medium/setMatrixZero.cpp
@@ -5,10 +5,23 @@ using namespace std;
 vector<vector<int>> setMatrixZero(vector<vector<int>> &matrix) {
    int rows = matrix.size();
    int cols = matrix[0].size();
+   for(int i = 0; i < rows; i++) {
+    for(int j = 0; j < cols; j++) {
+        matrix[1][j] = 0;
+        matrix[i][1] = 0; 
+    }
+   }
 }
 
 int main() {
-  vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
 
+    setMatrixZero(matrix);
+    for (auto &rows : matrix) {
+        for (int val : rows) {
+            cout << val << " ";
+        }
+        cout << "\n";
+    }
   return 0;
 }
