9259a1000572600ae834bf6ab0c3c5703fc93d58
ayushporwal01 <porwalayush953@gmail.com>
2025-08-13 11:18:46 +0530

    Initial Commit

--- 03 - Arrays/Medium/setMatrixZero.cpp
+++ 03 - Arrays/Medium/setMatrixZero.cpp
@@ -49,8 +49,8 @@ void setMatrixZero(vector<vector<int>> &matrix) {
 void setMatrixZero(vector<vector<int>> &matrix) {
     int n = matrix.size();
     int m = matrix[0].size();
-    int col[n] = {0};
-    int row[m] = {0};
+    int col[m] = {0};
+    int row[n] = {0};
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             if(matrix[i][j] == 0) {
