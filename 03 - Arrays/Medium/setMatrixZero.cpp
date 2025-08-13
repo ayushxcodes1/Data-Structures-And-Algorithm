4ae02a831e56df01d66e2efcf2db31c0d34a8920
ayushporwal01 <porwalayush953@gmail.com>
2025-08-13 10:40:48 +0530

    Initial Commit

--- 03 - Arrays/Medium/setMatrixZero.cpp
+++ 03 - Arrays/Medium/setMatrixZero.cpp
@@ -20,16 +20,16 @@ void markCol(vector<vector<int>> &matrix, int cols, int i, int j) {
 
 //Brute Force - TC: O(N^2), SC: O(1)
 void setMatrixZero(vector<vector<int>> &matrix) {
-   int rows = matrix.size();
-   int cols = matrix[0].size();
-   for(int i = 0; i < rows; i++) {
-    for(int j = 0; j < cols; j++) {
-        if(matrix[i][j] = 0) {
-           markRow(matrix, i, j, rows);
-           markCol(matrix, i, j, cols);
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            if(matrix[i][j] = 0) {
+                markRow(matrix, i, j, rows);
+                markCol(matrix, i, j, cols);
+            }
         }
     }
-   }
 }
 
 int main() {
