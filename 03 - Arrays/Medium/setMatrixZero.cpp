0adc66cf119a7ffa3f1c0bc411e55b46ba139f12
ayushporwal01 <porwalayush953@gmail.com>
2025-08-13 11:17:41 +0530

    Initial Commit

--- 03 - Arrays/Medium/setMatrixZero.cpp
+++ 03 - Arrays/Medium/setMatrixZero.cpp
@@ -59,6 +59,15 @@ void setMatrixZero(vector<vector<int>> &matrix) {
             }
         }
     }
+    
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            if(matrix[i][j] == 0) {
+               row[i] = 1;
+               col[j] = 1;
+            }
+        }
+    }
 }
 
 int main() {
