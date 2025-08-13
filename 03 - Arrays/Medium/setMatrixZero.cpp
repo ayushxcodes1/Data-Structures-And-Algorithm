52c78232e5398945ef9df983407020e944bb4b2a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-13 11:32:55 +0530

    Initial Commit

--- 03 - Arrays/Medium/setMatrixZero.cpp
+++ 03 - Arrays/Medium/setMatrixZero.cpp
@@ -69,6 +69,30 @@ void setMatrixZero2(vector<vector<int>> &matrix) {
     }
 }
 
+//Optimal - TC: O(2*N*M)), SC: O(1)
+void setMatrixZero2(vector<vector<int>> &matrix) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int col[m] = {0};
+    int row[n] = {0};
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(matrix[i][j] == 0) {
+               row[i] = 1;
+               col[j] = 1;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            if(row[i] || col[j]) {
+               matrix[i][j] = 0;
+            }
+        }
+    }
+}
+
 int main() {
     vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
 
