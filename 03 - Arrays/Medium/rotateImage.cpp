ab3e763ec8c44fb7bb45d849e42103f14fdd6544
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 16:29:06 +0530

    Initial Commit

--- 03 - Arrays/Medium/rotateImage.cpp
+++ 03 - Arrays/Medium/rotateImage.cpp
@@ -19,7 +19,7 @@ void rotateImage2(vector<vector<int>> &mat) {
     int n = mat.size();
     for(int i = 0; i < n - 1; i++) {
         for(int j = i + 1; j < n; j++) {
-          
+            swap(mat[i][j], mat[j][i]);
         }
     }
 }
