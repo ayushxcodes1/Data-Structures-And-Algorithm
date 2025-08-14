905dddd731cfeae896f69a6e77f5bd8f700803d1
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 16:02:30 +0530

    Initial Commit

---
+++ 03 - Arrays/Medium/rotateImage.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Brute Force - TC: O(), SC: O()
+void rotateImage(vector<vector<int>> &mat) {
+    int rows = mat.size();
+    int cols = mat[0].size();
+}
+
+int main() {
+    vector<vector<int>> mat = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+     
+    return 0;
+}
