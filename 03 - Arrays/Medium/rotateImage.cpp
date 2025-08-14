86672eb66c4465580aad41ccfb6353ee5f906986
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 16:36:23 +0530

    Initial Commit

--- 03 - Arrays/Medium/rotateImage.cpp
+++ 03 - Arrays/Medium/rotateImage.cpp
@@ -18,14 +18,14 @@ vector<vector<int>> rotateImage(vector<vector<int>> &mat) {
 void rotateImage2(vector<vector<int>> &mat) {
     int n = mat.size();
 
-    //Transpose - O(N/2 * N/2)
+    //Transpose - O(N^2)
     for(int i = 0; i < n - 1; i++) {
         for(int j = i + 1; j < n; j++) {
             swap(mat[i][j], mat[j][i]);
         }
     }
 
-    //Reverse - O(N * N/2)
+    //Reverse - O(N^2)
     for(int i = 0; i < n; i++) {
         reverse(mat[i].begin(), mat[i].end());
     }
