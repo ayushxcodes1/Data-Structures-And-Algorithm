06bb9373a4f627938ecd5e9df1abc487106944c1
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 16:11:16 +0530

    Initial Commit

--- 03 - Arrays/Medium/rotateImage.cpp
+++ 03 - Arrays/Medium/rotateImage.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void rotateImage(vector<vector<int>> mat) {
     int n = mat.size();
     int m = mat[0].size();
-    vector<vector<int>> ans[n][m];
+    vector<vector<int>> ans(m, vector<int>(n));
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             ans[j][n - i - 1] = mat[i][j]; 
