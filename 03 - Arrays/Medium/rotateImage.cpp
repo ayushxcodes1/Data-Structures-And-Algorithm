eb23d7dfb839f4fa97c84eb77113a13c77558796
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 16:28:43 +0530

    Initial Commit

--- 03 - Arrays/Medium/rotateImage.cpp
+++ 03 - Arrays/Medium/rotateImage.cpp
@@ -17,8 +17,8 @@ vector<vector<int>> rotateImage(vector<vector<int>> &mat) {
 //Better - TC: O(N^2), SC: O(1)
 void rotateImage2(vector<vector<int>> &mat) {
     int n = mat.size();
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
           
         }
     }
