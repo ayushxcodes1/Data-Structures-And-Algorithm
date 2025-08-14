225035054f8109e08383ebbfe4242924f8771e3a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 18:48:37 +0530

    Initial Commit

--- 03 - Arrays/Medium/spiralMatrix.cpp
+++ 03 - Arrays/Medium/spiralMatrix.cpp
@@ -16,7 +16,7 @@ vector<int> spiralMatrix(vector<vector<int>> &mat) {
 
     //bottom
     for(int i = top; i <= bottom; i++) {
-        ans.push_back(mat[top][i]);
+        ans.push_back(mat[i][right]);
         top++;
     }
 
