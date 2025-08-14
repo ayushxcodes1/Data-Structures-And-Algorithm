7657490b16ceac1bab0a759ab5b45d8bfb9d1f34
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 19:03:38 +0530

    Initial Commit

--- 03 - Arrays/Medium/spiralMatrix.cpp
+++ 03 - Arrays/Medium/spiralMatrix.cpp
@@ -9,30 +9,31 @@ vector<int> spiralMatrix(vector<vector<int>> &mat) {
     int top = 0, bottom = n - 1;
     vector<int> ans;
 
-    //right
-    for(int i = left; i <= right; i++) {
-        ans.push_back(mat[top][i]);
+    while(top <= bottom && left <= right) {
+        //right
+        for(int i = left; i <= right; i++) {
+            ans.push_back(mat[top][i]);
+        }
+        top++;
+        
+        //bottom
+        for(int i = top; i <= bottom; i++) {
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+    
+        //left
+        for(int i = right; i >= left; i--) {
+            ans.push_back(mat[bottom][i]);
+        }
+        bottom--;
+    
+        //top
+        for(int i = bottom; i >= top; i--) {
+            ans.push_back(mat[i][left]);
+        }
+        left++;
     }
-    top++;
-
-    //bottom
-    for(int i = top; i <= bottom; i++) {
-        ans.push_back(mat[i][right]);
-    }
-    right--;
-
-    //left
-    for(int i = right; i >= left; i--) {
-        ans.push_back(mat[bottom][i]);
-    }
-    bottom--;
-
-    //top
-    for(int i = bottom; i >= top; i--) {
-        ans.push_back(mat[i][left]);
-    }
-    left++;
-
     return ans;
 }
 
