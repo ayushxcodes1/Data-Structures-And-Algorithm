0068ce84c9c76a1cfbbc58f2e952c9674884b025
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 19:09:10 +0530

    Initial Commit

--- 03 - Arrays/Medium/spiralMatrix.cpp
+++ 03 - Arrays/Medium/spiralMatrix.cpp
@@ -43,9 +43,9 @@ vector<int> spiralMatrix(vector<vector<int>> &mat) {
 
 int main() {
   vector<vector<int>> mat = {{1, 2, 3}, {8, 9, 4}, {7, 6, 5}};
-  vector<vector<int>> ans = spiralMatrix(mat);
+  vector<int> ans = spiralMatrix(mat);
   for(int i = 0; i < ans.size(); i++) {
-    
+      
   }
   return 0;
 }
