37d7218493d1543eecb2df20a181803eb744209b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-14 16:12:09 +0530

    Initial Commit

--- 03 - Arrays/Medium/rotateImage.cpp
+++ 03 - Arrays/Medium/rotateImage.cpp
@@ -16,6 +16,12 @@ void rotateImage(vector<vector<int>> mat) {
 
 int main() {
     vector<vector<int>> mat = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
-     
+    rotateImage(mat);
+    for (auto &rows : mat) {
+        for(int val : rows) {
+            cout << val << " ";
+        }
+        cout << "\n";
+    }
     return 0;
 }
