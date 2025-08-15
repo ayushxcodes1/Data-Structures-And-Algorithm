d8f49e11a894c12c4da221082a8f5589b8e8eaba
ayushporwal01 <porwalayush953@gmail.com>
2025-08-15 18:25:01 +0530

    Initial Commit

--- 03 - Arrays/Hard/majorityElementII.cpp
+++ 03 - Arrays/Hard/majorityElementII.cpp
@@ -6,9 +6,10 @@ vector<int> majorityElementII(vector<int> &arr) {
     int n = arr.size();
     vector<int> ans;
     for(int i = 0; i < n; i++) {
+        int cnt = 0;
         for(int j = i; j < n; j++) {
             if(arr[j] == arr[i]) {
-                
+               cnt++;
             }
         }
     }
