3d009aafad2be1d6155a3c5af707d13dc6219a9f
ayushporwal01 <porwalayush953@gmail.com>
2025-08-15 12:01:26 +0530

    Initial Commit

--- 03 - Arrays/Hard/pascalsTriangle.cpp
+++ 03 - Arrays/Hard/pascalsTriangle.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int nCr(int n, int r) {
     long long res = 1;
     for(int i = 0; i < r; i++) {
-        res = res * (n - 1);
+        res = res * (n - i);
         res = res / (i + 1);
     }
     return res;
