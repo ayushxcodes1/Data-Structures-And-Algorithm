61fdf4398289a5307ddd3656b98541cd77202bd0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-15 19:12:07 +0530

    Initial Commit

--- 03 - Arrays/Hard/majorityElementII.cpp
+++ 03 - Arrays/Hard/majorityElementII.cpp
@@ -28,6 +28,9 @@ vector<int> majorityElementII(vector<int> arr) {
     int n = arr.size();
     vector<int> ans;
     map<int, int> mpp;
+
+    int mini = (int)(n / 3) + 1;
+    
     for(int i = 0; i < n; i++) {
         mpp[arr[i]]++;
         if(mpp[arr[i]] > (n / 3)) {
