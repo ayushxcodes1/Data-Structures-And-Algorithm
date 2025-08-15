375e4067004d4e74ca0920e4d8eb9d9a83174fb5
ayushporwal01 <porwalayush953@gmail.com>
2025-08-15 18:24:42 +0530

    Initial Commit

--- 03 - Arrays/Hard/majorityElementII.cpp
+++ 03 - Arrays/Hard/majorityElementII.cpp
@@ -7,7 +7,9 @@ vector<int> majorityElementII(vector<int> &arr) {
     vector<int> ans;
     for(int i = 0; i < n; i++) {
         for(int j = i; j < n; j++) {
-            if()
+            if(arr[j] == arr[i]) {
+                
+            }
         }
     }
 }
