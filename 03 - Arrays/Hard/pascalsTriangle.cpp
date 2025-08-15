4ef07bc4e2c3eaa4c4c77042a0b5110addc5bb67
ayushporwal01 <porwalayush953@gmail.com>
2025-08-15 17:07:49 +0530

    Initial Commit

--- 03 - Arrays/Hard/pascalsTriangle.cpp
+++ 03 - Arrays/Hard/pascalsTriangle.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-/*Variation - 1: Given Row No. and Column No., print the element*/
+/*Variation - 1: Given Row No. and Column No., print the element at that place*/
 
 //Brute Force - TC: O(r), SC: O(1)
 int nCr(int n, int r) {
