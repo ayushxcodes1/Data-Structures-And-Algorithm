e647a425bb61dde37394408a5c36a8e9221da22a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 12:37:10 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 vector<vector<int>> threeSum(vector<int> arr) {
-   
+    int n = arr.size();
+    for(int i = 0; i < n - 2; i++) {
+        for(int j = i + 1; j < n - 1; j++) {
+            for(int k = j + 1; k < n; k++) {
+                
+            }
+        }
+    }
 }
 
 int main() {
