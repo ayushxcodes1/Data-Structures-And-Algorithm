99de4712c612c5cf178adc098350279d949c3fa0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 14:08:23 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -5,30 +5,6 @@ using namespace std;
 //Brute Force - TC: O(N^3), SC: O(N^3)
 vector<vector<int>> threeSum(vector<int> arr) {
     int n = arr.size();
-    vector<vector<int>> ans;
-
-    for(int i = 0; i < n - 2; i++) {
-        for(int j = i + 1; j < n - 1; j++) {
-            for(int k = j + 1; k < n; k++) {
-                long long sum = arr[i] + arr[j] + arr[k];
-
-                if(sum == 0) {
-                    vector<int> triplet = {arr[i], arr[j], arr[k]};
-                    sort(triplet.begin(), triplet.end()); 
-                    if(find(ans.begin(), ans.end(), triplet) == ans.end()) {
-                       ans.push_back(triplet);
-                    }
-                }
-            }
-        }
-    }
-
-    return ans;
-}
-
-//Better - TC: O(N^3), SC: O(N^3)
-vector<vector<int>> threeSum2(vector<int> arr) {
-    int n = arr.size();
     set<vector<int>> st;
 
     for(int i = 0; i < n - 2; i++) {
