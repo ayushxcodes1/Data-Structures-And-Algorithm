c544b0a8d29ea940053f227eb79a03656003ec2f
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 14:21:21 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -22,6 +22,26 @@ vector<vector<int>> threeSum(vector<int> arr) {
     return ans;
 }
 
+//Brute Force - TC: O(N^3 * log(No. of unique triplets)), SC: O(2 * (No. of unique triplets)
+vector<vector<int>> threeSum2(vector<int> arr) {
+    int n = arr.size();
+    set<vector<int>> st;
+
+    for(int i = 0; i < n; i++) {
+        set<int> hashset;
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] + arr[k] == 0) {
+                vector<int> temp = {arr[i], arr[j], arr[k]};
+                sort(temp.begin(), temp.end()); 
+                st.insert(temp);
+            }
+
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
 int main() {
   vector<int> arr = {-1, 0, 1, 2, -1, 4};
   vector<vector<int>> ans = threeSum(arr);
