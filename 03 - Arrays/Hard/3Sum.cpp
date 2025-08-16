cbfcbd0eb509c5a2f6b375696d7ea5db4dc44d02
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 12:51:38 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -22,5 +22,8 @@ vector<vector<int>> threeSum(vector<int> arr) {
 int main() {
   vector<int> arr = {-1, 0, 1, 2, -1, 4};
   vector<vector<int>> ans = threeSum(arr);
+  for(auto it : ans) {
+
+  }
   return 0;
 }
