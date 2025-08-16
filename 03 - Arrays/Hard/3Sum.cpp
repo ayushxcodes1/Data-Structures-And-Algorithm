c76d8ee9b4af41f1ac8b6f29a14fea2d2e31af7e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 14:08:50 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -7,8 +7,8 @@ vector<vector<int>> threeSum(vector<int> arr) {
     int n = arr.size();
     set<vector<int>> st;
 
-    for(int i = 0; i < n - 2; i++) {
-        for(int j = i + 1; j < n - 1; j++) {
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
             for(int k = j + 1; k < n; k++) {
                 long long sum = arr[i] + arr[j] + arr[k];
 
