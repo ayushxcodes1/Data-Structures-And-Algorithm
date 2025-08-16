ccedd73ea2f27d263ba4d08abcd29c6fcc432326
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 13:30:30 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -13,6 +13,7 @@ vector<vector<int>> threeSum(vector<int> arr) {
 
                 if(sum == 0) {
                    vector<int> triplet = {arr[i], arr[j], arr[k]};
+                   sort(triplet.begin(), triplet.end());
                 }
             }
         }
