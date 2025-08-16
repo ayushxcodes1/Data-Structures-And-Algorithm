0eb61e6494b8d4112dcab24ac8257e0b6ceafd14
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 13:45:27 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -26,6 +26,30 @@ vector<vector<int>> threeSum(vector<int> arr) {
     return ans;
 }
 
+//Better - TC: O(N^3), SC: O(N^3)
+vector<vector<int>> threeSum(vector<int> arr) {
+    int n = arr.size();
+    vector<vector<int>> ans;
+
+    for(int i = 0; i < n - 2; i++) {
+        for(int j = i + 1; j < n - 1; j++) {
+            for(int k = j + 1; k < n; k++) {
+                long long sum = arr[i] + arr[j] + arr[k];
+
+                if(sum == 0) {
+                    vector<int> triplet = {arr[i], arr[j], arr[k]};
+                    sort(triplet.begin(), triplet.end()); 
+                    if(find(ans.begin(), ans.end(), triplet) == ans.end()) {
+                       ans.push_back(triplet);
+                    }
+                }
+            }
+        }
+    }
+
+    return ans;
+}
+
 int main() {
   vector<int> arr = {-1, 0, 1, 2, -1, 4};
   vector<vector<int>> ans = threeSum(arr);
