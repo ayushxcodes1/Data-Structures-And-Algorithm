517222b998763fcf97d1b16bb1a44642fe9af931
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 12:38:25 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -7,7 +7,9 @@ vector<vector<int>> threeSum(vector<int> arr) {
     for(int i = 0; i < n - 2; i++) {
         for(int j = i + 1; j < n - 1; j++) {
             for(int k = j + 1; k < n; k++) {
-                
+                if(arr[i] + arr[j] + arr[k] == 0) {
+                   ans.push_back(arr[i], arr[j], arr[k]);
+                }
             }
         }
     }
