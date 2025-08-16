32084a46e5e826dabc4c0ee9438ea739286d09f7
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 17:08:21 +0530

    Initial Commit

--- 03 - Arrays/Hard/4Sum.cpp
+++ 03 - Arrays/Hard/4Sum.cpp
@@ -10,10 +10,12 @@ vector<vector<int>> threeSum(vector<int> arr) {
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
             for(int k = j + 1; k < n; k++) {
-                if(arr[i] + arr[j] + arr[k] == 0) {
-                    vector<int> temp = {arr[i], arr[j], arr[k]};
-                    sort(temp.begin(), temp.end()); 
-                    st.insert(temp);
+                for(int l = k + 1; l < n; l++) {
+                    if(arr[i] + arr[j] + arr[k] + arr[l] == 0) {
+                        vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                        sort(temp.begin(), temp.end()); 
+                        st.insert(temp);
+                    }
                 }
             }
         }
