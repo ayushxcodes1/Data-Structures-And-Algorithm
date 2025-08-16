b4427fff9b55bcec6d21f15ff85812ee63ce642d
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 17:20:19 +0530

    Initial Commit

--- 03 - Arrays/Hard/4Sum.cpp
+++ 03 - Arrays/Hard/4Sum.cpp
@@ -27,7 +27,7 @@ vector<vector<int>> fourSum(vector<int> arr, int target) {
 int main() {
   vector<int> arr = {-1, 0, -1, 0, -2, 2};
 
-  vector<vector<int>> ans = fourSum(arr);
+  vector<vector<int>> ans = fourSum(arr, 0);
   for(auto it : ans) {
     for(auto ele : it) {
         cout << ele << " ";
