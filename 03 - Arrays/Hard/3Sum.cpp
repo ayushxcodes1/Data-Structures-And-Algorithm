621020225b6b08db15524ee0fe2889bedcc52b3a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 16:38:15 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -66,6 +66,7 @@ vector<vector<int>> threeSum2(vector<int> arr) {
                j++;
                k--;
                while(j < k && arr[j] == arr[j - 1]) j++;
+               while(j < k && arr[k] == arr[k + 1]) k--;
             }
         }
     }
