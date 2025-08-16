bae6f7376dc438009acc631d0c728c9951a1a68c
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 17:50:47 +0530

    Initial Commit

--- 03 - Arrays/Hard/4Sum.cpp
+++ 03 - Arrays/Hard/4Sum.cpp
@@ -35,15 +35,13 @@ vector<vector<int>> fourSum(vector<int> arr, int target) {
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
             for(int k = j + 1; k < n; k++) {
-                for(int l = k + 1; l < n; l++) {
-                    long long sum = (long long)arr[i] + arr[j];
-                    sum += arr[k];
-                    sum += arr[l];
-                    if(sum == target) {
-                        vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
-                        sort(temp.begin(), temp.end()); 
-                        st.insert(temp);
-                    }
+                long long sum = (long long)arr[i] + arr[j];
+                sum += arr[k];
+                sum += arr[l];
+                if(sum == target) {
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    sort(temp.begin(), temp.end()); 
+                    st.insert(temp);
                 }
             }
         }
