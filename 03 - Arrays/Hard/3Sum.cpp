19c31596b58db26610c62839471a94c044027675
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 16:57:50 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -51,7 +51,7 @@ vector<vector<int>> threeSum3(vector<int> arr) {
     for(int i = 0; i < n; i++) {
         if(i > 0 && arr[i] == arr[i - 1]) continue;
         int j = i + 1;
-        int k = j + 1;
+        int k = n - 1;
         while(j < k) {
             int sum = arr[i] + arr[j] + arr[k];
             if(sum < 0) {
