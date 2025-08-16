c14a8b557fbb14b33e5c4794e4ad4c1638c8fe60
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 17:35:33 +0530

    Initial Commit

--- 03 - Arrays/Hard/4Sum.cpp
+++ 03 - Arrays/Hard/4Sum.cpp
@@ -27,6 +27,31 @@ vector<vector<int>> fourSum(vector<int> arr, int target) {
     return ans;
 }
 
+//Better - TC: O(N^3), SC: O(no. of quadruplets)
+vector<vector<int>> fourSum(vector<int> arr, int target) {
+    int n = arr.size();
+    set<vector<int>> st;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = i + 1; j < n; j++) {
+            for(int k = j + 1; k < n; k++) {
+                for(int l = k + 1; l < n; l++) {
+                    long long sum = (long long)arr[i] + arr[j];
+                    sum += arr[k];
+                    sum += arr[l];
+                    if(sum == target) {
+                        vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                        sort(temp.begin(), temp.end()); 
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
 int main() {
   vector<int> arr = {-1, 0, -1, 0, -2, 2};
 
