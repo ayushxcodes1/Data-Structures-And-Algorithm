861bd235bc64684715b3665d36e16a3c5b11717d
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 18:28:13 +0530

    Initial Commit

--- 03 - Arrays/Hard/4Sum.cpp
+++ 03 - Arrays/Hard/4Sum.cpp
@@ -58,24 +58,27 @@ vector<vector<int>> fourSum3(vector<int> arr, int target) {
     sort(arr.begin(), arr.end());
     for(int i = 0; i < n; i++) {
         if(i > 0 && arr[i] == arr[i - 1]) continue;
-        int j = i + 1;
-        int k = n - 1;
-        while(j < k) {
-            int sum = arr[i] + arr[j] + arr[k];
-            if(sum < 0) {
-               j++;
-            }
-            else if(sum > 0) {
-              k--;
-            }
-            else {
-               vector<int> temp = {arr[i], arr[j], arr[k]};
-               ans.push_back(temp);
-               j++;
-               k--;
-               while(j < k && arr[j] == arr[j - 1]) j++;
-               while(j < k && arr[k] == arr[k + 1]) k--;
-            }
+        for(int j = i + 1; j < n; j++) {
+            if(j != i + 1 && arr[j] == arr[j - 1]) continue;
+            int k = j + 1;
+            int l = n - 1;
+            while(j < k) {
+                int sum = arr[i] + arr[j] + arr[k];
+                if(sum < 0) {
+                   j++;
+                }
+                else if(sum > 0) {
+                  k--;
+                }
+                else {
+                   vector<int> temp = {arr[i], arr[j], arr[k]};
+                   ans.push_back(temp);
+                   j++;
+                   k--;
+                   while(j < k && arr[j] == arr[j - 1]) j++;
+                   while(j < k && arr[k] == arr[k + 1]) k--;
+                }
+        }
         }
     }
     return ans;
