e84ccc80aaca7f62ad2d8def546da8228bf0c608
ayushporwal01 <porwalayush953@gmail.com>
2025-08-16 13:18:23 +0530

    Initial Commit

--- 03 - Arrays/Hard/3Sum.cpp
+++ 03 - Arrays/Hard/3Sum.cpp
@@ -8,10 +8,9 @@ vector<vector<int>> threeSum(vector<int> arr) {
 
     for(int i = 0; i < n - 2; i++) {
         for(int j = i + 1; j < n - 1; j++) {
-            long long sum = 0;
             vector<int> triplet;
             for(int k = j + 1; k < n; k++) {
-                sum = arr[i] + arr[j] + arr[k];
+                long long sum = arr[i] + arr[j] + arr[k];
                 triplet.insert(triplet.end(), {arr[i], arr[j], arr[k]});
                 sort(triplet.begin(), triplet.end());
                 if(sum == 0 && (find(ans.begin(), ans.end(), triplet) == ans.end())) {
