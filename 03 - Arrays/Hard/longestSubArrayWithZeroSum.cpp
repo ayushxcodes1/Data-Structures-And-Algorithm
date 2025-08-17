11f014322ab16f2674521f15aa506a76f7b45686
ayushporwal01 <porwalayush953@gmail.com>
2025-08-17 18:20:36 +0530

    Initial Commit

--- 03 - Arrays/Hard/longestSubArrayWithZeroSum.cpp
+++ 03 - Arrays/Hard/longestSubArrayWithZeroSum.cpp
@@ -19,7 +19,20 @@ int maxLength(vector<int> &arr) {
 }
 
 //Better 
-
+int maxLength2(vector<int> &arr) {
+    int n = arr.size();
+    int maxLen = 0;
+    for(int i = 0; i < n; i++) {
+        int sum = 0;
+        for(int j = i; j < n; j++) {
+            sum += arr[j];
+            if(sum == 0) {
+               maxLen = max(maxLen, j - i + 1);
+            }
+        }
+    }
+    return maxLen;
+}
 
 int main() {
   vector<int> arr = {15, -2, 2, -8, 1, 7, 10, 23};
