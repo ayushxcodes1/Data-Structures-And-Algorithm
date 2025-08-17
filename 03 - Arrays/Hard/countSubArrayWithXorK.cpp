adc419cff3113908b086b67a745965178ed68074
ayushporwal01 <porwalayush953@gmail.com>
2025-08-17 19:10:27 +0530

    Initial Commit

--- 03 - Arrays/Hard/countSubArrayWithXorK.cpp
+++ 03 - Arrays/Hard/countSubArrayWithXorK.cpp
@@ -7,7 +7,7 @@ int subarrayXor(vector<int> &arr, int k) {
     int n = arr.size();
     for(int i = 0; i < n; i++) {
         int xorr = 0;
-        for(int j = i + 1; j < n; j++) {
+        for(int j = i; j < n; j++) {
             
         }
     }
