487d614b05d518ed39e351248d64a728e52e0f70
ayushporwal01 <porwalayush953@gmail.com>
2025-08-17 19:04:52 +0530

    Initial Commit

--- 03 - Arrays/Hard/countSubArrayWithXorK.cpp
+++ 03 - Arrays/Hard/countSubArrayWithXorK.cpp
@@ -2,12 +2,16 @@
 
 using namespace std;
 
+int subarrayXor(vector<int> &arr, int k) {
+    
+}
+
 int main() {
   vector<int> arr = {1, 1, 1, 1};
 
   int ans = subarrayXor(arr, 0);
   cout << ans;
 
-  
+
   return 0;
 }
