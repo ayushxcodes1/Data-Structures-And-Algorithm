81d4b208883115751fd446b1081e880a0ec5b78f
ayushporwal01 <porwalayush953@gmail.com>
2025-08-17 19:13:28 +0530

    Initial Commit

--- 03 - Arrays/Hard/countSubArrayWithXorK.cpp
+++ 03 - Arrays/Hard/countSubArrayWithXorK.cpp
@@ -5,10 +5,14 @@ using namespace std;
 //Brute Force
 int subarrayXor(vector<int> &arr, int k) {
     int n = arr.size();
+    int cnt = 0;
     for(int i = 0; i < n; i++) {
         int xorr = arr[i];
         for(int j = i + 1; j < n; j++) {
             xorr = xorr ^ arr[j];
+            if(xorr == k) {
+               cnt++;
+            }
         }
     }
 }
