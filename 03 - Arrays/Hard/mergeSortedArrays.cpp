20f77a95bd9751f5e3cc907d42768871f19f83cd
ayushporwal01 <porwalayush953@gmail.com>
2025-08-18 10:31:39 +0530

    Initial Commit

---
+++ 03 - Arrays/Hard/mergeSortedArrays.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<int> merge(vector<int> arr, int m, int n) {
+
+}
+
+int main() {
+  vector<int> arr = {1, 2, 3, 0, 0, 0};
+    
+  return 0;
+}
