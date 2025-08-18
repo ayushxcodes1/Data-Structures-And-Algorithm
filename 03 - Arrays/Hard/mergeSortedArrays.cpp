70c52ff4bb2510cd93418ecdf0896a438d714f1e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-18 10:32:57 +0530

    Initial Commit

--- 03 - Arrays/Hard/mergeSortedArrays.cpp
+++ 03 - Arrays/Hard/mergeSortedArrays.cpp
@@ -8,6 +8,9 @@ vector<int> merge(vector<int> arr, int m, int n) {
 
 int main() {
   vector<int> arr = {1, 2, 3, 0, 0, 0};
-    
+  vector<int> arr2 = {2, 5, 6};
+  int m = 6, n = 3;
+
+  
   return 0;
 }
