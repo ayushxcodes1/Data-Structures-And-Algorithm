105440e0c33cc14d974c9927f726fa030e0f36db
ayushporwal01 <porwalayush953@gmail.com>
2025-08-18 10:50:57 +0530

    Initial Commit

--- 03 - Arrays/Hard/mergeSortedArrays.cpp
+++ 03 - Arrays/Hard/mergeSortedArrays.cpp
@@ -3,7 +3,19 @@
 using namespace std;
 
 vector<int> merge(vector<int> &arr, vector<int> arr2, int m, int n) {
-   
+    int p1 = arr[0];
+    int p2 = arr2[0];
+    vector<int> ans(m + n, 0);
+    for(int i = 0; i < m; i++) {
+        ans.push_back(arr[i]);
+    }
+
+    for(int i = 0; i < n; i++) {
+        ans.push_back(arr2[i]);
+    }
+
+    sort(ans.begin(), ans.end());
+    return ans;
 }
 
 int main() {
