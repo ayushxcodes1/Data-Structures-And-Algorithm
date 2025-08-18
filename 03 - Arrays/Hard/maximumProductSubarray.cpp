0a4f272a56ca7a9b5f94d47d9c6f7f62ed5f6bf0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-18 09:20:03 +0530

    Initial Commit

--- 03 - Arrays/Hard/maximumProductSubarray.cpp
+++ 03 - Arrays/Hard/maximumProductSubarray.cpp
@@ -18,10 +18,8 @@ vector<int> maxProduct(vector<int> &arr) {
 
 int main() {
   vector<int> arr = {2, 3, -2, 4};
-  vector<int> ans = maxProduct(arr);
-  for(auto it : ans) {
-    cout << it << " ";
-  }
+  int ans = maxProduct(arr);
+  cout << ans << " ";
 
   return 0;
 }
