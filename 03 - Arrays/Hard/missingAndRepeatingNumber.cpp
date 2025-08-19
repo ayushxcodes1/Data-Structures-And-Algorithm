61a6292f4f9e8464625f187de8918561f19ff595
ayushporwal01 <porwalayush953@gmail.com>
2025-08-19 12:13:36 +0530

    Initial Commit

--- 03 - Arrays/Hard/missingAndRepeatingNumber.cpp
+++ 03 - Arrays/Hard/missingAndRepeatingNumber.cpp
@@ -2,6 +2,27 @@
 
 using namespace std;
 
+//Brute - TC: O(N^2), SC: O(1)  
+vector<int> findTwoElement(vector<int> &arr) {
+    int n = arr.size();
+    int repeating = -1, missing = -1;
+
+    for(int i = 1; i < n; i++) {
+        int cnt = 0; 
+        for(int j = 0; j < n; j++) {
+            if(arr[j] == i) {
+               cnt++;
+            }
+            if(cnt == 2) repeating = i;
+            else if(cnt == 0) missing = i;
+        }
+        if(repeating != -1 && missing != -1) {
+           break;
+        }
+    }
+
+    return {repeating, missing};
+}
 //Brute Force - TC: O(NlogN + N), SC: O(1)
 vector<int> findTwoElement(vector<int> &arr) {
     sort(arr.begin(), arr.end()); //O(NlogN)
@@ -40,27 +61,7 @@ vector<int> findTwoElement(vector<int> &arr) {
 }
 
 
-//Brute - TC: O(N^2), SC: O(1)  
-vector<int> findTwoElement(vector<int> &arr) {
-    int n = arr.size();
-    int repeating = -1, missing = -1;
 
-    for(int i = 1; i < n; i++) {
-        int cnt = 0; 
-        for(int j = 0; j < n; j++) {
-            if(arr[j] == i) {
-               cnt++;
-            }
-            if(cnt == 2) repeating = i;
-            else if(cnt == 0) missing = i;
-        }
-        if(repeating != -1 && missing != -1) {
-           break;
-        }
-    }
-
-    return {repeating, missing};
-}
 
 
 int main() {
