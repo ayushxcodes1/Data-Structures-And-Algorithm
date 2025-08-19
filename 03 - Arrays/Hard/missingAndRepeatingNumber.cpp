436e33aa15480f3258a4c265541815206f5bdef0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-19 11:01:54 +0530

    Initial Commit

--- 03 - Arrays/Hard/missingAndRepeatingNumber.cpp
+++ 03 - Arrays/Hard/missingAndRepeatingNumber.cpp
@@ -10,9 +10,9 @@ vector<int> findTwoElement(vector<int> &arr) {
     sort(arr.begin(), arr.end());
 
     for(int i = 1; i < n; i++) {
-       if(arr[i] == arr[i - 1]) {
-          
-       }
+        if(arr[i] == arr[i - 1]) {
+           repeating = arr[i];
+        }
     }
 }
 
