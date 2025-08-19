ad12e0810895edd626a45ffe57e80b2c562ba35f
ayushporwal01 <porwalayush953@gmail.com>
2025-08-19 11:18:51 +0530

    Initial Commit

--- 03 - Arrays/Hard/missingAndRepeatingNumber.cpp
+++ 03 - Arrays/Hard/missingAndRepeatingNumber.cpp
@@ -2,21 +2,22 @@
 
 using namespace std;
 
+//Brute Force - TC: O(NlogN + 2N)
 vector<int> findTwoElement(vector<int> &arr) {
     int n = arr.size();
     int repeating = -1;
     int missing = -1;
 
-    sort(arr.begin(), arr.end()); 
+    sort(arr.begin(), arr.end()); //O(NlogN)
 
-    //Repeating
+    //Repeating - //O(N)
     for(int i = 1; i < n; i++) {
         if(arr[i] == arr[i - 1]) {
            repeating = arr[i];
         }
     }
 
-    //Missing
+    //Missing - O(N)
     for(int i = 0; i < n - 1; i++) {
         if(arr[i] + 1 != arr[i + 1]) {
            missing = arr[i] + 1;
