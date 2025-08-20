317d93e5a51ee7e4081b1dee7d80580c1bb546c7
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 15:33:41 +0530

    Initial Commit

--- 03 - Arrays/Hard/countInversions.cpp
+++ 03 - Arrays/Hard/countInversions.cpp
@@ -16,6 +16,45 @@ int countInversions(vector<int> &arr) {
     return cnt;
 }
 
+//Merge Sort
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    while(left <= mid && right <= high) {
+        if(arr[left] <= arr[right]) {
+           temp.push_back(arr[left]);
+           left++;
+        }
+        else {
+           temp.push_back(arr[right]);
+           right++;
+        }
+    }
+
+    while(left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while(right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for(int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mS(vector<int> &arr, int low, int high) {
+    if(low == high) return;
+    int mid = (low + high) / 2;
+    mS(arr, low, mid);  
+    mS(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
 //Optimal - TC: O(NlogN), SC: O(1) - Merge Sort Approach
 int countInversions(vector<int> &arr) {
     int n = arr.size();
