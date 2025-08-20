a31446536606abf3cf76642fd12dfd59d5873c5a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 21:09:56 +0530

    Initial Commit

--- 03 - Arrays/Hard/reversePairs.cpp
+++ 03 - Arrays/Hard/reversePairs.cpp
@@ -51,6 +51,9 @@ void merge(vector<int> &arr, int low, int mid, int high) {
 
 void countPairs(vector<int> &arr, int low, int mid, int high) {
     int right = mid + 1;
+    for(int i = low; i <= mid; i++) {
+ 
+    }
 }
 
 void mS(vector<int> &arr, int low, int high) {
