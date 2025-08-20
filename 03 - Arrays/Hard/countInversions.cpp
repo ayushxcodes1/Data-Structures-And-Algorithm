16453cd12a58e3a5cee968d2a8064dd273f6fe3a
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 15:30:55 +0530

    Initial Commit

--- 03 - Arrays/Hard/countInversions.cpp
+++ 03 - Arrays/Hard/countInversions.cpp
@@ -16,6 +16,20 @@ int countInversions(vector<int> &arr) {
     return cnt;
 }
 
+//Optimal - TC: O(N^2), SC: O(1) - Merge Sort Approach
+int countInversions(vector<int> &arr) {
+    int n = arr.size();
+    int cnt = 0;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] > arr[j]) {
+               cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
 int main() {
   vector<int> arr = {2, 4, 1, 3, 5};
   return 0;
