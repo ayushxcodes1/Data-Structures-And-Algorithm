5eef6e4c745f7a0b7bce44259a2afcf48bdb67e8
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 20:43:11 +0530

    Initial Commit

--- 03 - Arrays/Hard/reversePairs.cpp
+++ 03 - Arrays/Hard/reversePairs.cpp
@@ -3,12 +3,12 @@
 using namespace std;
 
 //Brute Force - TC: O(N^2), SC: O(1)
-int countInversions(vector<int> &arr) {
+int reversePairs(vector<int> &arr) {
     int n = arr.size();
     int cnt = 0;
     for(int i = 0; i < n - 1; i++) {
         for(int j = i + 1; j < n; j++) {
-            if(arr[i] > arr[j]) {
+            if(arr[i] > 2 * arr[j]) {
                cnt++;
             }
         }
