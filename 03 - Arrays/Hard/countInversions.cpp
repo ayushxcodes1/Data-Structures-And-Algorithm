6a5b1e0d300a6d94595e6147ebee72f94844ae05
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 15:06:07 +0530

    Initial Commit

--- 03 - Arrays/Hard/countInversions.cpp
+++ 03 - Arrays/Hard/countInversions.cpp
@@ -6,7 +6,9 @@ int countInversions(vector<int> &arr) {
     int n = arr.size();
     int cnt = 0;
     for(int i = 0; i < n; i++) {
-    
+        for(int j = i + 1; j < n; j++) {
+            
+        }
     }
 }
 
