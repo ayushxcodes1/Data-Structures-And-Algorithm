ea82e7e26bc213fb46f6ba4701eea0a05b2e0e80
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 20:45:54 +0530

    Initial Commit

--- 03 - Arrays/Hard/reversePairs.cpp
+++ 03 - Arrays/Hard/reversePairs.cpp
@@ -17,10 +17,27 @@ int reversePairs(vector<int> &arr) {
 }
 
 //Optimal - TC: O(NlogN + N), SC: O(N)
+int reversePairs2(vector<int> &arr) {
+    int n = arr.size();
+    int cnt = 0;
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] > 2 * arr[j]) {
+               cnt++;
+            }
+        }
+    }
+    return cnt;
+}
 
 int main() {
   vector<int> arr = {40, 25, 19, 12, 9, 6, 2};
 
+  int ans = reversePairs(arr);
+  cout << ans;
+
+  cout << "\n";
+  
   int ans = reversePairs(arr);
   cout << ans;
 
