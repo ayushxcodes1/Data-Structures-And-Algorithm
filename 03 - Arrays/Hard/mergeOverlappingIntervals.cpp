763d06bf9aef835e326a70dc560cf30b04234a79
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 17:16:35 +0530

    Initial Commit

--- 03 - Arrays/Hard/mergeOverlappingIntervals.cpp
+++ 03 - Arrays/Hard/mergeOverlappingIntervals.cpp
@@ -9,6 +9,18 @@ vector<vector<int>> mergeIntervals(vector<vector<int>> &arr) {
     for(int i = 0; i < n; i++) {
         int start = arr[i][0];
         int end = arr[i][1];
+        if(!ans.empty() && end <= ans.back()[1]) {
+           continue;
+        }
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j][0] <= end) {
+               end = max(end, arr[j][1]); 
+            }
+            else {
+                break;
+            }
+        }
+        ans.push_back({start, end});
     }
     return ans;
 }
