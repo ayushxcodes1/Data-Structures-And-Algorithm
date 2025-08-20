2d848694513425def23d1e93fbc5bd64fb5e6bf8
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 17:29:11 +0530

    Initial Commit

--- 03 - Arrays/Hard/mergeOverlappingIntervals.cpp
+++ 03 - Arrays/Hard/mergeOverlappingIntervals.cpp
@@ -10,9 +10,13 @@ vector<vector<int>> mergeIntervals(vector<vector<int>> &arr) {
     for(int i = 0; i < n; i++) {
         int start = arr[i][0];
         int end = arr[i][1];
+
+        // Step 2: If current interval is inside the last merged interval, skip it
         if(!ans.empty() && end <= ans.back()[1]) {
            continue;
         }
+
+        // Step 3: Merge with overlapping intervals
         for(int j = i + 1; j < n; j++) {
             if(arr[j][0] <= end) {
                end = max(end, arr[j][1]); 
@@ -28,7 +32,7 @@ vector<vector<int>> mergeIntervals(vector<vector<int>> &arr) {
 
 int main() {
     vector<vector<int>> arr = {{1,3}, {2,6}, {8,10}, {15,18}};
-    
+
     vector<vector<int>> ans = mergeIntervals(arr);
     for(auto it : ans) {
         cout << "[";
