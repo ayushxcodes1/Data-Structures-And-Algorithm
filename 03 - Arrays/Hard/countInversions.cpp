b03d8b3e0c908bed48b96818720c804fffd54556
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 15:07:01 +0530

    Initial Commit

--- 03 - Arrays/Hard/countInversions.cpp
+++ 03 - Arrays/Hard/countInversions.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int countInversions(vector<int> &arr) {
     int n = arr.size();
     int cnt = 0;
-    for(int i = 0; i < n; i++) {
+    for(int i = 0; i < n - 2; i++) {
         for(int j = i + 1; j < n; j++) {
-            
+
         }
     }
 }
