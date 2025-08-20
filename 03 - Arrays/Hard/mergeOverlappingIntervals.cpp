9fee8425d35530c3d12bcb68e20f6c29ad3eed87
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 18:02:52 +0530

    Initial Commit

--- 03 - Arrays/Hard/mergeOverlappingIntervals.cpp
+++ 03 - Arrays/Hard/mergeOverlappingIntervals.cpp
@@ -30,7 +30,7 @@ vector<vector<int>> mergeIntervals(vector<vector<int>> &arr) {
     return ans;
 }
 
-//Brute Force - TC: O(NlogN + 2N), SC: O(N)
+//Brute Force - TC: O(NlogN), SC: O(N)
 vector<vector<int>> mergeIntervals(vector<vector<int>> &arr) {
     int n = arr.size();
     sort(arr.begin(), arr.end());  
