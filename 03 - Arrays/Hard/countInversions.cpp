adacff3aa7278fb31d8be5a645b4c301049d32ea
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 15:39:01 +0530

    Initial Commit

--- 03 - Arrays/Hard/countInversions.cpp
+++ 03 - Arrays/Hard/countInversions.cpp
@@ -22,6 +22,7 @@ void merge(vector<int> &arr, int low, int mid, int high) {
     vector<int> temp;
     int left = low;
     int right = mid + 1;
+    
     while(left <= mid && right <= high) {
         if(arr[left] <= arr[right]) {
            temp.push_back(arr[left]);
