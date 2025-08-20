b429e95ab2bd82f5aceecc6d43fbaadb3f14b580
ayushporwal01 <porwalayush953@gmail.com>
2025-08-20 15:31:17 +0530

    Initial Commit

--- 03 - Arrays/Hard/countInversions.cpp
+++ 03 - Arrays/Hard/countInversions.cpp
@@ -19,15 +19,7 @@ int countInversions(vector<int> &arr) {
 //Optimal - TC: O(NlogN), SC: O(1) - Merge Sort Approach
 int countInversions(vector<int> &arr) {
     int n = arr.size();
-    int cnt = 0;
-    for(int i = 0; i < n - 1; i++) {
-        for(int j = i + 1; j < n; j++) {
-            if(arr[i] > arr[j]) {
-               cnt++;
-            }
-        }
-    }
-    return cnt;
+    
 }
 
 int main() {
