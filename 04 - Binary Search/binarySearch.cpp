a633a89a99adf0b10fdf13e6ddcdf8756efcab4e
ayushporwal01 <porwalayush953@gmail.com>
2025-08-21 17:02:50 +0530

    Initial Commit

--- 04 - Binary Search/binarySearch.cpp
+++ 04 - Binary Search/binarySearch.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 int search(vector<int> &arr, int target) {
     int n = arr.size();
-    
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) {
+           return i;
+        }
+    }
+    return -1;
 }
 
 int main() {
