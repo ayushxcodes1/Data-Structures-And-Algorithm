7422d12232759faa1b196508a20931de01cd9435
ayushporwal01 <porwalayush953@gmail.com>
2025-08-21 17:01:08 +0530

    Initial Commit

--- 04 - Binary Search/binarySearch.cpp
+++ 04 - Binary Search/binarySearch.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int search(vector<int> &arr, int target) {
-    
+
 }
 
 int main() {
   vector<int> arr = {-1, 0, 3, 5, 9, 12};
+  int ans = search(arr, 9);
+  cout << "Target found at index " << ans;
   return 0;
 }
