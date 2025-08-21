e481bdbdff8584e713358cf30e60fabf9c9766bc
ayushporwal01 <porwalayush953@gmail.com>
2025-08-21 17:05:16 +0530

    Initial Commit

--- 04 - Binary Search/binarySearch.cpp
+++ 04 - Binary Search/binarySearch.cpp
@@ -13,6 +13,17 @@ int search(vector<int> &arr, int target) {
     return -1;
 }
 
+//Iterative - TC: O(N), SC: O(1)
+int search(vector<int> &arr, int target) {
+    int n = arr.size();
+    for(int i = 0; i < n; i++) {
+        if(arr[i] == target) {
+           return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
   vector<int> arr = {-1, 0, 3, 5, 9, 12};
   int ans = search(arr, 9);
