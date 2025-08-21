f611b64811f73a9e5b1b371ec8e7b7f06d5b53a7
ayushporwal01 <porwalayush953@gmail.com>
2025-08-21 22:36:40 +0530

    Initial Commit

---
+++ 04 - Binary Search/upperBound.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int upperBound(vector<int> &arr, int low, int high, int target) {
+    int n = arr.size();
+    int ans = n;
+    while(low <= high) {
+       int mid = (low + (high - low) / 2);
+
+        if(arr[mid] > target) {
+           ans = mid;
+           high = mid - 1;
+        }
+        else {  
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+  vector<int> arr = {1, 2, 3, 3, 7, 8, 9, 9, 9, 11};
+  int n = arr.size();
+  int target = 9;
+
+  int ans = upperBound(arr, 0, n - 1, target);
+  cout << "Upper bound of " << target << " is at index: " << ans << "\n";
+
+  //Using STL
+  auto it = upper_bound(arr.begin(), arr.end(), target);
+  int index = it - arr.begin();
+  cout << "Upper bound of " << target << " is at index: " << index;
+
+  return 0;
+}
