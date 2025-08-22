c32639b30568e3c4c6edd50099fd7da8e9f656ad
ayushporwal01 <porwalayush953@gmail.com>
2025-08-22 16:55:10 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
+++ 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
@@ -2,9 +2,12 @@
 
 using namespace std;
 
+vector<int> firstAndLastPosition(vector<int> &arr, int target) {
+    
+}
 
 int main() {
   vector<int> arr = {2, 4, 6, 8, 8, 8, 11, 13};
-  
+
   return 0;
 }
