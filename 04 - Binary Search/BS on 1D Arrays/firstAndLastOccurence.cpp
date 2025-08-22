2aaa4e435ef79a899810f0980b23c542140b73ae
ayushporwal01 <porwalayush953@gmail.com>
2025-08-22 17:10:56 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
+++ 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
@@ -50,7 +50,7 @@ vector<int> firstAndLastPosition(vector<int> &arr, int n, int target) {
 int main() {
   vector<int> arr = {2, 4, 6, 8, 8, 8, 11, 13};
   int n = arr.size();
-  int target = 8;
+  int target = 10;
 
   vector<int> ans = firstAndLastPosition(arr, n, target);
   cout << "[" << ans[0] << "," << ans[1] << "]";
