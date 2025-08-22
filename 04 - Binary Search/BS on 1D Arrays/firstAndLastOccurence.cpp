6060b5a434256823f3434722d23650a2989a5a83
ayushporwal01 <porwalayush953@gmail.com>
2025-08-22 16:57:30 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
+++ 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
@@ -38,7 +38,12 @@ int upperBound(vector<int> &arr, int low, int high, int target) {
 
 
 vector<int> firstAndLastPosition(vector<int> &arr, int target) {
-    
+    int n = arr.size();
+    int low = 0, high = n - 1;
+
+    while(low <= high) {
+        
+    }
 }
 
 int main() {
