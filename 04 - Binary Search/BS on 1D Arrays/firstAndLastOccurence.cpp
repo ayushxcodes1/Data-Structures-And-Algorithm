37fa616212131707b54031586824abe0f7f2bc90
ayushporwal01 <porwalayush953@gmail.com>
2025-08-22 17:32:56 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
+++ 04 - Binary Search/BS on 1D Arrays/firstAndLastOccurence.cpp
@@ -36,6 +36,26 @@ int upperBound(vector<int> &arr, int low, int high, int target) {
     return ans;
 }
 
+int firstOccurrence(vector<int> &arr, int low, int high, int target) {
+    int n = arr.size();
+    int first = -1;
+    while(low <= high) {
+       int mid = (low + (high - low) / 2);
+
+        if(arr[mid] == target) {
+           first = mid;
+           high = mid - 1;
+        }
+        else if(arr[mid] < target){  
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return first;
+}
+
 //Using LowerBound & UpperBound
 vector<int> firstAndLastPosition(vector<int> &arr, int n, int target) {
     int stIndex = lowerBound(arr, 0, n - 1, target);
@@ -50,12 +70,8 @@ vector<int> firstAndLastPosition(vector<int> &arr, int n, int target) {
 
 //Without Using LowerBound & UpperBound
 vector<int> firstAndLastPosition2(vector<int> &arr, int n, int target) {
-    int stIndex = lowerBound(arr, 0, n - 1, target);
-    int endIndex = upperBound(arr, 0, n - 1, target) - 1;
-
-    if((stIndex == n) || arr[stIndex] != target) {
-        return {-1, -1};
-    }
+    int stIndex = firstOccurrence(arr, 0, n - 1, target);
+    int endIndex = lastOccurrence(arr, 0, n - 1, target);
 
     return {stIndex, endIndex};
 }
