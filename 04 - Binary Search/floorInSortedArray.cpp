d534b9570523339d6501cf276ec5c6d79bba9c9f
ayushporwal01 <porwalayush953@gmail.com>
2025-08-22 11:32:43 +0530

    Initial Commit

--- 04 - Binary Search/floorInSortedArray.cpp
+++ 04 - Binary Search/floorInSortedArray.cpp
@@ -5,12 +5,19 @@ using namespace std;
 int findFloor(vector<int> &arr, int target) {
     int n = arr.size();
     int low = 0, high = n - 1;
+    int ans = -1;
 
     while(low <= high) {
         int mid = (low + (high - low) / 2);
         
-        
+        if(arr[mid] <= target) {
+           ans = mid;
+           low = mid + 1;
+        }
+        else if(arr[mid] > target)
     }
+
+    return ans;
 }
 
 
