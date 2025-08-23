65b173294ef12ddcd68f0c0c46198f3034cf59bb
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 23:03:16 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/peakElement.cpp
+++ 04 - Binary Search/BS on 1D Arrays/peakElement.cpp
@@ -8,5 +8,7 @@ int findPeakElement(vector<int>& arr) {
 
 int main() {
   vector<int> arr = {1, 2, 1, 3, 5, 6, 4};
+  int ans = findPeakElement(arr);
+  cout << "Peak Element at index " << ans;
   return 0;
 }
