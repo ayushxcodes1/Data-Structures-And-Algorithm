7748d4a8553de7a8f619bd5c6a4238510a9aa655
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 11:51:47 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/findMinInRotatedSortedArray.cpp
+++ 04 - Binary Search/BS on 1D Arrays/findMinInRotatedSortedArray.cpp
@@ -27,6 +27,32 @@ int findMin(vector<int>& arr) {
     return mini;
 }
 
+//Duplicates
+int findMin2(vector<int>& arr) {
+    int n = arr.size();
+    int mini = INT_MAX;
+    int low = 0, high = n - 1;
+    while(low <= high) {
+        int mid = (low + (high - low) / 2);
+        
+        //search space is already sorted 
+        if(arr[low] <= arr[high]) {
+            mini = min(arr[low], mini);
+            break;
+        }
+
+        if(arr[low] <= arr[mid]) {
+            mini = min(arr[low], mini);
+            low = mid + 1;
+        }
+        else {
+            mini = min(arr[mid], mini);
+            high = mid - 1;
+        }
+    }
+    return mini;
+}
+
 int main() {
   vector<int> arr = {4, 5, 6, 7, 0, 1, 2};
   int ans = findMin(arr);
