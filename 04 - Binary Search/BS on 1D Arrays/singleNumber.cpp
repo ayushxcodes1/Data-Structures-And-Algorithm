94307503bf2ff8a8cfe9ff5dced41559549c317b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 22:21:04 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/singleNumber.cpp
+++ 04 - Binary Search/BS on 1D Arrays/singleNumber.cpp
@@ -13,7 +13,9 @@ int singleNumber(vector<int> &arr) {
     while(low <= high) {
         int mid = (low + (high - low) / 2);
         
-        
+        if(arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1]) {
+           return arr[mid];
+        }
 
         
     }
