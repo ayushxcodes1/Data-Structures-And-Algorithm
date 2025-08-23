8d7c85cdf799494f023231d4e2d151b6787a1f49
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 18:24:02 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/CntRotationsInRotatedSortedArray.cpp
+++ 04 - Binary Search/BS on 1D Arrays/CntRotationsInRotatedSortedArray.cpp
@@ -42,6 +42,46 @@ int cntRotations(vector<int> &arr) {
     return index;
 }
 
+//unique
+int cntRotations(vector<int> &arr) {
+    int n = arr.size();
+    int low = 0, high = n - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    
+    while(low <= high) {
+        int mid = (low + (high - low) / 2);
+        
+        //array is sorted
+        if(arr[low] <= arr[high]) {
+            if(arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+        
+        //left half is sorted
+        if(arr[low] <= arr[mid]) {
+            if(arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            low = mid + 1;
+        }
+        
+        //right half is sorted
+        else {
+            if(arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+            high = mid  - 1;
+        }
+    }
+    return index;
+}
+
 int main() {
   vector<int> arr = {3, 4, 5, 1, 2};
   int ans = cntRotations(arr);
