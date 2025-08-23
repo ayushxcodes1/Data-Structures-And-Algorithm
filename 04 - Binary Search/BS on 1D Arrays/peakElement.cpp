b8345482ab430bbaf842e5bc72f2c73ab2ce2ce0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 23:02:18 +0530

    Initial Commit

---
+++ 04 - Binary Search/BS on 1D Arrays/peakElement.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findPeakElement(vector<int>& arr) {
+         
+}
+
+int main() {
+  vector<int> arr = {1, 2, 1, 3, 5, 6, 4};
+  return 0;
+}
