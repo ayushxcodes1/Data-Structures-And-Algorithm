55ee14ff689a7ab54a7591b27f82ee7c4d5530f5
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 18:21:24 +0530

    Initial Commit

---
+++ 04 - Binary Search/BS on 1D Arrays/CntRotationsInRotatedSortedArray.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findKRotation(vector<int> &arr) {
+    int n = arr.size();
+    int low = 0, high = n - 1;
+    int ans = INT_MAX;
+    int index = -1;
+    
+    while(low <= high) {
+        int mid = (low + (high - low) / 2);
+        
+        //array is sorted
+        if(arr[low] <= arr[high]) {
+            if(arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            break;
+        }
+        
+        //left half is sorted
+        if(arr[low] <= arr[mid]) {
+            if(arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
+            low = mid + 1;
+        }
+        
+        //right half is sorted
+        else {
+            if(arr[mid] < ans) {
+                index = mid;
+                ans = arr[mid];
+            }
+            high = mid  - 1;
+        }
+    }
+    return index;
+}
+
+int main() {
+  vector<int> arr = {3, 4, 5, 1, 2};
+  return 0;
+}
