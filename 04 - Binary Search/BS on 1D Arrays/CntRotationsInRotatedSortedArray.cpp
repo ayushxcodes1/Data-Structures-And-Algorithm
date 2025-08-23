0d50d8278da9b5c29c70d4afee731bfd1ca08b8c
ayushporwal01 <porwalayush953@gmail.com>
2025-08-23 19:02:55 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/CntRotationsInRotatedSortedArray.cpp
+++ 04 - Binary Search/BS on 1D Arrays/CntRotationsInRotatedSortedArray.cpp
@@ -6,33 +6,39 @@ using namespace std;
 int cntRotations(vector<int> &arr) {
     int n = arr.size();
     int low = 0, high = n - 1;
-    int index = 0;
+    int ans = INT_MAX;
+    int index = -1;
     
     while(low <= high) {
         int mid = (low + (high - low) / 2);
         
         //array is sorted
         if(arr[low] <= arr[high]) {
-            index = low;
+            if(arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
             break;
         }
         
         //left half is sorted
         if(arr[low] <= arr[mid]) {
-            if(arr[low] <= arr[index]) {
+            if(arr[low] < ans) {
                 index = low;
+                ans = arr[low];
             }
             low = mid + 1;
         }
         
         //right half is sorted
         else {
-            if(arr[mid] < arr[index]) {
+            if(arr[mid] < ans) {
                 index = mid;
+                ans = arr[mid];
             }
             high = mid  - 1;
         }
-    }   
+    }
     return index;
 }
 
@@ -40,7 +46,8 @@ int cntRotations(vector<int> &arr) {
 int cntRotations2(vector<int> &arr) {
     int n = arr.size();
     int low = 0, high = n - 1;
-    int index = 0;
+    int ans = INT_MAX;
+    int index = -1;
     
     while(low <= high) {
         int mid = (low + (high - low) / 2);
@@ -51,28 +58,33 @@ int cntRotations2(vector<int> &arr) {
             high--;
         }
 
-         //array is sorted
+        //array is sorted
         if(arr[low] <= arr[high]) {
-            index = low;
+            if(arr[low] < ans) {
+                index = low;
+                ans = arr[low];
+            }
             break;
         }
-        
+
         //left half is sorted
         if(arr[low] <= arr[mid]) {
-            if(arr[low] <= arr[index]) {
+            if(arr[low] < ans) {
                 index = low;
+                ans = arr[low];
             }
             low = mid + 1;
         }
         
         //right half is sorted
         else {
-            if(arr[mid] < arr[index]) {
+            if(arr[mid] < ans) {
                 index = mid;
+                ans = arr[mid];
             }
             high = mid  - 1;
         }
-    }   
+    }
     return index;
 }
 
