22767aa431d287375c8a0b287ee86b2db49bb572
ayushporwal01 <porwalayush953@gmail.com>
2025-08-24 08:36:34 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/singleNumber.cpp
+++ 04 - Binary Search/BS on 1D Arrays/singleNumber.cpp
@@ -32,9 +32,9 @@ int singleNumber(vector<int> &arr) {
 int singleNumber2(vector<int> &arr) {
     int n = arr.size();
 
-    if(n == 1) return arr[0];
-    if(arr[0] != arr[1]) return arr[0];
-    if(arr[n - 1] != arr[n - 2]) return arr[n - 1];
+    if(n == 1) return 0;
+    if(arr[0] != arr[1]) return 0;
+    if(arr[n - 1] != arr[n - 2]) return n - 1;
 
     int low = 0, high = n - 1;
     while(low <= high) {
