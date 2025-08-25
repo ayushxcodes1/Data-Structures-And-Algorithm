f7992b271bdadc7580e8040b369762b3633ca678
ayushporwal01 <porwalayush953@gmail.com>
2025-08-25 12:23:18 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/BS on Answers/findNthRoot.cpp
+++ 04 - Binary Search/BS on 1D Arrays/BS on Answers/findNthRoot.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+
+//Brute Force - TC: O(M * N)
 int power(int i, int n) {
     long long ans = 1;
     for(int j = 0; j < n; j++) {
@@ -10,7 +12,6 @@ int power(int i, int n) {
     return ans;
 }
 
-//Brute Force - TC: O(M * N)
 int findNthRoot(int n, int m) {
     for(int i = 1; i <= m; i++) {
         if(power(i, n) == m) {
