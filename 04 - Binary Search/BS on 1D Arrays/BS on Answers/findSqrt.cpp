2e1e76648515c9a32938997e7de794f0d6dcd8cb
ayushporwal01 <porwalayush953@gmail.com>
2025-08-25 11:27:53 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/BS on Answers/findSqrt.cpp
+++ 04 - Binary Search/BS on 1D Arrays/BS on Answers/findSqrt.cpp
@@ -2,8 +2,21 @@
 
 using namespace std;
 
+int findSqrt(int n) {
+    int ans = 1;
+    for(int i = 1; i <= n; i++) {
+        if(i * i <= n) {
+           ans = i;
+        }
+        else {
+           break;
+        }
+    }
+}
 
 int main() {
-  
+  int n = 28;
+  int ans = findSqrt(n);
+  cout << "Square Root Of " << n << " is " << ans;
   return 0;
 }
