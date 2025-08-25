bd179db2b1e4501a992bd18146b5b5f0349bc86b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-25 12:12:40 +0530

    Initial Commit

--- 04 - Binary Search/BS on 1D Arrays/BS on Answers/findNthRoot.cpp
+++ 04 - Binary Search/BS on 1D Arrays/BS on Answers/findNthRoot.cpp
@@ -2,9 +2,18 @@
 
 using namespace std;
 
+bool find(int i, int n) {
+    
+}
+
 //Linear Search
 int findNthRoot(int n, int m) {
-    
+    for(int i = 1; i <= m; i++) {
+        if(find(i, n) == m) {
+           return i;
+        }
+    }
+    return -1;
 }
 
 
