12c759ae3f3499a6b7a4c09a8d6754c944f4a2a1
ayushporwal01 <porwalayush953@gmail.com>
2025-08-26 11:15:59 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/kokoEatingBananas.cpp
+++ 04 - Binary Search/BS on Answers/kokoEatingBananas.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+//Brute Force
+
+//Find Max
+int findMax(vector<int> &arr) {
+    int n = arr.size();
+    int maxi = arr[0];
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > maxi) {
+           maxi = arr[i];
+        }
+        return maxi;
+    }
+}
 
 int main() {
-  
+  vector<int> arr = {3, 6, 7, 11};
+  int ans = minEatingSpeed(arr);
   return 0;
 }
