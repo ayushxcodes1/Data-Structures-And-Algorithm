c9811d2c5e98f93d958a5c5b42b2daf934d115b8
ayushporwal01 <porwalayush953@gmail.com>
2025-08-27 17:55:00 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/kokoEatingBananas.cpp
+++ 04 - Binary Search/BS on Answers/kokoEatingBananas.cpp
@@ -18,8 +18,8 @@ int computeTotalHrs(vector<int> &arr, int hourly) {
     int n = arr.size();
     int totalH = 0;
     for(int i = 0; i < n; i++) {
-        //totalH += ceil(arr[i] / hourly); //slow
-        totalH += (arr[i] + hourly - 1) / hourly; //fast
+        totalH += ceil((double)arr[i] / hourly); //slow
+        // totalH += (arr[i] + hourly - 1) / hourly; //fast
     }
     return totalH;
 }
