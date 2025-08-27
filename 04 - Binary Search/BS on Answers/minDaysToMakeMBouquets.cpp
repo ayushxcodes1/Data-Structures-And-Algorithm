097afbd2d5f0944a25e974f081af4e4726b9afdc
ayushporwal01 <porwalayush953@gmail.com>
2025-08-27 21:20:27 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/minDaysToMakeMBouquets.cpp
+++ 04 - Binary Search/BS on Answers/minDaysToMakeMBouquets.cpp
@@ -34,36 +34,36 @@ bool findMinDays(vector<int> &arr, int day, int m, int k) {
     return bouquets >= m ? true : false;
 }
 
-    int minDays(vector<int> &arr, int m, int k) {
-        int ans = 0;
-        int maximum = findMax(arr);
-        for(int i = 1; i <= maximum; i++) {
-            bool mini = findMinDays(arr, i, m, k);
-            if(mini == true) return i;
-        }
-        return -1;
-    }  
+int minDays(vector<int> &arr, int m, int k) {
+    int ans = 0;
+    int maximum = findMax(arr);
+    for(int i = 1; i <= maximum; i++) {
+        bool mini = findMinDays(arr, i, m, k);
+        if(mini == true) return i;
+    }
+    return -1;
+}  
 
-    //Binary Search - TC: O(NlogM)
-    int minDays2(vector<int> &arr, int m, int k) {
-        int n = arr.size();
-        int maxi = findMax(arr);
-        int ans = -1;
+//Binary Search - TC: O(NlogM)
+int minDays2(vector<int> &arr, int m, int k) {
+    int n = arr.size();
+    int maxi = findMax(arr);
+    int ans = -1;
 
-        int low = 1, high = maxi;
-        while(low <= high) {
-            int mid = (low + (high - low) / 2);
-            bool mini = findMinDays(arr, mid, m, k);
-            if(mini == true) {
-            ans = mid;
-            high = mid - 1;
-            }
-            else {
-            low = mid + 1;
-            }
+    int low = 1, high = maxi;
+    while(low <= high) {
+        int mid = (low + (high - low) / 2);
+        bool mini = findMinDays(arr, mid, m, k);
+        if(mini == true) {
+        ans = mid;
+        high = mid - 1;
         }
-        return ans;
-    }  
+        else {
+        low = mid + 1;
+        }
+    }
+    return ans;
+}  
 
 int main() {
   vector<int> arr = {1, 10, 3, 10, 2};
