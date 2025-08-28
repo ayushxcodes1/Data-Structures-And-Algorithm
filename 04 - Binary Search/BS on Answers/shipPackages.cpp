adc0f2d06fabe87f15e1db0b4fa19871bca15505
ayushporwal01 <porwalayush953@gmail.com>
2025-08-28 22:19:32 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/shipPackages.cpp
+++ 04 - Binary Search/BS on Answers/shipPackages.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//Brute Force - TC: O(max * N)
+//Brute Force
 int findMax(vector<int> &arr) {
     int n = arr.size();
     int maxi = INT_MIN;
@@ -14,43 +14,8 @@ int findMax(vector<int> &arr) {
     return maxi;
 }
 
-int findDivisorSum(vector<int> &arr, int n, int d) {
-    int sum = 0;
-    for(int i = 0; i < n; i++) {
-        // sum += (arr[i] + d - 1) / d; //fast
-        sum += ceil((double)arr[i] / d); //slow
-    }
-    return sum;
-}
-
-int smallestDivisor(vector<int> &arr, int threshold) {
-    int n = arr.size();
-    int maxi = findMax(arr);
-
-    for(int i = 1; i <= maxi; i++) {
-        int sum = findDivisorSum(arr, n, i);
-        if(sum <= threshold) {
-           return i;
-        }
-    }
-    return -1;
-}
-
-//Optimal - O(log(max) * N)
-int shipPackages2(vector<int> &arr, int threshold) {
-    int n = arr.size();
-    int maxi = findMax(arr);
-    int low = 1, high = maxi;
-    while(low <= high) {
-        int mid = low + (high - low) / 2;
-        int divisorSum = findDivisorSum(arr, n, mid);
-        
-        if(divisorSum <= threshold) {
-           high = mid - 1;
-        }
-        else low = mid + 1;
-    }
-    return low;
+int shipPackages(vector<int> &arr, int threshold) {
+    
 }
 
 int main() {
