c864e9d5f88ebd056783cd398096e61955f61d17
ayushporwal01 <porwalayush953@gmail.com>
2025-08-28 22:16:57 +0530

    Initial Commit

---
+++ 04 - Binary Search/BS on Answers/shipPackages.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//Brute Force - TC: O(max * N)
+int findMax(vector<int> &arr) {
+    int n = arr.size();
+    int maxi = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(arr[i] > maxi) {
+           maxi = arr[i];
+        }
+    }
+    return maxi;
+}
+
+int findDivisorSum(vector<int> &arr, int n, int d) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        // sum += (arr[i] + d - 1) / d; //fast
+        sum += ceil((double)arr[i] / d); //slow
+    }
+    return sum;
+}
+
+int smallestDivisor(vector<int> &arr, int threshold) {
+    int n = arr.size();
+    int maxi = findMax(arr);
+
+    for(int i = 1; i <= maxi; i++) {
+        int sum = findDivisorSum(arr, n, i);
+        if(sum <= threshold) {
+           return i;
+        }
+    }
+    return -1;
+}
+
+//Optimal - O(log(max) * N)
+int smallestDivisor2(vector<int> &arr, int threshold) {
+    int n = arr.size();
+    int ans = -1;
+    int maxi = findMax(arr);
+    int low = 1, high = maxi;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        int divisorSum = findDivisorSum(arr, n, mid);
+        
+        if(divisorSum <= threshold) {
+           high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return low;
+}
+
+int main() {
+  vector<int> arr = {1, 2, 5, 9};
+  int threshold = 6;
+
+  int ans = smallestDivisor(arr, threshold);
+  cout << ans << "\n";
+
+  int ans2 = smallestDivisor2(arr, threshold);
+  cout << ans2;
+  
+  return 0;
+}
