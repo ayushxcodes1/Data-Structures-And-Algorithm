7c6b4b239540c78b22f87831ee9488867cb0ba44
ayushporwal01 <porwalayush953@gmail.com>
2025-08-28 17:29:27 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/minDaysToMakeMBouquets.cpp
+++ 04 - Binary Search/BS on Answers/minDaysToMakeMBouquets.cpp
@@ -16,7 +16,7 @@ int findMini(vector<int> &arr) {
 
 int findMaxi(vector<int> &arr) {
     int n = arr.size();
-    int maxi = INT_MAX;
+    int maxi = INT_MIN;
     for(int i = 0; i < n; i++) {
         if(arr[i] > maxi) {
            maxi = arr[i];
