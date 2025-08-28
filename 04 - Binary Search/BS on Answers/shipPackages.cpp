da5723195bd6aa5b8feb0355441d560228b0f4ac
ayushporwal01 <porwalayush953@gmail.com>
2025-08-28 22:43:29 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/shipPackages.cpp
+++ 04 - Binary Search/BS on Answers/shipPackages.cpp
@@ -12,8 +12,29 @@ int sumOfN(vector<int> &arr) {
     return sum;
 }
 
+bool func(vector<int> &arr, int capacity) {
+    int n = arr.size();
+    int sum = 0;
+    int days = 1;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+        if(sum >= capacity) {
+            days++;
+            sum = 0;
+        }
+    }
+
+}
+
 int shipPackages(vector<int> &arr, int days) {
-    in
+    int n = arr.size();
+    int sum = sumOfN(arr);
+    for(int i = 1; i <= sum; i++) {
+        bool x = func(arr, i);
+        if(x == true) {
+           return i;
+        }
+    }
 }
 
 int main() {
