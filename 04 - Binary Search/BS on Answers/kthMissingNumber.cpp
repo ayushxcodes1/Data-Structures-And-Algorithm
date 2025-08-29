837b95db1c8798b037477301b70410fc74a224e0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-29 17:39:47 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/kthMissingNumber.cpp
+++ 04 - Binary Search/BS on Answers/kthMissingNumber.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+int findKthMissing(vector<int> &arr, int k) {
+    
+}
+
 int main() {
-  
+  vector<int> arr = {2, 3, 4, 7, 11};
+  int k = 5;
+
+  int ans = findKthMissing(arr, k);
   return 0;
 }
