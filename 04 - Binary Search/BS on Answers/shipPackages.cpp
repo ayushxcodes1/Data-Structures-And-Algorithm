899ec549391e3afd10c8a5ed578a95d6f8b307b0
ayushporwal01 <porwalayush953@gmail.com>
2025-08-29 17:05:13 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/shipPackages.cpp
+++ 04 - Binary Search/BS on Answers/shipPackages.cpp
@@ -26,12 +26,12 @@ int findArraySum(vector<int> &arr) {
 int computeDaysReq(vector<int> &arr, int capacity) {
     int n = arr.size();
     int sum = 0;
-    int days = 0;
+    int days = 1;
     for(int i = 0; i < n; i++) {
         if(sum + arr[i] > capacity) {
             days++;
             sum = 0;
-            sum += arr[i];
+            sum = arr[i];
         }
         else {
            sum += arr[i];
