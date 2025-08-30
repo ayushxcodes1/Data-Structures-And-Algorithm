b890555a5e5fc8f4c8356e805aad96c5045c490c
ayushporwal01 <porwalayush953@gmail.com>
2025-08-30 18:44:39 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/allocateMinimumPages.cpp
+++ 04 - Binary Search/BS on Answers/allocateMinimumPages.cpp
@@ -33,6 +33,21 @@ int allocateMinimumPages(vector<int> &arr, int k) {
   return -1;
 }
 
+//Optimal - TC: O(log(sum - max + 1) * N)
+int allocateMinimumPages(vector<int> &arr, int k) {
+  int n = arr.size();
+  if(k > n) return -1;
+  
+  int max = *max_element(arr.begin(), arr.end());
+  int sum = accumulate(arr.begin(), arr.end(), 0);
+  for(int i = max; i <= sum; i++) {
+    if(findPages(arr, i) == k) {
+      return i;
+    }
+  } 
+  return -1;
+}
+
 int main() {
   vector<int> arr = {12, 34, 67, 90};
   int k = 2;
