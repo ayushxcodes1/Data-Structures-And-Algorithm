c66c4e8106fa2c4fc5da55cca5a42cef115d0b3b
ayushporwal01 <porwalayush953@gmail.com>
2025-08-30 11:00:31 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/aggressiveCows.cpp
+++ 04 - Binary Search/BS on Answers/aggressiveCows.cpp
@@ -30,6 +30,23 @@ int aggressiveCows(vector<int> &stalls, int k) {
     return limit;
 }
 
+//Optimal 
+int aggressiveCows2(vector<int> &stalls, int k) {
+    sort(stalls.begin(), stalls.end());
+
+    int n = stalls.size();
+    int limit = stalls[n - 1] - stalls[0];
+    int low = 1, high = limit;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        if(canWePlace(stalls, mid, k) == false) {
+           high = mid - 1;
+        }
+        else low = mid + 1;
+    }
+    return low;
+}
+
 int main() {
   vector<int> stalls = {0, 3, 4, 7, 9, 10};
   int k = 4;
