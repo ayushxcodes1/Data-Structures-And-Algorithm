125dc3d972e397f23c0962ca65bddf729901da84
ayushporwal01 <porwalayush953@gmail.com>
2025-08-30 18:03:24 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/allocateMinPages.cpp
+++ 04 - Binary Search/BS on Answers/allocateMinPages.cpp
@@ -6,11 +6,15 @@ int findPages(vector<int> &arr, int k) {
     
 }
 
+int allocateBooks(vector<int> &arr, int k) {
+  
+}
+
 int main() {
   vector<int> arr = {12, 34, 67, 90};
   int k = 2;
   
-  int ans = findPages(arr, k);
+  int ans = allocateBooks(arr, k);
   cout << ans << "\n";
   return 0;
 }
