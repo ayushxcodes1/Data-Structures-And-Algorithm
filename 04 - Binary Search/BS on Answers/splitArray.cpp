9c7e316b7799a235f3a4fc0fe50fe47c8d98f66c
ayushporwal01 <porwalayush953@gmail.com>
2025-08-31 11:08:46 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/splitArray.cpp
+++ 04 - Binary Search/BS on Answers/splitArray.cpp
@@ -10,9 +10,9 @@ int findMaxSum(vector<int> &arr, int k, int NoOfELe) {
         if(cnt != NoOfELe) {
            cnt++;
            sum += arr[i];
+           maxSum = max(sum, maxSum);
         }
         else {
-           maxSum = max(sum, maxSum);
            sum = 0;
            cnt = 0; 
         }
