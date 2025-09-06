e52431f2f327b6e24f333545bb12fd4e2d53f488
ayushporwal01 <porwalayush953@gmail.com>
2025-09-06 22:00:55 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
@@ -2,8 +2,16 @@
 
 using namespace std;
 
+int findMedian(vector<int> &arr1, vector<int> &arr2) {
+
+}
 
 int main() {
+  vector<int> arr1 = {1, 3, 4, 7, 10, 12};
+  vector<int> arr2 = {2, 3, 6, 15}; 
+
+  int ans = findMedian(arr1, arr2);
+  cout << "Median is " << ans;
   
   return 0;
 }
