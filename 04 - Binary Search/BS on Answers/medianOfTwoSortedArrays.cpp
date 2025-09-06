2fd6ccd2c365c9f6059077d061c0c4a33d56916a
ayushporwal01 <porwalayush953@gmail.com>
2025-09-06 22:39:27 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-//Brute Force
-int findMedian(vector<int> &arr1, vector<int> &arr2) {
+//Brute Force - TC: O(min(N1, N2) + O(N1)), SC: O(N1 + N2)
+double findMedian(vector<int> &arr1, vector<int> &arr2) {
     int n1 = arr1.size(); 
     int n2 = arr2.size();
-    vector<int> temp;
+    vector<int> temp; //O(N1 + N2)
 
     int i = 0, j = 0;
-    while(i < n1 && j < n2) {
+    while(i < n1 && j < n2) { //O(min(N1, N2))
         if(arr1[i] < arr2[j]) {
            temp.push_back(arr1[i]);
            i++;
@@ -21,13 +21,13 @@ int findMedian(vector<int> &arr1, vector<int> &arr2) {
     }
 
     //push leftover from arr1
-    while(i < n1) {
+    while(i < n1) { //O(N1)
         temp.push_back(arr1[i]);
         i++;
     }
 
     //push leftovers from arr2
-    while(j < n2) {
+    while(j < n2) { //O(N2)
         temp.push_back(arr2[j]);
         j++;
     }
@@ -37,12 +37,10 @@ int findMedian(vector<int> &arr1, vector<int> &arr2) {
        //odd length
        return temp[n / 2];
     }
-    else {
-       //even length
-       int evenAns = (temp[n / 2 - 1] + temp[n / 2]) / 2;
-       return evenAns;
-    }
-    return -1;
+       
+    //even length
+    int evenAns = (temp[n / 2 - 1] + temp[n / 2]) / 2;
+    return evenAns;
 }
 
 int main() {
