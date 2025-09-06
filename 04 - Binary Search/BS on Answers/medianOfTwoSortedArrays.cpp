1f3135dcfbca35e53e8fc8d513497c69f1c196f1
ayushporwal01 <porwalayush953@gmail.com>
2025-09-06 22:48:51 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
@@ -42,9 +42,45 @@ double findMedian(vector<int> &arr1, vector<int> &arr2) {
     double evenAns = ((double)temp[n / 2 - 1] + (double)temp[n / 2]) / 2;
     return (double)evenAns;
 }
+double findMedian(vector<int> &arr1, vector<int> &arr2) {
+    int n1 = arr1.size(); 
+    int n2 = arr2.size();
+    vector<int> temp;
+
+    int i = 0, j = 0;
+    while(i < n1 && j < n2) { 
+        if(arr1[i] < arr2[j]) {
+           temp.push_back(arr1[i]);
+           i++;
+        }
+        else {
+           temp.push_back(arr2[j]);
+           j++;
+        }
+    }
+
+    //push leftovers from arr1
+    while(i < n1) {
+        temp.push_back(arr1[i]);
+        i++;
+    }
+
+    //push leftovers from arr2
+    while(j < n2) { 
+        temp.push_back(arr2[j]);
+        j++;
+    }
+    
+    int low = 0, high = temp.size() - 1;
+    while(low <= high) {
+        int mid = low + (high - low) / 2;
+        
+    }
+}
 
 
 //Optimal - TC: O(log(N1 + N2)), SC: O(N1 + N2)
+
 
 int main() {
   vector<int> arr1 = {1, 2};
