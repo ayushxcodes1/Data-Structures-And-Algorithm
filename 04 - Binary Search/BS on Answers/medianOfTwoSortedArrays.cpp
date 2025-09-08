38d4003347f414c6289ed126a084e972070e8011
ayushporwal01 <porwalayush953@gmail.com>
2025-09-08 22:20:10 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
@@ -99,7 +99,8 @@ double findMedian2(vector<int> &arr1, vector<int> &arr2) {
     int n2 = arr2.size();
     if(n1 > n2) return findMedian2(arr2, arr1);
     int low = 0, high = n1;
-    
+    int left = (n1 + n2 + 1) / 2;
+     
 }
 
 int main() {
