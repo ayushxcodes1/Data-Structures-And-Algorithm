e493ac69622f755ccd59b0fea793ad035047e3b0
ayushporwal01 <porwalayush953@gmail.com>
2025-09-08 23:17:14 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/kthElementOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/kthElementOfTwoSortedArrays.cpp
@@ -3,6 +3,11 @@
 using namespace std;
 
 int main() {
-  
+  vector<int> arr1 = {2, 3, 6, 7, 9}; 
+  vector<int> arr2 = {1, 4, 8, 10}; 
+  int k = 5;
+
+  int ans = kthElement(arr1, arr2, k);
+  cout << "Kth Element is: " << ans;
   return 0;
 }
