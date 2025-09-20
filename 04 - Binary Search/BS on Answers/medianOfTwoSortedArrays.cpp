a55472f531ec5facf3646b4c51bfe6d78edfef4e
ayushporwal01 <porwalayush953@gmail.com>
2025-09-20 23:08:21 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/medianOfTwoSortedArrays.cpp
@@ -107,7 +107,7 @@ double findMedian3(vector<int> &arr1, vector<int> &arr2) {
         int l1 = INT_MIN, l2 = INT_MIN;
         int r1 = INT_MAX, r2 = INT_MAX;
         if(mid1 < n1) r1 = arr1[mid1];
-        if(mid2 < n1) r1 = arr2[mid2];
+        if(mid2 < n2) r2 = arr2[mid2];
         if(mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
         if(mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
         if(l1 <= r2 && l2 <= r1) {
