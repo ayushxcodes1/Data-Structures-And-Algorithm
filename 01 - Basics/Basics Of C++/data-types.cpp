dd4d2de0be39a3d9bfb7bfa9413a62ae3e31f96e
ayushporwal01 <porwalayush953@gmail.com>
2025-09-29 17:52:42 +0530

    Initial Commit

--- 01 - Basics/Basics Of C++/data-types.cpp
+++ 01 - Basics/Basics Of C++/data-types.cpp
@@ -6,6 +6,23 @@ int main() {
     //int, long, long long, float, double
     //string and getline
     //char
+
+    int a = 10;
+    long long b = 10000000000LL;
+    float c = 5.5;
+    double d = 3.14159265359;
+    char ch = 'Z';
+    string name;
     
+    cout << "Enter your name: ";
+    getline(cin, name);
+
+    cout << "int: " << a << "\n";
+    cout << "long long: " << b << "\n";
+    cout << "float: " << c << "\n";
+    cout << "double: " << d << "\n";
+    cout << "char: " << ch << "\n";
+    cout << "string: " << name << "\n";
+
     return 0;
 }
