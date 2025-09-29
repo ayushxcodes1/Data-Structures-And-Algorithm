3c6e0581f781b715907877f914db82bfd0b91286
ayushporwal01 <porwalayush953@gmail.com>
2025-09-29 18:00:04 +0530

    Initial Commit

--- 01 - Basics/Basics Of C++/if-else.cpp
+++ 01 - Basics/Basics Of C++/if-else.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main() {
     int age;
     cin >> age;
-   
+    if(age >= 18) {
+      
+    }
+    else {
+       cout << "eligible for driving license.";
+    }
     return 0;
 }
