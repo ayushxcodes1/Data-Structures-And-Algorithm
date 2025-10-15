9556aec3296e421bcc2929fc6bfe3dc70877bcf4
ayushporwal01 <porwalayush953@gmail.com>
2025-10-15 12:37:50 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/rowWithMax1s.cpp
+++ 04 - Binary Search/BS on 2D Arrays/rowWithMax1s.cpp
@@ -7,7 +7,11 @@ int rowWithMax1s(vector<vector<int>> &arr) {
 }
 
 int main() {
-    vector<vector<int>> arr = {{0, 0, 1, 1, 1}, {0, 0, 0, 0, 0}, {0, 1, 1, 1, 1}, {0, 0, 0, 0, 0}, {0, 0, 1, 1, 1}};
+    vector<vector<int>> arr = {{0, 0, 1, 1, 1}, 
+                               {0, 0, 0, 0, 0}, 
+                               {0, 1, 1, 1, 1}, 
+                               {0, 0, 0, 0, 0}, 
+                               {0, 0, 1, 1, 1}};
     int ans = rowWithMax1s(arr);
     return 0;
 }
