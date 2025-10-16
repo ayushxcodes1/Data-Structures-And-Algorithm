5a78ac23f54c70e5b4c20c118e97d39d9cf09cd7
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 09:44:28 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
+++ 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
@@ -46,9 +46,8 @@ int rowWithMaxOnes(vector<vector<int>> &mat) {
   int ind = -1;
   for(int i = 0; i < n; i++) {
     int cnt = 0;
-    for(int j = 0; j < m; j++) {
-      if(mat[i][j] == 1) cnt++;
-    }
+    int ind = -1;
+    
     if(cnt > maxCnt) {
       maxCnt = max(cnt, maxCnt);
       ind = i;
