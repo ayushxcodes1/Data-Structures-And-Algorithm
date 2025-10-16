d08e6cdedebf933dd776db2f131b8cc12dae92e6
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 16:40:57 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+bool searchMatrix(vector<vector<int>> &mat, int n, int m) {
+    
+}
+
 int main() {
   vector<vector<int>> mat = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
   return 0;
