8c0e18db4a86d80349a72b1f2638606cc1c364eb
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 09:39:50 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
+++ 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 //Brute Force - TC:O(N * M), SC: O(1)
 int rowWithMaxOnes(vector<vector<int>> &mat) {
-  int rows = mat.size();
-  int cols = mat[0].size();
+  int n = mat.size();
+  int m = mat[0].size();
   
   int maxCnt = -1;
   int ind = -1;
-  for(int i = 0; i < rows; i++) {
+  for(int i = 0; i < n; i++) {
     int cnt = 0;
-    for(int j = 0; j < cols; j++) {
+    for(int j = 0; j < m; j++) {
       if(mat[i][j] == 1) cnt++;
     }
     if(cnt > maxCnt) {
