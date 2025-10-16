f16788389c6a8265c2d4f50f52a59bb996448b5f
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 16:57:28 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
@@ -31,7 +31,7 @@ bool search(vector<int> arr, int m, int target) {
     return false;
 }
 
-bool searchMatrix(vector<vector<int>> &mat, int n, int m, int target) {
+bool searchMatrix2(vector<vector<int>> &mat, int n, int m, int target) {
     for(int i = 0; i < n; i++) {
         bool ans = search(mat[i], m, target);
         if(ans == true) {
