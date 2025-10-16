4e69922794e9c53782098a7ec918b2ccf636c69e
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 17:23:44 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
@@ -47,8 +47,12 @@ bool searchMatrix3(vector<vector<int>> &mat, int n, int m, int target) {
        int mid = (low + high) / 2;
        int row = mid / m; 
        int col = mid % m;
-       
+
+       if(mat[row][col] == target) return true;
+       else if(mat[row][col] < target) low = mid + 1;
+       else high = mid - 1;
     }
+    return false;
 }
 
 int main() {
