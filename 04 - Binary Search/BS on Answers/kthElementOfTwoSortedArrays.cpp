a7a2cbb572baadc20729177b3e77d54c7485feef
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 12:30:42 +0530

    Initial Commit

--- 04 - Binary Search/BS on Answers/kthElementOfTwoSortedArrays.cpp
+++ 04 - Binary Search/BS on Answers/kthElementOfTwoSortedArrays.cpp
@@ -20,7 +20,7 @@ int kthElement(vector<int> &arr1, vector<int> &arr2, int k) {
     if(mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
     if(mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
     if(l1 <= r2 && l2 <= r1) {
-        return max(l1, l2);
+      return max(l1, l2);
     }
     else if(l1 > l2) high = mid1 - 1;
     else low = mid1 + 1;
