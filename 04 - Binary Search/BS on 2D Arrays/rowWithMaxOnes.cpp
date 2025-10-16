628abe2cb35fdc063fba1e8fa708bc98953ab60e
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 09:38:53 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
+++ 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
@@ -28,8 +28,14 @@ int lowerBound(vector<int> arr, int n, int x) {
   int ans = -1;
   while(low <= high) {
     int mid = (low + high) / 2;
-    
+
+    if(arr[mid] >= x) {
+      ans = mid;
+      high = mid - 1;
+    }
+    else low = mid + 1;
   }
+  return ans;
 }
 
 
