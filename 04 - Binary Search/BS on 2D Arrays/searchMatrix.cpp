2072516c90b8f72a14ef0d3122bca94c6f40e367
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 17:20:11 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
@@ -40,6 +40,8 @@ bool searchMatrix2(vector<vector<int>> &mat, int n, int m, int target) {
     return false;
 }
 
+//Optimal - TC: O(log(N * M)), SC: O(1)
+
 int main() {
   vector<vector<int>> mat = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
   int n = mat.size();
