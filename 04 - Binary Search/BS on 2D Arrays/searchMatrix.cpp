32a9991a7686baca43a77df47eeeadcded13fd2d
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 16:45:36 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/searchMatrix.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//Brute Force
+//Brute Force - TC: O(N * M), SC: O(1)
 bool searchMatrix(vector<vector<int>> &mat, int n, int m, int target) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
