5cfd2bfbf64bf4aefac5f26d1d5a0294b78630d7
ayushporwal01 <porwalayush953@gmail.com>
2025-10-16 08:56:45 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
+++ 04 - Binary Search/BS on 2D Arrays/rowWithMaxOnes.cpp
@@ -2,9 +2,14 @@
 
 using namespace std;
 
-int rowWithMaxOnes()
+int rowWithMaxOnes() {
+  
+}
 
 int main() {
-  
+  vector<vector<int>> mat = {{0,1,1,1}, 
+                             {0,0,1,1}, 
+                             {1,1,1,1}, 
+                             {0,0,0,0}};
   return 0;
 }
