b1c797759bb9ec53c56b2e98af383094255b325f
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 11:05:49 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/peakElementII.cpp
+++ 04 - Binary Search/BS on 2D Arrays/peakElementII.cpp
@@ -20,6 +20,24 @@ vector<int> findPeakIndex(vector<vector<int>> &mat, int n, int m) {
     return {-1, -1};
 }
 
+//Brute Force - TC: O(N * M * 4), SC: O(1)
+vector<int> findPeakIndex(vector<vector<int>> &mat, int n, int m) { 
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            int top = (i > 0) ? mat[i - 1][j] : -1;
+            int bottom = (i < n - 1) ? mat[i + 1][j] : -1;
+            int left = (j > 0) ? mat[i][j - 1] : -1;
+            int right = (j < m - 1) ? mat[i][j + 1] : -1;
+
+            if(mat[i][j] > left && mat[i][j] > right && 
+               mat[i][j] > top && mat[i][j] > bottom) {
+               return {i, j};
+            }
+        }
+    }
+    return {-1, -1};
+}
+
 
 int main() {
   vector<vector<int>> mat = {{10, 20, 15}, {21, 30, 14}, {7, 16, 32}};
