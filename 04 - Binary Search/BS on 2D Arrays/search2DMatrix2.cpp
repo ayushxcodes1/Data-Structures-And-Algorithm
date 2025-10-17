e46ed00fb1054db16891127e2d11a5aa4ae6aea5
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 09:45:08 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/search2DMatrix2.cpp
+++ 04 - Binary Search/BS on 2D Arrays/search2DMatrix2.cpp
@@ -40,6 +40,21 @@ bool searchMatrix2(vector<vector<int>> &mat, int n, int m, int target) {
     return false;
 }
 
+//Optimal - TC: O(log(N * M)), SC: O(1)
+bool searchMatrix3(vector<vector<int>> &mat, int n, int m, int target) {
+    int low = 0, high = n * m - 1;
+    while(low <= high) {
+       int mid = (low + high) / 2;
+       int row = mid / m; 
+       int col = mid % m;
+
+       if(mat[row][col] == target) return true;
+       else if(mat[row][col] < target) low = mid + 1;
+       else high = mid - 1;
+    }
+    return false;
+}
+
 int main() {
   vector<vector<int>> mat = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
   int n = mat.size();
