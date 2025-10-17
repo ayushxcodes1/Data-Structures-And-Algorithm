eb347be52c5912c9376727d667c42b36a319a799
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 11:22:43 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/peakElementII.cpp
+++ 04 - Binary Search/BS on 2D Arrays/peakElementII.cpp
@@ -37,6 +37,16 @@ vector<int> findPeakIndex(vector<vector<int>> &mat, int n, int m) {
     int low = 0, high = m - 1;
     while(low <= high) {
         int mid = (low + high) / 2;
+        int maxRowIndex = findMaxIndex(mat, n, m, mid);
+        int left = mid - 1 >= 0 ? mat[maxRowIndex][mid - 1] : -1;
+        int right = mid + 1 < m ? mat[maxRowIndex][mid + 1] : -1;
+        if(mat[maxRowIndex][mid] > left && mat[maxRowIndex][mid] > right) {
+           return {maxRowIndex, mid};
+        }
+        else if(mat[maxRowIndex][mid] < left) {
+           high = mid - 1;
+        }
+        else low = mid + 1;
     }
 }
 
