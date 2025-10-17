4e1050d87b108ebdca12096fc85506ab456b37c5
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 10:48:52 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/peakElementII.cpp
+++ 04 - Binary Search/BS on 2D Arrays/peakElementII.cpp
@@ -6,12 +6,13 @@ using namespace std;
 vector<int> findPeakIndex(vector<vector<int>> &mat, int n, int m) {
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            if(mat[i][j] > mat[i][j - 1] && mat[i][j] > mat[i][j + 1] && 
-               mat[i][j] > mat[i - 1][j] && mat[i][j] > mat[i + 1][j]) {
-
+            if(mat[i][j] > left && mat[i][j] > right && 
+               mat[i][j] > top && mat[i][j] > bottom) {
+               return {i, j};
             }
         }
     }
+    return {-1, -1};
 }
 
 
