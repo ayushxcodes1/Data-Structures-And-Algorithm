70916358875fd1d67f29fd8418a6fa2f75b9b08b
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 14:29:02 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/findMedianInSortedMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/findMedianInSortedMatrix.cpp
@@ -36,6 +36,26 @@ int upperBound(vector<int> &arr, int low, int high, int target) {
     return ans;
 }
 
+int countSmallEqual(vector<vector<int>> &mat, int n, int m, int x) {
+    int cnt = 0; 
+
+}
+
+int findMedian(vector<vector<int>> &mat, int n, int m) {
+    vector<int> temp;
+
+    //O(N * M)
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            temp.push_back(mat[i][j]);
+        }
+    }
+
+    //O((N * M) * log(N * M))
+    sort(temp.begin(), temp.end());
+    return temp[temp.size() / 2];
+}
+
 int main() {
     vector<vector<int>> mat = {{1, 3, 5}, {2, 6, 9}, {3, 6, 9}};
     int n = mat.size();
