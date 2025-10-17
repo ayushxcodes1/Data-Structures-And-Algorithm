13fe28f8c335a2aa69d262f853e4b82db5ebc81d
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 09:37:06 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/search2DMatrix2.cpp
+++ 04 - Binary Search/BS on 2D Arrays/search2DMatrix2.cpp
@@ -15,7 +15,7 @@ bool searchMatrix(vector<vector<int>> &mat, int n, int m, int target) {
 }
 
 int main() {
-  vector<vector<int>> mat = {{1, 4, 7, 11, 15}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+  vector<vector<int>> mat = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
   int n = mat.size();
   int m = mat[0].size();
 
