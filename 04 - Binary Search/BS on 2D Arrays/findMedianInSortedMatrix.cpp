6cd666075f9cef1edc3bab7731c589cf1cb9d5ad
ayushporwal01 <porwalayush953@gmail.com>
2025-10-17 13:51:34 +0530

    Initial Commit

--- 04 - Binary Search/BS on 2D Arrays/findMedianInSortedMatrix.cpp
+++ 04 - Binary Search/BS on 2D Arrays/findMedianInSortedMatrix.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-int findMedian(vector<vector<int>> &mat, int n, int m)
+int findMedian(vector<vector<int>> &mat, int n, int m) {
+    
+}
 
 int main() {
   vector<vector<int>> mat = {{1, 3, 5}, {2, 6, 9}, {3, 6, 9}};
