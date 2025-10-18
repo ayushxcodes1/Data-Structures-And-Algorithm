b8e862a77387548df10fa57976559b1ac34b5331
ayushporwal01 <porwalayush953@gmail.com>
2025-10-18 16:28:04 +0530

    Initial Commit

--- 05 - Strings/Easy/removeOuterParenthesis.cpp
+++ 05 - Strings/Easy/removeOuterParenthesis.cpp
@@ -20,10 +20,28 @@ string removeOuterParenthesis(string s) {
     return res;
 }
 
+//Optimal - TC: O(N), SC: O(1) - Depth Counter
+string removeOuterParenthesis(string s) {
+    stack<char> st;
+    string res = "";
+
+    for(auto c : s) {
+        if(c == '(') {
+           if(!st.empty()) res += c;
+           st.push(c);
+        }
+        else {
+           st.pop();
+           if(!st.empty()) res += c;
+        }
+    }
+    return res;
+}
+
 int main() {
   string s = "(()())(())";
   string res = removeOuterParenthesis(s);
   cout << res;
-  
+
   return 0;
 }
