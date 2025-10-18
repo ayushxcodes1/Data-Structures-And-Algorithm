637dda9acf3657468c93156d6b2f3dca979e762b
ayushporwal01 <porwalayush953@gmail.com>
2025-10-18 18:17:11 +0530

    Initial Commit

--- 05 - Strings/Easy/validParenthesis.cpp
+++ 05 - Strings/Easy/validParenthesis.cpp
@@ -2,27 +2,6 @@
 
 using namespace std;
 
-//Brute Force - TC: O(N), SC: O(N) - Using Stack
-bool validParenthesis(string s) {
-    stack<char> st;
-
-    for(auto c : s) {
-        if(c == '(' || c == '{' || c == '[') {
-           st.push(c);
-        }
-        else {
-            if(st.empty()) return false;
-            
-            if(c == ')' && st.top() != '(' ||
-               c == '}' && st.top() != '{' ||
-               c == ']' && st.top() != '[') {
-               return false;
-            }
-            st.pop();
-        }
-    }
-    return st.empty();
-}
 //Approach 1 - TC: O(N), SC: O(N)
 bool validParenthesis(string s) {
     stack<char> st;
