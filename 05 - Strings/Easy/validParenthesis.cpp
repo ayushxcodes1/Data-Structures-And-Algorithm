1201df7e99f85a396d45a8884a73d8fae86de1c9
ayushporwal01 <porwalayush953@gmail.com>
2025-10-18 18:13:18 +0530

    Initial Commit

--- 05 - Strings/Easy/validParenthesis.cpp
+++ 05 - Strings/Easy/validParenthesis.cpp
@@ -23,6 +23,48 @@ bool validParenthesis(string s) {
     }
     return st.empty();
 }
+//Approach 1 - TC: O(N), SC: O(N) - Using Stack
+bool validParenthesis(string s) {
+    stack<char> st;
+
+    for(auto c : s) {
+        if(c == '(' || c == '{' || c == '[') {
+           st.push(c);
+        }
+        else {
+            if(st.empty()) return false;
+
+            if(c == ')' && st.top() != '(' ||
+               c == '}' && st.top() != '{' ||
+               c == ']' && st.top() != '[') {
+               return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+//Brute Force - TC: O(N), SC: O(N) - Using Stack
+bool validParenthesis(string s) {
+    stack<char> st;
+
+    for(auto c : s) {
+        if(c == '(' || c == '{' || c == '[') {
+           st.push(c);
+        }
+        else {
+            if(st.empty()) return false;
+
+            if(c == ')' && st.top() != '(' ||
+               c == '}' && st.top() != '{' ||
+               c == ']' && st.top() != '[') {
+               return false;
+            }
+            st.pop();
+        }
+    }
+    return st.empty();
+}
 
 int main() {
   string s = "(()())(())";
