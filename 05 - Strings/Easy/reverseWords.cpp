6a622a61bb46e7f078811d849914d9d03bc5a260
ayushporwal01 <porwalayush953@gmail.com>
2025-10-19 14:50:15 +0530

    Initial Commit

--- 05 - Strings/Easy/reverseWords.cpp
+++ 05 - Strings/Easy/reverseWords.cpp
@@ -66,6 +66,29 @@ string reverseWords3(string s) {
     return ans.substr(1);
 }
 
+//Approach 3 - TC: O(N), SC: O(N) 
+string reverseWords3(string s) {
+    int n = s.length();
+    string ans = ""; //extra space
+
+    reverse(s.begin(), s.end());
+
+    for(int i = 0; i < n; i++) {
+        string word = "";
+        while(i < n && s[i] != ' ') {
+            word += s[i];
+            i++;
+        }
+
+        reverse(word.begin(), word.end());
+     
+        if(word.length() > 0) {
+           ans += " " + word;
+        }    
+    }
+    return ans.substr(1);
+}
+
 int main() {
   string s = "  hello  world  ";
 
