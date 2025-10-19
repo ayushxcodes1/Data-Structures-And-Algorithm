9e4d828ecc218d13a16c4ed609ef83edfd75f3e4
ayushporwal01 <porwalayush953@gmail.com>
2025-10-19 13:24:12 +0530

    Initial Commit

--- 05 - Strings/Easy/reverseWords.cpp
+++ 05 - Strings/Easy/reverseWords.cpp
@@ -4,31 +4,8 @@ using namespace std;
 
 //Brute Force - TC: O(N), SC: O(N)
 string reverseWords(string s) {
-    stack<string> st;
-    string word = "";
-    string ans = "";
-
-    //O(N)
-    for(auto c : s) {
-        if(c == ' ' && word == "") continue;
-        else if(c == ' ' && word != "") {
-            st.push(word);
-            word = "";  
-        }
-        else word += c;
-    }
-    if(word != "") st.push(word);
-
-    //O(N)
-    while(!st.empty()) {
-        ans += st.top();
-        st.pop();
-        if(!st.empty()) ans += " ";
-    }
-    return ans;
+    
 }
-
-//Optimal - TC: O(N), SC: O(1)
 
 int main() {
   string s = "  hello  world  ";
