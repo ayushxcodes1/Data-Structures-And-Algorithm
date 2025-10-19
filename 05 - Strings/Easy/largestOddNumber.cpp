632eb85c2f937b799cb855c91e19a7e28844fea9
ayushporwal01 <porwalayush953@gmail.com>
2025-10-19 17:47:39 +0530

    Initial Commit

--- 05 - Strings/Easy/largestOddNumber.cpp
+++ 05 - Strings/Easy/largestOddNumber.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 string largestOddNumber(string s) {
     int num = stoi(s);
-    int lastDigit = num % 10;
     string ans = "";
-
+    
     //even
+    int lastDigit = num % 10;
     if(lastDigit % 2 == 0) {
        num = num / 10; 
     }
