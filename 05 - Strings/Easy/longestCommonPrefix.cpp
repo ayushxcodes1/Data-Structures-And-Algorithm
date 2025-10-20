b748c25e0ecda83f14ad5be70590d55b90cdc278
ayushporwal01 <porwalayush953@gmail.com>
2025-10-20 18:12:31 +0530

    Initial Commit

--- 05 - Strings/Easy/longestCommonPrefix.cpp
+++ 05 - Strings/Easy/longestCommonPrefix.cpp
@@ -2,22 +2,25 @@
 
 using namespace std;
 
+//Brute Force - TC: O(N * M), SC: (M)
 string longestCommonPrefix(vector<string> &s) {
     int n = s.size();
     if(n == 0) return "";
 
     string ans = s[0];
 
+    //O(N)
     for(int i = 1; i < n; i++) {
         string curr = s[i];
         string temp = "";
         int len = min(ans.size(), curr.size());
 
+        //O(M)
         for(int j = 0; j < len; j++) {
             if(ans[j] == curr[j]) temp += ans[j];
             else break;
         }
-        
+
         ans = temp;
         if(ans.empty()) return "";
     }
