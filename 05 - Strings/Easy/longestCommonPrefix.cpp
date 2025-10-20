aaa3397dc7123bf1b4fa71be530e1da01b9ccc71
ayushporwal01 <porwalayush953@gmail.com>
2025-10-20 17:34:22 +0530

    Initial Commit

--- 05 - Strings/Easy/longestCommonPrefix.cpp
+++ 05 - Strings/Easy/longestCommonPrefix.cpp
@@ -5,10 +5,19 @@ using namespace std;
 string longestCommonPrefix(vector<int> &s) {
     int n = s.size();
     string word = "";
+    string word2 = "";
     string ans = "";
     int i = 0;
     while(i < n) {
         word += s[i];
+        i++;
+        
+        int l = 0;
+        int r = 0;
+        if(l == r) {
+           l++; 
+           r++;
+        }
     }
 }    
 
