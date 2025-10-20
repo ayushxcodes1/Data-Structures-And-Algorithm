4345920727a7b1c8d59ca24488b200bd48750d30
ayushporwal01 <porwalayush953@gmail.com>
2025-10-20 12:40:26 +0530

    Initial Commit

--- 05 - Strings/Easy/largestOddNumber.cpp
+++ 05 - Strings/Easy/largestOddNumber.cpp
@@ -4,7 +4,13 @@ using namespace std;
 
 //Brute - TC(N), SC: O(N)
 string largestOddNumber(string s) {
-    
+    int n = s.size();
+
+    for(int i = n - 1; i >= 0; i++) {
+        if((s[i] - '0') % 2 == 1) {
+            
+        }
+    }
 }
 
 int main() {
