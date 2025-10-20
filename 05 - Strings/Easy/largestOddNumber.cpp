5444533980a565c0dc8e17051eb3bf27e4e8a756
ayushporwal01 <porwalayush953@gmail.com>
2025-10-20 12:23:15 +0530

    Initial Commit

--- 05 - Strings/Easy/largestOddNumber.cpp
+++ 05 - Strings/Easy/largestOddNumber.cpp
@@ -6,18 +6,15 @@ string largestOddNumber(string s) {
     int num = stoi(s);
     string ans = "";
     
-    while(num > 0) {
+    while(num % 2 == 0) {
         int lastDigit = num % 10;
         
         //even
         if(lastDigit % 2 == 0) {
            num = num / 10; 
         }
-        else {
-            ans = to_string(num); 
-            break;
-        }
     }
+    ans = to_string(num); 
     return ans;
 }
 
