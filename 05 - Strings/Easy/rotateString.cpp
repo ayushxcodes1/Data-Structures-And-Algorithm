74859b4a5b179c8c82b604e940263630fb86acb6
ayushporwal01 <porwalayush953@gmail.com>
2025-10-21 16:41:23 +0530

    Initial Commit

--- 05 - Strings/Easy/rotateString.cpp
+++ 05 - Strings/Easy/rotateString.cpp
@@ -18,6 +18,19 @@ bool rotateString(string s, string goal) {
 }
 
 //Optimal - TC: O(N), SC: O(1)
+bool rotateString(string s, string goal) {
+    int n = s.size();
+    
+    for(int i = 0; i < n - 2; i++) {
+        char temp = s[0];
+        for(int j = 1; j < n; j++) {
+            s[j - 1] = s[j]; 
+        }
+        s[n - 1] = temp;
+        if(s == goal) return true;
+    }
+    return false;
+}
 
 int main() {
   string s = "abcde";
