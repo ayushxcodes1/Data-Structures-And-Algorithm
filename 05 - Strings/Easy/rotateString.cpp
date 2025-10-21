db518e25d4132e9771121fd2b201e2d7c0dfae67
ayushporwal01 <porwalayush953@gmail.com>
2025-10-21 16:23:44 +0530

    Initial Commit

--- 05 - Strings/Easy/rotateString.cpp
+++ 05 - Strings/Easy/rotateString.cpp
@@ -2,12 +2,16 @@
 
 using namespace std;
 
+bool rotateString() {
+    
+}
+
 int main() {
   string s = "abcde";
   string goal = "cdeab";
 
   bool ans = rotateString(s, goal);
   cout << ans;
-  
+
   return 0;
 }
