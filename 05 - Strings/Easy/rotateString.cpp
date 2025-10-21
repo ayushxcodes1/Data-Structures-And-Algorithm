80506f9769da89d3597fd80cfeaaee98f2cafd63
ayushporwal01 <porwalayush953@gmail.com>
2025-10-21 16:45:19 +0530

    Initial Commit

--- 05 - Strings/Easy/rotateString.cpp
+++ 05 - Strings/Easy/rotateString.cpp
@@ -21,6 +21,12 @@ bool rotateString(string s, string goal) {
 bool rotateString(string s, string goal) {
     int n = s.size();
     if(n != goal.size()) return false;
+
+    for(int i = 0; i < n; i++) {
+        s = s.substr(1) + s[0];
+        if (s == goal) return true;
+    }
+    return false;
    
 }
 
