c63b5c473be9076b8c4fb361f27d713d28cfd48f
ayushporwal01 <porwalayush953@gmail.com>
2025-10-21 16:42:41 +0530

    Initial Commit

--- 05 - Strings/Easy/rotateString.cpp
+++ 05 - Strings/Easy/rotateString.cpp
@@ -6,7 +6,7 @@ using namespace std;
 bool rotateString(string s, string goal) {
     int n = s.size();
     
-    for(int i = 0; i < n - 2; i++) {
+    for(int i = 0; i < n; i++) {
         char temp = s[0];
         for(int j = 1; j < n; j++) {
             s[j - 1] = s[j]; 
