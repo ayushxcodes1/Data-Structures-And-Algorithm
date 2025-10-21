8579060a8ffd98455f7d885a9e8fb349da8a264d
ayushporwal01 <porwalayush953@gmail.com>
2025-10-21 17:01:28 +0530

    Initial Commit

--- 05 - Strings/Easy/rotateString.cpp
+++ 05 - Strings/Easy/rotateString.cpp
@@ -49,7 +49,10 @@ int main() {
   bool ans = rotateString(s, goal);
   cout << "Brute: " << ans << endl;
 
-  bool ans2 = rotateString(s, goal);
+  bool ans2 = rotateString2(s, goal);
+  cout << "Optimal: " << ans2 << endl;
+  
+  bool ans2 = rotateString2(s, goal);
   cout << "Optimal: " << ans2 << endl;
 
   return 0;
