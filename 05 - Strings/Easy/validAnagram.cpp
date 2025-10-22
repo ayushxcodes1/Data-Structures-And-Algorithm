4d5b998d7ef776487156218fa42740650ef973d4
ayushporwal01 <porwalayush953@gmail.com>
2025-10-22 21:42:42 +0530

    Initial Commit

--- 05 - Strings/Easy/validAnagram.cpp
+++ 05 - Strings/Easy/validAnagram.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 //Brute Force
 bool isAnagram(string s, string t) {
-    
+  if(s.size() != t.size()) return false;
+  sort(s.begin(), s.end());
+  sort(t.begin(), t.end());
+  if(s == t) return true;
+  return false;
 }
 
 int main() {
