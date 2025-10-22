dc27a4be43f1fbc547e02fdfb5772c2ec6fd4fc2
ayushporwal01 <porwalayush953@gmail.com>
2025-10-22 21:48:26 +0530

    Initial Commit

--- 05 - Strings/Easy/validAnagram.cpp
+++ 05 - Strings/Easy/validAnagram.cpp
@@ -6,8 +6,13 @@ using namespace std;
 bool isAnagram(string s, string t) {
   if(s.size() != t.size()) return false;
   sort(s.begin(), s.end());
-  sort(t.begin(), t.end());
-  return s == t;
+  
+  do {
+    if(s == t) return true;
+  }
+  while(next_permutation(s.begin(), s.end()));
+  
+  return false;
 }
 
 //Better - TC: O(NlogN), SC: O(1)
