bd66e46d2399fed52ff0094b6918f0e5dc80328a
ayushporwal01 <porwalayush953@gmail.com>
2025-10-22 11:48:03 +0530

    Initial Commit

--- 05 - Strings/Easy/rotateString.cpp
+++ 05 - Strings/Easy/rotateString.cpp
@@ -63,7 +63,7 @@ bool KMP(string s, string goal) {
             j++;
         }
 
-        if (j == m) { // full pattern found
+        if (j == m) { // pattern found
             return true;
         } else if (i < n && s[i] != goal[j]) {
             if (j != 0)
