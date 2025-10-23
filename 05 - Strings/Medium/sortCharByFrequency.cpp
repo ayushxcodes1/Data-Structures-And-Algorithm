5d783427a024603c4644ad8314cfeed0503638d7
ayushporwal01 <porwalayush953@gmail.com>
2025-10-23 11:56:33 +0530

    Initial Commit

--- 05 - Strings/Medium/sortCharByFrequency.cpp
+++ 05 - Strings/Medium/sortCharByFrequency.cpp
@@ -4,30 +4,15 @@ using namespace std;
 
 //Method 1 - TC: O(N), SC: O(N) - ASCII Array
 string frequencySort(string s) { 
-    int n = s.size();
-    
-    vector<pair<char, int>> v(123);
+    vector<int> freq(128, 0);
+    for(auto ch : s) freq[ch]++;
 
-    for(char &ch : s) {
-        int freq = v[ch].second;
-        v[ch] = {ch, freq + 1};
-    }
-
-    auto cmp = [&](pair<char, int> &a, pair<char, int> &b) {
-        return a.second > b.second;
+    auto cmp = [&](char a, char b) {
+        if(freq[a] == freq[b]) return a < b;
+        return freq[a] > freq[b];
     };
-
-    string ans = "";
-    for(int i = 0; i <= 122; i++) {
-        if(v[i].second > 0) {
-           char ch = v[i].first;
-           int freq = v[i].second;
-           string temp = string(freq, ch);
-
-           ans += temp;
-        }
-    }
-    return ans;
+    sort(s.begin(), s.end(), cmp);
+    return s;
 }
 
 //Method 1 - TC: O(N), SC: O(N) - ASCII Array
