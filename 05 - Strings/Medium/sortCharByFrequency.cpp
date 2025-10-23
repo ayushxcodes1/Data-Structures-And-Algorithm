9ef27d7bc0edbef6622292397397fdd0f6618aef
ayushporwal01 <porwalayush953@gmail.com>
2025-10-23 12:21:34 +0530

    Initial Commit

--- 05 - Strings/Medium/sortCharByFrequency.cpp
+++ 05 - Strings/Medium/sortCharByFrequency.cpp
@@ -93,6 +93,16 @@ string frequencySort(string s) {
     unordered_map<char, int> freq;
     for(char ch : s) freq[ch]++;
 
+    vector<vector<char>> bucketData(s.length() + 1);
+    for(auto [ch, fq] : freq) bucketData[fq].push_back(ch);
+
+    string ans = "";
+    for(int fq = s.length(); fq >= 1; fq--) {
+        for(auto ch : bucketData[fq]) {
+            ans.append(fq, ch);
+        }
+    }
+    return ans;
 }
 
 int main() {
