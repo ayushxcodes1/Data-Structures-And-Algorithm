4cf137bdfd111fb92a1a1ae24aaffebd20b490fb
ayushporwal01 <porwalayush953@gmail.com>
2025-10-23 11:36:04 +0530

    Initial Commit

--- 05 - Strings/Medium/sortCharByFrequency.cpp
+++ 05 - Strings/Medium/sortCharByFrequency.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+//Brute: TC: O(NlogK), SC: O(N) - Map + Priority Queue
+string frequencySort2(string s) { 
+    int n = s.size();
+    
+    unordered_map<char, int> freq;
+    for(char c : s) {
+        freq[c]++;
+    }
+
+    priority_queue<pair<int, char>> pq;
+    for(auto &[ch, count] : freq) {
+        pq.push({count, ch});
+    }
+
+    string ans = "";
+    while(!pq.empty()) {
+        auto [count, ch] = pq.top();
+        pq.pop();
+        ans.append(count, ch);
+    }
+    return ans;
+}
+
 //Brute: TC: O(N), SC: O(N) - ASCII Array Method
 string frequencySort(string s) { 
     int n = s.size();
@@ -30,28 +53,6 @@ string frequencySort(string s) {
     return ans;
 }
 
-//Brute: TC: O(NlogK), SC: O(N) - Map + Priority Queue
-string frequencySort2(string s) { 
-    int n = s.size();
-    
-    unordered_map<char, int> freq;
-    for(char c : s) {
-        freq[c]++;
-    }
-
-    priority_queue<pair<int, char>> pq;
-    for(auto &[ch, count] : freq) {
-        pq.push({count, ch});
-    }
-
-    string ans = "";
-    while(!pq.empty()) {
-        auto [count, ch] = pq.top();
-        pq.pop();
-        ans.append(count, ch);
-    }
-    return ans;
-}
 
 //Optimal - TC: O(N), SC: O(1)
 // string frequencySort(string s) {
