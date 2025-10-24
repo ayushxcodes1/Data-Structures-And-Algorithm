0d6fbe7860c462b08daa4be95a4cb8b8543e1b9d
ayushporwal01 <porwalayush953@gmail.com>
2025-10-24 11:27:16 +0530

    Initial Commit

--- 05 - Strings/Easy/isomorphicString.cpp
+++ 05 - Strings/Easy/isomorphicString.cpp
@@ -8,6 +8,10 @@ bool isomorphicString(string s, string t) {
     int m2[256] = {0};
 
     int n = s.size();
+
+    for(int i = 0; i < n; i++) {
+        if(m1[s[i]] != m2[t[i]]) return false;
+    }
 }
 
 int main() {
