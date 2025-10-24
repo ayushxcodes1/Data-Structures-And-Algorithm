fec80e473567bc75f2d567ab61e0489d7e291d54
ayushporwal01 <porwalayush953@gmail.com>
2025-10-24 11:35:34 +0530

    Initial Commit

--- 05 - Strings/Easy/isomorphicString.cpp
+++ 05 - Strings/Easy/isomorphicString.cpp
@@ -13,7 +13,7 @@ bool isomorphicString(string s, string t) {
         if(m1[s[i]] != m2[t[i]]) return false;
         
         m1[s[i]] = i + 1;
-        m1[t[i]] = i + 1;
+        m2[t[i]] = i + 1;
     }
     return true;
 }
