b4b7f69b6caf740fa48806b8d1e734f2c43c2dc6
ayushporwal01 <porwalayush953@gmail.com>
2025-10-28 10:52:18 +0530

    Initial Commit

--- 05 - Strings/Medium/maximumNestingDepth.cpp
+++ 05 - Strings/Medium/maximumNestingDepth.cpp
@@ -10,7 +10,7 @@ int maxDepth(string s) {
     int maxDepth = 0;
 
     for(int i = 0; i < s.length(); i++) {
-        if(s[i] == '(' ||  s[i] == '[' || s[i] == '}') {
+        if(s[i] == '(' ||  s[i] == '[' || s[i] == '{') {
            st.push(s[i]);
            depth = st.size();
            maxDepth = max(depth, maxDepth);
