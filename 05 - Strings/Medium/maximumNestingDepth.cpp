4cc0b7697850ec8661315af163114f64bb52c623
ayushporwal01 <porwalayush953@gmail.com>
2025-10-28 10:22:42 +0530

    Initial Commit

--- 05 - Strings/Medium/maximumNestingDepth.cpp
+++ 05 - Strings/Medium/maximumNestingDepth.cpp
@@ -2,24 +2,15 @@
 
 using namespace std;
 
-//Brute - TC: O(NlogN), SC: O(N) - Sort N Elements
-string frequencySort(string s) { 
-    vector<int> freq(128, 0);
-    for(auto ch : s) freq[ch]++;
-
-    //lambda function
-    auto cmp = [&](char a, char b) {
-        if(freq[a] == freq[b]) return a < b;
-        return freq[a] > freq[b];
-    };
-    sort(s.begin(), s.end(), cmp);
-    return s;
+//Brute - TC: O(), SC: O() - Depth Counter
+string maxDepth(string s) { 
+    
 }
 
 int main() {
   string s = "(1+(2*3)+((8)/4))+1";
 
-  string ans = frequencySort(s);
+  string ans = maxDepth(s);
   cout << "Brute: " << ans << endl;
 
 //   string ans2 = frequencySort2(s);
