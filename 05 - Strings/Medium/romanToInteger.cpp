d1a82ee4671437a51454f2e1670bedcd7f854fa2
ayushporwal01 <porwalayush953@gmail.com>
2025-10-29 18:20:41 +0530

    Initial Commit

--- 05 - Strings/Medium/romanToInteger.cpp
+++ 05 - Strings/Medium/romanToInteger.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+int romanToInteger(string s) {
+    int I = 1;
+    int V = 5;
+    int X = 10;
+    int L = 50;
+    int C = 100;
+    int D = 500;
+    int M = 1000;
+
+    int a = 0, b = 0;
+    for(int i = 0; i < s.length(); i++) {
+      if(s[i] == 'I' && s[i + 1] == 'V' || s[i + 1] == 'X') {
+         
+      }
+    }
+}
 
 int main() {
   
