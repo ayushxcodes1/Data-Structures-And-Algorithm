b49829867968d7e34dbbb4fadaba970e3315d771
ayushporwal01 <porwalayush953@gmail.com>
2025-10-30 12:08:57 +0530

    Initial Commit

--- 05 - Strings/Medium/stringToInteger.cpp
+++ 05 - Strings/Medium/stringToInteger.cpp
@@ -6,25 +6,10 @@ using namespace std;
 int stringToInteger(string s) {
   int res = 0;
   int sign = 1;
-
-  for(int i = 0; i < s.length(); i++) {
-    if(res < INT_MIN) res = INT_MIN;
-    else if(res > INT_MAX) res = INT_MAX;
-
-    if(s[i] == ' ') continue;
-    else if(isdigit(s[i])) {
-      res = res * 10 + (s[i] - '0');
-    }
-    else if(s[i] == '-') {  
-      sign = -1;
-    }
-    else {
-      res = res * sign;
-      return res;
-    }
-  }
-  res = res * sign;
-  return res;
+  int n = s.length();
+  
+  int i = 0;
+  while(i < n && s[i] == ' ') continue;
 }
 
 int main() {
