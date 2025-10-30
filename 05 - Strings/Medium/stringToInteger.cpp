77e9f32b8b57b80e7fbe956172c451c8adbac936
ayushporwal01 <porwalayush953@gmail.com>
2025-10-30 10:48:39 +0530

    Initial Commit

--- 05 - Strings/Medium/stringToInteger.cpp
+++ 05 - Strings/Medium/stringToInteger.cpp
@@ -3,17 +3,8 @@
 using namespace std;
 
 int stringToInteger(string s) {
-  int res = 0;
 
-  for(int i = 0; i < s.length(); i++) {
-    if(res < -2e31) round(-2e31);
-    if(res > 2e31-1) round(2e31-1);
-    if(s[i] == ' ') continue;
-    else if(isdigit(s[i])) {
-      res = res * 10 + (s[i] - '0');
-    }
-    else return res;
-  }
+
 }
 
 int main() {
