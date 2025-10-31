50dc8698b567ede941235ce166561d198b58234a
ayushporwal01 <porwalayush953@gmail.com>
2025-10-31 12:51:27 +0530

    Initial Commit

--- 05 - Strings/Medium/countSubstrings.cpp
+++ 05 - Strings/Medium/countSubstrings.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+int countSubstrings(string s) {
+    int n = string.length();
+
+    
+}
+
 int main() {
   string s = "abcabc";
+  int ans = countSubstrings(string s);
   return 0;
 }
