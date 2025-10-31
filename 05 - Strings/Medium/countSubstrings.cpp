70325253ced5c3e7e518726a7943da69ce422125
ayushporwal01 <porwalayush953@gmail.com>
2025-10-31 15:49:48 +0530

    Initial Commit

--- 05 - Strings/Medium/countSubstrings.cpp
+++ 05 - Strings/Medium/countSubstrings.cpp
@@ -27,21 +27,11 @@ int countSubstrings(string s) {
 //Optimal - TC: O(N^2), SC: O(1) - Sliding Window 
 int countSubstrings(string s) {
     int n = s.length();
+    vector<int> lastSeen(3, -1);
     int cnt = 0;
 
     for(int i = 0; i < n; i++) {
-        bool isA = false;
-        bool isB = false;
-        bool isC = false;
-        for(int j = i; j < n; j++) {
-            if(s[j] == 'a') isA = true;
-            else if(s[j] == 'b') isB = true;
-            else isC = true;
-
-            if(isA == true && isB == true && isC == true) {
-               cnt++;
-            }
-        }
+    
     }
     return cnt;
 }
