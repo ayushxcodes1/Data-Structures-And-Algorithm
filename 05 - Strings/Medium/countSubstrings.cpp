3ac72bf3a01dd911f800a8eb95990613854ee37f
ayushporwal01 <porwalayush953@gmail.com>
2025-10-31 13:56:14 +0530

    Initial Commit

--- 05 - Strings/Medium/countSubstrings.cpp
+++ 05 - Strings/Medium/countSubstrings.cpp
@@ -7,16 +7,19 @@ int countSubstrings(string s) {
     int cnt = 0;
 
     for(int i = 0; i < n; i++) {
+        cout << "i = " << i << endl; 
         bool isA = false;
         bool isB = false;
         bool isC = false;
         for(int j = 0; j < n; j++) {
+            cout << "j = " << j << endl; 
             if(s[j] == 'a') isA = true;
             else if(s[j] == 'b') isB = true;
             else isC = true;
 
             if(isA == true && isB == true && isC == true) {
                cnt++;
+               cout << "count: " << cnt << endl;
             }
         }
     }
