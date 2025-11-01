ae239330802d7884ac9fc3f21eb99d80b3682c30
ayushporwal01 <porwalayush953@gmail.com>
2025-11-01 07:48:38 +0530

    Initial Commit

--- 05 - Strings/Medium/longestPalindromicSubstring.cpp
+++ 05 - Strings/Medium/longestPalindromicSubstring.cpp
@@ -5,6 +5,12 @@ using namespace std;
 string longestSubstring(string s) {
     int n = s.length();
     string ans = "";
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            
+        }
+    }
 }
 
 int main() {
