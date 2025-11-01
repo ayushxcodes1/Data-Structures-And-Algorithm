1803831b12173ac7f20a0f29ed3a481c379da194
ayushporwal01 <porwalayush953@gmail.com>
2025-11-01 08:03:31 +0530

    Initial Commit

--- 05 - Strings/Medium/longestPalindromicSubstring.cpp
+++ 05 - Strings/Medium/longestPalindromicSubstring.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+bool isPalindrome(string s) {
+    int n = s.length();
+    int i = 0, j = n - 1;
+    while(i < j) {
+        
+    }
+}
+
 string longestSubstring(string s) {
     int n = s.length();
     string curr = "";
