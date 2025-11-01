707a37e8e8fd8a0b856e9d6117cf0799469b516b
ayushporwal01 <porwalayush953@gmail.com>
2025-11-01 07:48:49 +0530

    Initial Commit

--- 05 - Strings/Medium/longestPalindromicSubstring.cpp
+++ 05 - Strings/Medium/longestPalindromicSubstring.cpp
@@ -7,8 +7,8 @@ string longestSubstring(string s) {
     string ans = "";
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            
+        for(int j = i; j < n; j++) {
+
         }
     }
 }
