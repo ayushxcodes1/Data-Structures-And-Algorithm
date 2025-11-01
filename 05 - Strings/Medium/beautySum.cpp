e1f6179c693410e9183cc6d589c1337193ab1af5
ayushporwal01 <porwalayush953@gmail.com>
2025-11-01 18:49:13 +0530

    Initial Commit

--- 05 - Strings/Medium/beautySum.cpp
+++ 05 - Strings/Medium/beautySum.cpp
@@ -2,9 +2,31 @@
 
 using namespace std;
 
-int beautySum(string s) {
-    int n = s.length();
+int findMostFreqCnt(string s, int i, int j) {
+  int n = s.length();
+  int cnt = 0;
+  unordered_map<char, int> mpp;
 
+  for(auto ch : s) {
+    mpp[ch]++;
+  }
+  return cnt;
+}
+
+int beautySum(string s) {
+  int n = s.length();
+  int ans = 0;
+  
+  for(int i = 0; i < n; i++) {
+    for(int j = i; j < n; j++) {
+      int mostFqCnt = findMostFreqCnt(s, i, j);
+      int leastFqCnt = findLeastFreqCnt(s, i, j);
+      int diff = mostFqCnt - leastFqCnt;
+      ans += diff;
+    }
+  }
+  return ans;
+    
 }
 
 int main() {
