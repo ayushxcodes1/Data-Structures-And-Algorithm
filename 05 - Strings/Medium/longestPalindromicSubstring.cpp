a439f0f401dd2860b2a6c73d84a7ea171f94c072
ayushporwal01 <porwalayush953@gmail.com>
2025-11-02 17:31:27 +0530

    Initial Commit

--- 05 - Strings/Medium/longestPalindromicSubstring.cpp
+++ 05 - Strings/Medium/longestPalindromicSubstring.cpp
@@ -33,6 +33,23 @@ string longestSubstring(string s) {
     return ans;
 }
 
+//Optimal - TC:O(N^3), SC: O(1)
+string longestSubstring(string s) {
+    int n = s.length();
+    string ans = "";
+
+    for(int i = 0; i < n; i++) {    
+        for(int j = i; j < n; j++) {
+            if(isPalindrome(s, i, j)) {
+                if(j - i + 1 > ans.size()) {
+                   ans = s.substr(i, j - i + 1);
+                }
+            }
+        }
+    }
+    return ans;
+}
+
 int main() {
   string s = "babad";
   string ans = longestSubstring(s);
