0d283a8c2fd1342091620e86493f034c442fb585
ayushporwal01 <porwalayush953@gmail.com>
2025-11-05 17:44:41 +0530

    Initial Commit

--- 05 - Strings/Medium/longestPalindromicSubstring.cpp
+++ 05 - Strings/Medium/longestPalindromicSubstring.cpp
@@ -41,7 +41,11 @@ string longestSubstring2(string s) {
     for(int i = 0; i < n; i++) dp[i][i] = true;
 
     for(int i = 0; i < n - 1; i++) {
-        
+        if(s[i] == s[i + 1]) {
+           dp[i][i + 1] = true;
+           st = i;
+           maxLen = 2;
+        }
     }
 }
 
