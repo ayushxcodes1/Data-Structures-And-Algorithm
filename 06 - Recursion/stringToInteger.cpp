d2812ec4164fda355ea6785fddf26ea547539847
ayushporwal01 <porwalayush953@gmail.com>
2025-11-12 17:15:23 +0530

    Initial Commit

--- 06 - Recursion/stringToInteger.cpp
+++ 06 - Recursion/stringToInteger.cpp
@@ -2,36 +2,14 @@
 
 using namespace std;
 
-int myAtoi(string s, int i) {
+int myAtoi(string s) {
     int n = s.size();
-    int ans = 0;
-    int sign = 1;
     
-    if(i == n) return;
-
-    if(s[i] == ' ') {
-       myAtoi(s, i + 1);
-    }
-
-    if(s[i] == '-') {
-       sign = -1;
-    } else 
-       sign = 1;
-
-    if(isdigit(s[i])) {
-       ans = ans * 10 + (s[i] - '0');
-    }
-    
-    if(ans < INT_MIN) ans = INT_MIN;
-    if(ans > INT_MAX) ans = INT_MAX;
-
-    myAtoi(s, i + 1);
-    return (ans * sign);
 }
 
 int main() {
   string s = "-42"; 
-  int ans = myAtoi(s, 0);
+  int ans = myAtoi(s);
   cout << ans << endl;
   return 0;
 }
