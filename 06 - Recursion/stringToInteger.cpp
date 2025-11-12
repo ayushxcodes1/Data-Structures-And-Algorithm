1f72f5b1cd100b06f3595e58c19d4d02bd4a1a28
ayushporwal01 <porwalayush953@gmail.com>
2025-11-12 17:07:22 +0530

    Initial Commit

--- 06 - Recursion/stringToInteger.cpp
+++ 06 - Recursion/stringToInteger.cpp
@@ -19,8 +19,11 @@ int myAtoi(string s, int i) {
        sign = 1;
 
     if(isdigit(s[i])) {
-       
+       ans = ans * 10 + (s[i] - '0');
     }
+    
+    if(ans < INT_MIN) ans = INT_MIN;
+    if(ans < INT_MAX) ans = INT_MAX;
 }
 
 int main() {
