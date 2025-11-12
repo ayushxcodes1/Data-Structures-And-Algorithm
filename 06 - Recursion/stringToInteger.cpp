86c94cbf24c925b2f066af92b16f9ab92876f4e9
ayushporwal01 <porwalayush953@gmail.com>
2025-11-12 17:16:19 +0530

    Initial Commit

--- 06 - Recursion/stringToInteger.cpp
+++ 06 - Recursion/stringToInteger.cpp
@@ -5,11 +5,12 @@ using namespace std;
 int myAtoi(string s) {
     int n = s.size();
     
+    while(string s, int i)
 }
 
 int main() {
   string s = "-42"; 
-  int ans = myAtoi(s);
+  int ans = myAtoi(s, 0);
   cout << ans << endl;
   return 0;
 }
