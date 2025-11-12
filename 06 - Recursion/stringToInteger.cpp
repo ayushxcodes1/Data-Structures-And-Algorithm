007d6d1f5f0c43e458495f61264a1984e5ff77af
ayushporwal01 <porwalayush953@gmail.com>
2025-11-12 17:18:32 +0530

    Initial Commit

--- 06 - Recursion/stringToInteger.cpp
+++ 06 - Recursion/stringToInteger.cpp
@@ -6,7 +6,13 @@ int myAtoi(string s, int i = 0) {
     int n = s.size();
     
     //skip leading whitespaces
-    while(i < n && s[i] == ' ') i++; 
+    while(i < n && s[i] == ' ') i++;
+    
+    int sign = 1;
+    if(s[i] == '-') {
+       sign = -1;
+    } else 
+       sign = 1;
 }
 
 int main() {
