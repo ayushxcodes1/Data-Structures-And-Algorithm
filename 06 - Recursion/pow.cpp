b095368acb7e6e83e715c91dc737a0feb4011c28
ayushporwal01 <porwalayush953@gmail.com>
2025-11-13 10:23:39 +0530

    Initial Commit

--- 06 - Recursion/pow.cpp
+++ 06 - Recursion/pow.cpp
@@ -39,23 +39,7 @@ double myPow2(double x, int n) {
 
 //Optimal - TC: O(logN), SC: O(logN) - Recursive - Exponentiation
 double myPow2(double x, int n) {
-  double ans = 1.0;
-  long long nn = n;
-  if(nn < 0) nn = -nn;
-
-  while(nn > 0) {
-    if(nn % 2 == 1) {
-      ans = ans * x;
-      nn = nn - 1;
-    }
-    else {
-      x = x * x;
-      nn = nn / 2;
-    }
-  }
-
-  if(n < 0) ans = 1.0 / ans;
-  return ans;
+  
 }
 
 int main() {
