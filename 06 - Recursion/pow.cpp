5dd7df5ce08b7ff46059cc956fb5896a9da0a09b
ayushporwal01 <porwalayush953@gmail.com>
2025-11-13 09:52:56 +0530

    Initial Commit

--- 06 - Recursion/pow.cpp
+++ 06 - Recursion/pow.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-//Brute - Iterative - TOO SLOW
+//Brute - Iterative - TOO SLOW for large N
 double myPow(double x, int n, double ans) {
   int ans = 
   if(n == 0) return ans;
